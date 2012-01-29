286f6888f3a3850c30d9f9039669bf2562ab77e2
Alex Parrill <initrd.gz@gmail.com>
2012-01-29 13:27:10 -0600

    Added type checking to files library and file.Append. Close #20 cause I failed the last message

--- lua/starfall2/libs_sh/files.lua
+++ lua/starfall2/libs_sh/files.lua
@@ -25,6 +25,7 @@ file.CreateDir("StarfallScriptData/")
 -- @return Contents, or nil if error
 -- @return Error message if applicable
 function files_library.read(path)
+	SF.CheckType(path, "string")
 	if path:find("..",1,true) then return nil, "path contains '..'" end
 	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
 	local contents = file.Read("StarfallScriptData/"..path)
@@ -36,9 +37,24 @@ end
 -- @return True if OK, nil if error
 -- @return Error message if applicable
 function files_library.write(path, data)
+	SF.CheckType(path, "string")
+	SF.CheckType(data, "string")
 	if path:find("..",1,true) then return nil, "path contains '..'" end
 	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
 	file.Write("StarfallScriptData/"..path, data)
+	return true
+end
+
+--- Appends a string to the end of a file
+-- @param path Filepath relative to data/StarfallScriptData/. Cannot contain '..'
+-- @param data String that will be appended to the file.
+-- @return Error message if applicable
+function files_library.append(path,data)
+	SF.CheckType(path, "string")
+	SF.CheckType(data, "string")
+	if path:find("..",1,true) then return nil, "path contains '..'" end
+	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
+	file.Append("StarfallScriptData/"..path, data)
 	return true
 end
 
@@ -47,6 +63,7 @@ end
 -- @return True if exists, false if not, nil if error
 -- @return Error message if applicable
 function files_library.exists(path)
+	SF.CheckType(path, "string")
 	if path:find("..",1,true) then return nil, "path contains '..'" end
 	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
 	return file.Exists(path)
@@ -57,6 +74,7 @@ end
 -- @return True if successful, nil if error
 -- @return Error message if applicable
 function files_library.delete(path)
+	SF.CheckType(path, "string")
 	if path:find("..",1,true) then return nil, "path contains '..'" end
 	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
 	if not file.Exists(path) then return nil, "doesn't exist" end
