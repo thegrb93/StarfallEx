6b0ae88e8d67e04b0c000531e9f2bfbc47abf3af
Cenius <Cenius@localhost.(none)>
2012-02-23 12:21:53 +0400

    Added Lua syntax highlight in editor, set default code in editor to information about Starfall (cherry picked from commit 9d1a2f7397ea1f4ff1a4e42c16dd72fa28f3a4b9)

--- lua/starfall/editor.lua
+++ lua/starfall/editor.lua
@@ -1,4 +1,3 @@
-
 SF.Editor = {}
 
 -- TODO: Server-side controls
@@ -16,8 +15,9 @@ if CLIENT then
 		SF.Editor.editor = vgui.Create("Expression2EditorFrame")
 		SF.Editor.editor:Setup("SF Editor", "Starfall", "nothing") -- Setting the editor type to not nil keeps the validator line
 		
-		--SF.Editor.editor:SetSyntaxColorLine( highlighter )
-		SF.Editor.editor:SetSyntaxColorLine( function(self, row) return {{self.Rows[row], Color(255,255,255)}} end)
+		-- Highlighting
+		SF.Editor.editor:SetSyntaxColorLine(SF.Editor.highlighter)
+		--SF.Editor.editor:SetSyntaxColorLine( function(self, row) return {{self.Rows[row], Color(255,255,255)}} end)
 		
 		function SF.Editor.editor:Validate(gotoerror)
 			local err = CompileString(self:GetCode(), "SF:"..(self:GetChosenFile() or "main"), false)
@@ -31,6 +31,24 @@ if CLIENT then
 				self.C['Val'].panel:SetFGColor(255, 255, 255, 128)
 				self.C['Val'].panel:SetText( "   No Syntax Errors" )
 			end
+		end
+		
+		local oldNewTab = SF.Editor.editor.NewTab
+		function SF.Editor.editor:NewTab(p) 
+			oldNewTab(self, p)
+			self:SetCode([=[--@name 
+--@author
+
+-- Starfall Scripting Environment
+-- ==============================
+-- 
+-- Starfall author: Colonel Thirty Two
+-- More info: http://colonelthirtytwo.net/index.php/starfall/
+-- Reference Page: http://colonelthirtytwo.net/sfdoc/
+-- Development Thread: http://www.wiremod.com/forum/developers-showcase/22739-starfall-processor.html
+-- Blog: http://blog.colonelthirtytwo.net/
+--
+]=])
 		end
 	end
 	
@@ -104,6 +122,102 @@ if CLIENT then
 			return false, msg
 		else
 			error(msg,0)
+		end
+	end
+	
+	--- A highlighter block
+	do
+		local keywords = {
+			"and", "break", "do", "else", "elseif",
+			"end", "false", "for", "function", "if", 
+			"in", "local", "nil", "not", "or",
+			"repeat", "return", "then", "true", "until", "while"
+		}
+		
+		local colors = {
+			["keyword"]   	= { Color(160, 240, 240), false},
+			["operator"]  	=  { Color(224, 224, 224), false},
+			["expression"] 	=    { Color(160, 160, 240), false},
+			
+			["number"]    	=  { Color(240, 160, 160), false}, 
+			["string"]    	=  { Color(128, 128, 128), false}, 
+			["globals"]     = { Color(180, 180, 260), false},
+			
+			["comment"] 	=  { Color(0, 255, 0), false},
+			["ppcommand"] 	= { Color(240, 240, 160), false},
+			
+			["notfound"]  	= { Color(240,  96,  96), false}, 
+		}
+		
+		--- (Client) Highlight
+		-- TODO (Hard) Do correct multiline things highlightion 
+		-- In fact need to rewrite the whole editor component because of its highlight specialities
+		function SF.Editor.highlighter(self, row)
+			local cols = {}
+			self:ResetTokenizer(row)
+			self:NextCharacter()
+			
+			while self.character do
+				local tokenname = ""
+				self.tokendata = ""
+
+				self:NextPattern(" *")
+				if !self.character then break end
+	
+				-- Numbers
+				if self:NextPattern("^0[xb][0-9A-F]+") then
+					tokenname = "number"
+				elseif self:NextPattern("^[0-9][0-9.e]*") then
+					tokenname = "number"
+					
+				-- Keyword/expression
+				elseif self:NextPattern("^[a-zA-Z0-9_]+") then
+					local sstr = self.tokendata:Trim()
+					
+					if table.HasValue(keywords, sstr) then
+						tokenname = "keyword"
+					elseif SF.DefaultEnvironment[sstr] != nil then
+						tokenname = "globals"
+					else
+						tokenname = "expression"
+					end
+				
+				-- String
+				elseif self.character == "'" or self.character == "\""  then
+					self.stringDelimiter = self.character
+					self:NextCharacter()
+					
+					while self.character and self.character != self.stringDelimiter do
+						if self.character == "\\" then self:NextCharacter() end
+						self:NextCharacter()
+					end
+					self:NextCharacter()
+					
+					tokenname = "string"
+					
+				-- Comment
+				elseif self:NextPattern("^%-%-.*$") then
+					if string.sub(self.tokendata, 1, 3) == "--@" then
+						tokenname = "ppcommand"
+					else
+						tokenname = "comment"
+					end
+					
+				-- Operator
+				else
+					self:NextCharacter()
+					tokenname = "operator"
+				end
+
+				color = colors[tokenname]
+				if #cols > 1 and color == cols[#cols][2] then
+					cols[#cols][1] = cols[#cols][1] .. self.tokendata
+				else
+					cols[#cols + 1] = {self.tokendata, color}
+				end
+			end
+			
+			return cols
 		end
 	end
 else
