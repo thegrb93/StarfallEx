5ff5fa1f97d372ec82e1292fa764a4b4746dc060
Robert Avery <daranable@gmail.com>
2012-04-21 02:02:02 -0500

    Created a provider for client side file writing.

---
+++ lua/starfall/permissions/providers_cl/files.lua
@@ -0,0 +1,31 @@
+--- Starfall file library permission provider
+
+-- start the provider table and set it to inherit from the default provider
+local P = {}
+P.__index = SF.Permissions.Provider
+setmetatable( P, P )
+
+-- localize the result set
+local ALLOW = SF.Permissions.Result.ALLOW
+local DENY = SF.Permissions.Result.DENY
+local NEUTRAL = SF.Permissions.Resulr.NEUTRAL
+
+-- define what permission keys we will allow
+local keys = {
+	["file.read" ] = true
+	["file.write"] = true
+}
+
+function P:check (principal, target, key)
+	if type( target ) ~= "string" then return NEUTRAL end
+	
+	-- allow if the localplayer is trying to write a file to their computer
+	if keys[ key ] and principal == LocalPlayer() then
+		return ALLOW
+	else
+		return NEUTRAL
+	end
+end
+
+-- register the provider
+SF.Permissions.registerProvider( P )
