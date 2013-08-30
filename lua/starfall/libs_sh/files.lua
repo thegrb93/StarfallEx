90eac937d00686bc33d6e592233934a44f96f5a8
Ashley Williamson <ashley@shortmail.me>
2013-08-30 16:23:14 +0100

    [Added] VON Library for table serialization/deserialization [Changed] File library to error correctly [Changed] File library to be linux folder name safe [Changed] File library todo's for when permissions are done

--- lua/starfall/libs_sh/files.lua
+++ lua/starfall/libs_sh/files.lua
@@ -1,4 +1,5 @@
-
+--- TODO: Permissions System before fixing this.
+--- TODO: Add VON encoding of any table's that are passed, work on 'universal' serializer and deserializer
 -------------------------------------------------------------------------------
 -- File functions
 -------------------------------------------------------------------------------
@@ -11,77 +12,77 @@ local files_library, _ = SF.Libraries.Register("files")
 -- @name Access Files Permission
 -- @class table
 -- @field name "Access Files"
--- @field desc "Allows access to data/StarfallScriptData/"
+-- @field desc "Allows access to data/starfallscriptdata/"
 -- @field level 1
 -- @field value True if clientside, false if serverside
 
 SF.Permissions:registerPermission({
 	name = "Access Files",
-	desc = "Allows access to data/StarfallScriptData/",
+	desc = "Allows access to data/starfallscriptdata/",
 	level = 1,
-	value = CLIENT,
+	value = 1,
 })
 
-file.CreateDir("StarfallScriptData/")
+file.CreateDir("starfallscriptdata/")
 
 --- Reads a file from path
--- @param path Filepath relative to data/StarfallScriptData/. Cannot contain '..'
+-- @param path Filepath relative to data/starfallscriptdata/. Cannot contain '..'
 -- @return Contents, or nil if error
 -- @return Error message if applicable
 function files_library.read(path)
 	SF.CheckType(path, "string")
-	if path:find("..",1,true) then return nil, "path contains '..'" end
-	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
-	local contents = file.Read("StarfallScriptData/"..path, "DATA")
-	if contents then return contents else return nil, "file not found" end
+	if path:find("..",1,true) then error("path contains '..'") return end
+	if not SF.instance.permissions:checkPermission("Access Files") then error("access denied") return end
+	local contents = file.Read("starfallscriptdata/"..path, "DATA")
+	if contents then return contents else error("file not found") return end
 end
 
 --- Writes to a file
--- @param path Filepath relative to data/StarfallScriptData/. Cannot contain '..'
+-- @param path Filepath relative to data/starfallscriptdata/. Cannot contain '..'
 -- @return True if OK, nil if error
 -- @return Error message if applicable
 function files_library.write(path, data)
 	SF.CheckType(path, "string")
 	SF.CheckType(data, "string")
-	if path:find("..",1,true) then return nil, "path contains '..'" end
-	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
-	file.Write("StarfallScriptData/"..path, data)
+	if path:find("..",1,true) then error("path contains '..'") return end
+	if not SF.instance.permissions:checkPermission("Access Files") then error("access denied") return end
+	file.Write("starfallscriptdata/"..path, data)
 	return true
 end
 
 --- Appends a string to the end of a file
--- @param path Filepath relative to data/StarfallScriptData/. Cannot contain '..'
+-- @param path Filepath relative to data/starfallscriptdata/. Cannot contain '..'
 -- @param data String that will be appended to the file.
 -- @return Error message if applicable
 function files_library.append(path,data)
 	SF.CheckType(path, "string")
 	SF.CheckType(data, "string")
-	if path:find("..",1,true) then return nil, "path contains '..'" end
-	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
-	file.Append("StarfallScriptData/"..path, data)
+	if path:find("..",1,true) then error("path contains '..'") return end
+	if not SF.instance.permissions:checkPermission("Access Files") then error("access denied") return end
+	file.Append("starfallscriptdata/"..path, data)
 	return true
 end
 
 --- Checks if a file exists
--- @param path Filepath relative to data/StarfallScriptData/. Cannot contain '..'
+-- @param path Filepath relative to data/starfallscriptdata/. Cannot contain '..'
 -- @return True if exists, false if not, nil if error
 -- @return Error message if applicable
 function files_library.exists(path)
 	SF.CheckType(path, "string")
-	if path:find("..",1,true) then return nil, "path contains '..'" end
-	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
-	return file.Exists("StarfallScriptData/"..path, "DATA")
+	if path:find("..",1,true) then error("path contains '..'") return end
+	if not SF.instance.permissions:checkPermission("Access Files") then error("access denied") return end
+	return file.Exists("starfallscriptdata/"..path, "DATA")
 end
 
 --- Deletes a file
--- @param path Filepath relative to data/StarfallScriptData/. Cannot contain '..'
+-- @param path Filepath relative to data/starfallscriptdata/. Cannot contain '..'
 -- @return True if successful, nil if error
 -- @return Error message if applicable
 function files_library.delete(path)
 	SF.CheckType(path, "string")
-	if path:find("..",1,true) then return nil, "path contains '..'" end
-	if not SF.instance.permissions:checkPermission("Access Files") then return nil, "access denied" end
-	if not file.Exists("StarfallScriptData/"..path, "DATA") then return nil, "doesn't exist" end
+	if path:find("..",1,true) then error("path contains '..'") return end
+	if not SF.instance.permissions:checkPermission("Access Files") then error("access denied") return end
+	if not file.Exists("starfallscriptdata/"..path, "DATA") then error("doesn't exist") return end
 	file.Delete(path)
 	return true
 end
