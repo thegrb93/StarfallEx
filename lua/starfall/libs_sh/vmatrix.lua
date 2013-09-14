e851037e1c51279092cc59e7322fdc6550bbcf21
Ashley Williamson <ashley@shortmail.me>
2013-09-14 19:42:41 +0100

    [Finished] VMatrix type functions

--- lua/starfall/libs_sh/vmatrix.lua
+++ lua/starfall/libs_sh/vmatrix.lua
@@ -13,6 +13,21 @@ SF.DefaultEnvironment.Matrix = function()
 	return wrap(Matrix())
 end
 
+function vmatrix_methods:getAngles()
+	SF.CheckType( self, vmatrix_metamethods )
+	return unwrap(self):GetAngles()
+end
+
+function vmatrix_methods:getScale()
+	SF.CheckType( self, vmatrix_metamethods )
+	return unwrap(self):GetScale()
+end
+
+function vmatrix_methods:getTranslation()
+	SF.CheckType( self, vmatrix_metamethods )
+	return unwrap(self):GetTranslation()
+end
+
 function vmatrix_methods:rotate( ang )
 	SF.CheckType( self, vmatrix_metamethods )
 	SF.CheckType( ang, "Angle")
@@ -22,10 +37,49 @@ function vmatrix_methods:rotate( ang )
 
 end
 
+function vmatrix_methods:scale( vec )
+	SF.CheckType( self, vmatrix_metamethods )
+	SF.CheckType( vec, "Vector" )
+
+	local v = unwrap(self)
+	v:Scale( vec )
+end
+
+function vmatrix_methods:scaleTranslation( num )
+	SF.CheckType( self, vmatrix_metamethods )
+	SF.CheckType( num, "Number" )
+
+	local v = unwrap(self)
+	v:ScaleTranslation( num )
+end
+
+function vmatrix_methods:setAngles( ang )
+	SF.CheckType( self, vmatrix_metamethods )
+	SF.CheckType( ang, "Angle" )
+
+	local v = unwrap(self)
+	v:SetAngles( ang )
+end
+
+function vmatrix_methods:setTranslation( vec )
+	SF.CheckType( self, vmatrix_metamethods )
+	SF.CheckType( vec, "Vector" )
+
+	local v = unwrap(self)
+	v:SetTranslation( vec )
+end
+
 function vmatrix_methods:translate( vec )
 	SF.CheckType( self, vmatrix_metamethods )
 	SF.CheckType( vec, "Vector" )
 
 	local v = unwrap(self)
 	v:Translate( vec )
+end
+
+function vmatrix_metamethods:__mul( lhs, rhs )
+	SF.CheckType( lhs, vmatrix_metamethods )
+	SF.CheckType( rhs, vmatrix_metamethods )
+
+	return unwrap(rhs) * unwrap(rhs)
 end
