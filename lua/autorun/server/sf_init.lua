c96be4b0b9b11e43264f0206f06e3a76c89affb1
Xandaros <mz.bremerhaven@googlemail.com>
2014-02-03 21:13:19 +0100

    Add the new model as resource
    Forgot to do that in my last commit

--- lua/autorun/server/sf_init.lua
+++ lua/autorun/server/sf_init.lua
@@ -1,1 +1,8 @@
-AddCSLuaFile("autorun/client/sf_models.lua")
+AddCSLuaFile( "autorun/client/sf_models.lua" )
+
+resource.AddFile( "materials/models/spacecode/glass.vmt" )
+resource.AddFile( "materials/models/spacecode/sfchip.vmt" )
+resource.AddFile( "materials/models/spacecode/sfpcb.vmt" )
+resource.AddFile( "models/spacecode/sfchip.mdl" )
+resource.AddFile( "models/spacecode/sfchip_medium.mdl" )
+resource.AddFile( "models/spacecode/sfchip_small.mdl" )
