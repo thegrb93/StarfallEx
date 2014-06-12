f8cff1e56d9ea02d2cd4ef5a28d4e2b8dca3f6dc
awilliamson <radongaming@gmail.com>
2014-06-12 11:04:31 +0100

    Added Angles Type, libs/sh.
    Ref #157

---
+++ lua/starfall/libs_sh/angles.lua
@@ -0,0 +1,132 @@
+SF.Angles = {}
+
+--- Angle Type
+-- @shared
+local ang_methods, ang_metamethods = SF.Typedef( "Angle" )
+local wrap, unwrap = SF.CreateWrapper( ang_metamethods, true, false, debug.getregistry().Angle )
+
+SF.DefaultEnvironment.Angle = function ( ... )
+	return wrap( Angle( ... ) )
+end
+
+SF.Angles.Wrap = wrap
+SF.Angles.Unwrap = unwrap
+SF.Angles.Methods = ang_methods
+SF.Angles.Metatable = ang_metamethods
+
+--- __newindex metamethod
+function ang_metamethods.__newindex ( t, k, v )
+	if k == "p" or k =="y" or k == "r" then
+		SF.UnwrapObject( t ).__newindex( SF.UnwrapObject( t ), k, v )
+	else
+		rawset( t, k, v )
+	end
+end
+
+local _p = ang_metamethods.__index
+--- __index metamethod
+function ang_metamethods.__index ( t, k )
+	if k == "p" or k =="y" or k == "r" then
+		return unwrap( t )[ k ]
+	end
+	return _p[ k ]
+end
+
+--- tostring metamethod.
+-- @return string representing the angle.
+function ang_metamethods:__tostring ()
+	return unwrap( self ):__tostring()
+end
+
+--- __mul metamethod ang1 * ang2.
+-- @param a Angle to multiply by.
+-- @return resultant angle.
+function ang_metamethods:__mul ( n )
+	SF.CheckType( n, "number" )
+	return SF.WrapObject( unwrap( self ):__mul( n ) )
+end
+
+--- __unm metamethod -ang.
+-- @return resultant angle.
+function ang_metamethods:__unm ()
+	return SF.WrapObject( unwrap( self ):__unm() )
+end
+
+--- __eq metamethod ang1 == ang2.
+-- @param a Angle to check against.
+-- @return bool
+function ang_metamethods:__eq ( a )
+	SF.CheckType( a, SF.Types[ "Angle" ] )
+	return SF.WrapObject( unwrap( self ):__eq( unwrap( a ) ) )
+end
+
+--- __add metamethod ang1 + ang2.
+-- @param a Angle to add.
+-- @return resultant angle.
+function ang_metamethods:__add ( a )
+	SF.CheckType( a, SF.Types[ "Angle" ] )
+	return SF.WrapObject( unwrap( self ):__add( unwrap( a ) ) )
+end
+
+--- __sub metamethod ang1 - ang2.
+-- @param a Angle to subtract.
+-- @return resultant angle.
+function ang_metamethods:__sub ( a )
+	SF.CheckType( a, SF.Types[ "Angle" ] )
+	return SF.WrapObject( unwrap( self ):__sub( unwrap( a ) ) )
+end
+
+
+--- Return the Forward Vector ( direction the angle points ).
+-- @return vector normalised.
+function ang_methods:getForward ()
+	return SF.WrapObject( unwrap( self ):Forward() )
+end
+
+--- Returns if p,y,r are all 0.
+-- @return boolean
+function ang_methods:isZero ()
+	return unwrap( self ):IsZero()
+end
+
+--- Normalise angles eg (0,181,1) -> (0,-179,1).
+-- @return nil
+function ang_methods:normalize ()
+	unwrap( self ):Normalize()
+end
+
+--- Return the Right Vector relative to the angle dir.
+-- @return vector normalised.
+function ang_methods:getRight ()
+	return SF.WrapObject( unwrap( self ):Right() )
+end
+
+--- Rotates the angle around the specified axis by the specified degrees.
+-- @param v Axis
+-- @param r Number of degrees.
+-- @return nil
+function ang_methods:rotateAroundAxis ( v, r )
+	SF.CheckType( v, SF.Types[ "Vector" ] )
+	SF.CheckType( r, "number" )
+	unwrap( self ):RotateAroundAxis( SF.UnwrapObject( v ), r )
+end
+
+--- Copies p,y,r from second angle to the first.
+-- @param a Angle to copy from.
+-- @return nil
+function ang_methods:set ( a )
+	SF.CheckType( a, SF.Types[ "Angle" ] )
+	unwrap( self ):Set( unwrap( a ) )
+end
+
+--- Return the Up Vector relative to the angle dir.
+-- @return vector normalised.
+function ang_methods:getUp ()
+	return SF.WrapObject( unwrap( self ):Up() )
+end
+
+--- Sets p,y,r to 0. This is faster than doing it manually.
+-- @return nil
+function ang_methods:setZero ()
+	unwrap( self ):Zero()
+end
