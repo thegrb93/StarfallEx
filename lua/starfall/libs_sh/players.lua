c7fbf585326e5aa67495d36f5117da0aca2a3e73
Ashley Williamson <11356993@students.lincoln.ac.uk>
2014-08-22 11:28:56 +0100

    Added player getEyeTrace method
    Fixed #245

--- lua/starfall/libs_sh/players.lua
+++ lua/starfall/libs_sh/players.lua
@@ -306,6 +306,14 @@ function player_methods:getUserID ()
 	return ent and ent:UserID()
 end
 
+--- Returns a table with information of what the player is looking at
+-- @shared
+-- @return table trace data
+function player_methods:getEyeTrace ()
+	if not SF.Permissions.check( SF.instance.player, SF.UnwrapObject( self ), "trace" ) then SF.throw( "Insufficient permissions", 2 ) end
+	return SF.Sanitize( SF.UnwrapObject( self ):GetEyeTrace() )
+end
+
 if CLIENT then
 	--- Returns the relationship of the player to the local client
 	-- @return One of: "friend", "blocked", "none", "requested"
