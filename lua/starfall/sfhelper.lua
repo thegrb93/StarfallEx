e0888db0b5fcabf9b5ed6fe39d4af06717b7ff11
Jazzelhawk <jazzelhawk@hotmail.com>
2014-12-24 04:25:41 +0000

    Reminder to change documentation location

--- lua/starfall/sfhelper.lua
+++ lua/starfall/sfhelper.lua
@@ -14,7 +14,7 @@ local settings_set = false
 
 if CLIENT then
 	function helper.getDocs()
-		http.Fetch( "http://jazzelhawk.zapto.org/doc.json", function( body, len, headers, code )
+		http.Fetch( "http://jazzelhawk.zapto.org/doc.json", function( body, len, headers, code ) -- <<-- Change this to official website
 			SF.Docs = util.JSONToTable( body )
 			docs_set = true
 		end, function( error ) print("Starfall failed to load documentation, Error: ", error) end )
