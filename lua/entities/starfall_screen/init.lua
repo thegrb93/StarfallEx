2c3363bb675f847fd60ba5e125560ded4c271c7f
Ashley Williamson <11356993@students.lincoln.ac.uk>
2015-01-01 00:16:10 +0000

    Fixing the missed table.copy from #210 fix.

--- lua/entities/starfall_screen/init.lua
+++ lua/entities/starfall_screen/init.lua
@@ -172,27 +172,6 @@ function ENT:OnRemove()
     screens[ self ] = nil
 end
 
-local function tableCopy ( t, lookup_table )
-	if ( t == nil ) then return nil end
-
-	local copy = {}
-	setmetatable( copy, debug.getmetatable( t ) )
-	for i, v in pairs( t ) do
-		if ( not istable( v ) ) then
-			copy[ i ] = v
-		else
-			lookup_table = lookup_table or {}
-			lookup_table[ t ] = copy
-			if lookup_table[ v ] then
-				copy[ i ] = lookup_table[ v ] -- we already copied this table. reuse the copy.
-			else
-				copy[ i ] = table.Copy( v, lookup_table ) -- not yet copied. copy it.
-			end
-		end
-	end
-	return copy
-end
-
 function ENT:BuildDupeInfo ()
 	local info = self.BaseClass.BuildDupeInfo( self ) or {}
 
