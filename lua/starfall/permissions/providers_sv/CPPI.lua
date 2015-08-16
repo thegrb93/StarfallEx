3e7aecfc5cc1542db7ae516078496d4d4dfab61c
Garrett Brown <grbrown93@sbcglobal.net>
2015-08-16 00:41:42 -0500

    Allow physgun functions to act on players if the owner has permission.

--- lua/starfall/permissions/providers_sv/CPPI.lua
+++ lua/starfall/permissions/providers_sv/CPPI.lua
@@ -46,8 +46,12 @@ function P:check ( principal, target, key )
 		if target:CPPICanTool( principal, "starfall_ent_lib" ) then return ALLOW end
 		return DENY
 	elseif canPhysgun[ key ] then
-		if not IsValid( target:CPPIGetOwner() ) then return DENY end
-		if target:CPPICanPhysgun( principal ) then return ALLOW end
+		if target:IsPlayer() then
+			return (hook.Call( "PhysgunPickup", GAMEMODE, principal, target ) ~= false) and ALLOW or DENY
+		else
+			if not IsValid( target:CPPIGetOwner() ) then return DENY end
+			if target:CPPICanPhysgun( principal ) then return ALLOW end
+		end
 		return DENY
 	end
 
