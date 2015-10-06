eea99d6c18686f707156123123d06933d8a3019e
Garrett Brown <grbrown93@sbcglobal.net>
2015-10-05 20:47:33 -0500

    Fix quotaUsed

--- lua/starfall/libs_sh/builtins.lua
+++ lua/starfall/libs_sh/builtins.lua
@@ -114,7 +114,7 @@ SF.DefaultEnvironment.SERVER = SERVER
 -- If used on screens, will show 0 if only rendering is done. Operations must be done in the Think loop for them to be counted.
 -- @return Current quota used this Think
 function SF.DefaultEnvironment.quotaUsed ()
-	return SF.instance.cpu_current
+	return SF.instance.cpu_total
 end
 
 --- Gets the Average CPU Time in the buffer
