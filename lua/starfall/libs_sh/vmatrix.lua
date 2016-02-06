2b47f788b8eba690ea046d4e5ef2a5b03543c8f1
Garrett Brown <grbrown93@sbcglobal.net>
2016-02-06 16:23:21 -0600

    Optimize matrix and fix matrix crash bug.

--- lua/starfall/libs_sh/vmatrix.lua
+++ lua/starfall/libs_sh/vmatrix.lua
@@ -4,12 +4,24 @@ SF.VMatrix = {}
 --- VMatrix type
 local vmatrix_methods, vmatrix_metamethods = SF.Typedef( "VMatrix" )
 local wrap, unwrap = SF.CreateWrapper( vmatrix_metamethods, true, false, debug.getregistry().VMatrix )
-local vwrap, vunwrap = SF.WrapObject, SF.UnwrapObject
+local vec_meta, vwrap, vunwrap, ang_meta, awrap, aunwrap
+
+SF.Libraries.AddHook("postload", function()
+	vec_meta = SF.Vectors.Metatable
+	vwrap = SF.Vectors.Wrap
+	vunwrap = SF.Vectors.Unwrap
+	
+	ang_meta = SF.Angles.Metatable
+	awrap = SF.Angles.Wrap
+	aunwrap = SF.Angles.Unwrap
+end)
 
 SF.VMatrix.Methods = vmatrix_methods
 SF.VMatrix.Metatable = vmatrix_metamethods
 SF.VMatrix.Wrap = wrap
 SF.VMatrix.Unwrap = unwrap
+
+local dgetmeta = debug.getmetatable
 
 --- Returns a new VMatrix
 -- @return New VMatrix
@@ -26,28 +38,28 @@ end
 --- Returns angles
 -- @return Angles
 function vmatrix_methods:getAngles ()
-	return SF.WrapObject( unwrap( self ):GetAngles() )
+	return awrap( unwrap( self ):GetAngles() )
 end
 
 --- Returns scale
 -- @return Scale
 function vmatrix_methods:getScale ()
-	return SF.WrapObject( unwrap( self ):GetScale() )
+	return vwrap( unwrap( self ):GetScale() )
 end
 
 --- Returns translation
 -- @return Translation
 function vmatrix_methods:getTranslation ()
-	return SF.WrapObject( unwrap( self ):GetTranslation() )
+	return vwrap( unwrap( self ):GetTranslation() )
 end
 
 --- Rotate the matrix
 -- @param ang Angle to rotate by
 function vmatrix_methods:rotate ( ang )
-	SF.CheckType( ang, SF.Types[ "Angle" ] )
+	SF.CheckType( ang, ang_meta )
 
 	local v = unwrap( self )
-	v:Rotate( SF.UnwrapObject( ang ) )
+	v:Rotate( aunwrap( ang ) )
 
 end
 
@@ -72,7 +84,7 @@ end
 --- Scale the matrix
 -- @param vec Vector to scale by
 function vmatrix_methods:scale ( vec )
-	SF.CheckType( vec, SF.Types[ "Vector" ] )
+	SF.CheckType( vec, vec_meta )
 	local vec = vunwrap( vec )
 
 	local v = unwrap( self )
@@ -91,7 +103,7 @@ end
 --- Sets the angles
 -- @param ang New angles
 function vmatrix_methods:setAngles ( ang )
-	SF.CheckType( ang, SF.Types[ "Angle" ] )
+	SF.CheckType( ang, ang_meta )
 
 	local v = unwrap( self )
 	v:SetAngles( SF.UnwrapObject( ang ) )
@@ -100,7 +112,7 @@ end
 --- Sets the translation
 -- @param vec New translation
 function vmatrix_methods:setTranslation ( vec )
-	SF.CheckType( vec, SF.Types[ "Vector" ] )
+	SF.CheckType( vec, vec_meta )
 	local vec = vunwrap( vec )
 
 	local v = unwrap( self )
@@ -110,7 +122,7 @@ end
 --- Translate the matrix
 -- @param vec Vector to translate by
 function vmatrix_methods:translate ( vec )
-	SF.CheckType( vec, SF.Types[ "Vector" ] )
+	SF.CheckType( vec, vec_meta )
 	local vec = vunwrap( vec )
 
 	local v = unwrap( self )
@@ -118,5 +130,13 @@ function vmatrix_methods:translate ( vec )
 end
 
 function vmatrix_metamethods.__mul ( lhs, rhs )
-	return vwrap( vunwrap( lhs ) * vunwrap( rhs ) )
+	SF.CheckType( lhs, vmatrix_metamethods )
+	local rhsmeta = dgetmeta( rhs )
+	if rhsmeta == vmatrix_metamethods then
+		return wrap( unwrap( lhs ) * unwrap( rhs ) )
+	elseif rhsmeta == vec_meta then
+		return vwrap( unwrap( lhs ) * vunwrap( rhs ) )
+	else
+		SF.throw("Matrix must be multiplied with another matrix or vector on right hand side",2)
+	end
 end
