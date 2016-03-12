60458596f92f416353ad2eb21440a43573f7ff32
Garrett Brown <grbrown93@sbcglobal.net>
2016-03-12 16:46:38 -0600

    Make http only work on owner, and remove other restrictions.

--- lua/starfall/libs_cl/http.lua
+++ lua/starfall/libs_cl/http.lua
@@ -3,27 +3,6 @@
 --- Http library. Requests content from urls.
 -- @shared
 local http_library, _ = SF.Libraries.Register( "http" )
-local http_interval = CreateConVar( "sf_http_interval", "0.5", { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Interval in seconds in which one http request can be made" )
-local http_max_active = CreateConVar( "sf_http_max_active", "3", { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "The maximum amount of active http requests at the same time" )
-
--- Initializes the lastRequest variable to a value which ensures that the first call to httpRequestReady returns true
--- and the "active requests counter" to 0
-SF.Libraries.AddHook( "initialize", function( instance )
-	instance.data.http = {
-		lastRequest = 0,
-		active = 0
-	}
-end )
-
--- Returns an error when a http request was already triggered in the current interval
--- or the maximum amount of simultaneous requests is currently active, returns true otherwise
-local function httpRequestReady ( instance )
-	local httpData = instance.data.http
-	if CurTime() - httpData.lastRequest < http_interval:GetFloat() or httpData.active >= http_max_active:GetInt() then
-		SF.throw( "You can't run a new http request yet", 2 )
-	end
-	return true
-end
 
 -- Runs the appropriate callback after a http request
 local function runCallback ( instance, callback, ... )
@@ -36,13 +15,6 @@ local function runCallback ( instance, callback, ... )
 			end
 		end
 	end
-	instance.data.http.active = instance.data.http.active - 1
-end
-
---- Checks if a new http request can be started
-function http_library.canRequest ( )
-	local httpData = SF.instance.data.http
-	return CurTime() - httpData.lastRequest >= http_interval:GetFloat() and httpData.active < http_max_active:GetInt()
 end
 
 --- Runs a new http GET request
@@ -51,15 +23,12 @@ end
 -- @param callbackFail the function to be called on request fail, taking the failing reason as an argument
 function http_library.get ( url, callbackSuccess, callbackFail )
 	local instance = SF.instance
-	
-	httpRequestReady( instance )
+	if instance.player ~= LocalPlayer() then SF.throw( "Http must only be used on the owner of the starfall", 2 ) end
 	
 	SF.CheckType( url, "string" )
 	SF.CheckType( callbackSuccess, "function" )
 	if callbackFail then SF.CheckType( callbackFail, "function" ) end
 	
-	instance.data.http.lastRequest = CurTime()
-	instance.data.http.active = instance.data.http.active + 1
 	http.Fetch( url, function ( body, len, headers, code ) 
 		runCallback( instance, callbackSuccess, body, len, headers, code )
 	end, function ( err )
@@ -74,9 +43,7 @@ end
 -- @param callbackFail the function to be called on request fail, taking the failing reason as an argument
 function http_library.post ( url, params, callbackSuccess, callbackFail )
 	local instance = SF.instance
-	
-	httpRequestReady( instance )
-	
+	if instance.player ~= LocalPlayer() then SF.throw( "Http must only be used on the owner of the starfall", 2 ) end
 	SF.CheckType( url, "string" )
 	
 	if params then
@@ -91,8 +58,6 @@ function http_library.post ( url, params, callbackSuccess, callbackFail )
 	SF.CheckType( callbackSuccess, "function" )	
 	if callbackFail then SF.CheckType( callbackFail, "function" ) end
 	
-	instance.data.http.lastRequest = CurTime()
-	instance.data.http.active = instance.data.http.active + 1
 	http.Post( url, params, function ( body, len, headers, code )
 		runCallback( instance, callbackSuccess, body, len, headers, code )
 	end, function ( err )
