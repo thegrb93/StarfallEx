0d498b064fc5e0a9442d4dcdcfe4ebe115d450a5
Garrett Brown <grbrown93@sbcglobal.net>
2016-05-23 16:46:13 -0500

    Tighten the hud enable/disable logic.

--- lua/entities/starfall_hud/cl_init.lua
+++ lua/entities/starfall_hud/cl_init.lua
@@ -74,22 +74,26 @@ net.Receive( "starfall_hud_set_enabled" , function()
 	if IsValid(ent) then
 		local hook_table = hook.GetTable().HUDPaint
 		local hook_name = hook_pref .. ent:EntIndex()
-		if hook_table[ hook_name ] and ( enable == -1 or enable == 0 ) then
-			hook.Remove("HUDPaint", hook_name)
-			hook.Remove("CalcView", hook_name) 
-			LocalPlayer():ChatPrint("Starfall HUD Disconnected.")
-		else
-			ent:CallOnRemove( "sf_hud_unlink_on_remove", function() 
-				hook.Remove("HUDPaint", hook_name) 
+		if hook_table[ hook_name ] then
+			if ( enable == -1 or enable == 0 ) then
+				hook.Remove("HUDPaint", hook_name)
 				hook.Remove("CalcView", hook_name) 
-			end ) 
-			hook.Add("HUDPaint", hook_name, function() ent:DrawHUD() end)
-			hook.Add("CalcView", hook_name, function(...) return ent:DoCalcView(...) end)
-			if (Hint_FirstPrint) then
-				LocalPlayer():ChatPrint("Starfall HUD Connected. NOTE: Type 'sf_hud_unlink' in the console to disconnect yourself from all HUDs.")
-				Hint_FirstPrint = nil
-			else
-				LocalPlayer():ChatPrint("Starfall HUD Connected.")
+				LocalPlayer():ChatPrint("Starfall HUD Disconnected.")
+			end
+		else
+			if ( enable == -1 or enable == 1 ) then
+				ent:CallOnRemove( "sf_hud_unlink_on_remove", function() 
+					hook.Remove("HUDPaint", hook_name) 
+					hook.Remove("CalcView", hook_name) 
+				end ) 
+				hook.Add("HUDPaint", hook_name, function() ent:DrawHUD() end)
+				hook.Add("CalcView", hook_name, function(...) return ent:DoCalcView(...) end)
+				if (Hint_FirstPrint) then
+					LocalPlayer():ChatPrint("Starfall HUD Connected. NOTE: Type 'sf_hud_unlink' in the console to disconnect yourself from all HUDs.")
+					Hint_FirstPrint = nil
+				else
+					LocalPlayer():ChatPrint("Starfall HUD Connected.")
+				end
 			end
 		end
 	end
