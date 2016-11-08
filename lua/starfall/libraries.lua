78d8848e5c29dd46d088282d821bb73da326255b
Garrett Brown <grbrown93@sbcglobal.net>
2016-11-07 22:18:01 -0600

    Prevent reloaded libraries from adding their hooks again.

--- lua/starfall/libraries.lua
+++ lua/starfall/libraries.lua
@@ -81,3 +81,8 @@ function SF.Libraries.CallHook(hookname, ...)
 		hook[i](...)
 	end
 end
+
+-- Prevent refreshed libraries from re-adding their hooks.
+SF.Libraries.AddHook("postload", function()
+	SF.Libraries.AddHook = function() end
+end)
