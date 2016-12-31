3201ae9e653b68e4f830edd3e43244c4d02a3fa1
Garrett Brown <grbrown93@sbcglobal.net>
2016-12-31 10:45:17 -0600

    Some fixes to input library

--- lua/starfall/libs_sh/input.lua
+++ lua/starfall/libs_sh/input.lua
@@ -36,11 +36,8 @@ if SERVER then return end
 ---- Input library.
 --- @client
 local input_methods, input_metamethods = SF.Libraries.Register( "input" )
-
-do
-	local P = SF.Permissions
-	P.registerPrivilege( "input", "Input", "Allows the user to use the input library" )
-end
+local buttonPolling = CreateConVar( "sf_input", "0", { FCVAR_ARCHIVE },
+	"Whether others' starfall chips that are not yours are allowed to poll your buttons" )
 
 ---- Gets the first key that is bound to the command passed
 --- @param binding The name of the bind
@@ -107,7 +104,7 @@ function input_methods.getCursorPos( )
 end
 
 function CheckButtonPerms(instance, ply, button)
-	if IsFirstTimePredicted() and SF.Permissions.check( instance.player, nil, "input" ) then
+	if (IsFirstTimePredicted() or game.SinglePlayer()) and (ply==LocalPlayer() or buttonPolling:GetBool()) then
 		return true, {button}
 	end
 	return false
