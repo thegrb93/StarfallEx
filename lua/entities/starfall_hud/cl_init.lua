f83d21d6f5660e4b38dd611d0983b9c69fd52865
Garrett Brown <grbrown93@sbcglobal.net>
2017-02-14 22:44:07 -0600

    Cleaned up the hud connection code. Made it easier to see what huds the player is connected to.

--- lua/entities/starfall_hud/cl_init.lua
+++ lua/entities/starfall_hud/cl_init.lua
@@ -56,54 +56,56 @@ function ENT:GetResolution()
 	return ScrW(), ScrH()
 end
 
-
+SF.ConnectedHuds = {}
 local hook_pref = "starfall_hud_hook_"
+local ConnectHUD, DisconnectHUD
+
 local Hint_FirstPrint = true
+function ConnectHUD( ent )
+	local hookname = hook_pref..ent:EntIndex()
+	ent:CallOnRemove( "sf_hud_unlink_on_remove", DisconnectHUD ) 
+	hook.Add("HUDPaint", hookname, function() ent:DrawHUD("render") end)
+	hook.Add("PreDrawOpaqueRenderables", hookname, function(...) ent:DrawHUD("predrawopaquerenderables", ...) end)
+	hook.Add("PostDrawOpaqueRenderables", hookname, function(...) ent:DrawHUD("postdrawopaquerenderables", ...) end)
+	hook.Add("CalcView", hookname, function(...) return ent:DoCalcView(...) end)
+	if (Hint_FirstPrint) then
+		LocalPlayer():ChatPrint("Starfall HUD Connected. NOTE: Type 'sf_hud_unlink' in the console to disconnect yourself from all HUDs.")
+		Hint_FirstPrint = nil
+	else
+		LocalPlayer():ChatPrint("Starfall HUD Connected.")
+	end
+	SF.ConnectedHuds[ent] = true
+end
+
+function DisconnectHUD( ent )
+	local hookname = hook_pref..ent:EntIndex()
+	hook.Remove("HUDPaint", hookname)
+	hook.Remove("PreDrawOpaqueRenderables", hookname)
+	hook.Remove("PostDrawOpaqueRenderables", hookname)
+	hook.Remove("CalcView", hookname)
+	LocalPlayer():ChatPrint("Starfall HUD Disconnected.")
+	SF.ConnectedHuds[ent] = nil
+end
+
 net.Receive( "starfall_hud_set_enabled" , function()
 	local ent = net.ReadEntity()
 	local enable = net.ReadInt(8)
-	if IsValid(ent) then
-		local hook_table = hook.GetTable().HUDPaint
-		local hook_name = hook_pref .. ent:EntIndex()
-		if hook_table[ hook_name ] then
+	if IsValid(ent) then		
+		if SF.ConnectedHuds[ent] then
 			if ( enable == -1 or enable == 0 ) then
-				hook.Remove("HUDPaint", hook_name)
-				hook.Remove("PreDrawOpaqueRenderables", hook_name)
-				hook.Remove("PostDrawOpaqueRenderables", hook_name)
-				hook.Remove("CalcView", hook_name) 
-				LocalPlayer():ChatPrint("Starfall HUD Disconnected.")
+				DisconnectHUD(ent)
 			end
 		else
 			if ( enable == -1 or enable == 1 ) then
-				ent:CallOnRemove( "sf_hud_unlink_on_remove", function() 
-					hook.Remove("HUDPaint", hook_name)
-					hook.Remove("PreDrawOpaqueRenderables", hook_name)
-					hook.Remove("PostDrawOpaqueRenderables", hook_name)
-					hook.Remove("CalcView", hook_name) 
-				end ) 
-				hook.Add("HUDPaint", hook_name, function() ent:DrawHUD("render") end)
-				hook.Add("PreDrawOpaqueRenderables", hook_name, function(...) ent:DrawHUD("predrawopaquerenderables", ...) end)
-				hook.Add("PostDrawOpaqueRenderables", hook_name, function(...) ent:DrawHUD("postdrawopaquerenderables", ...) end)
-				hook.Add("CalcView", hook_name, function(...) return ent:DoCalcView(...) end)
-				if (Hint_FirstPrint) then
-					LocalPlayer():ChatPrint("Starfall HUD Connected. NOTE: Type 'sf_hud_unlink' in the console to disconnect yourself from all HUDs.")
-					Hint_FirstPrint = nil
-				else
-					LocalPlayer():ChatPrint("Starfall HUD Connected.")
-				end
+				ConnectHUD(ent)
 			end
 		end
 	end
 end)
 
 concommand.Add("sf_hud_unlink",function()
-	local hook_table = hook.GetTable().HUDPaint
-	for k, v in pairs(hook_table) do
-		if k:sub(1, #hook_pref) == hook_pref then
-			hook.Remove("HUDPaint", k)
-		end
+	for ent, _ in pairs(SF.ConnectedHuds) do
+		DisconnectHUD(ent) --Should be valid or something horrible has happened.
 	end
 	LocalPlayer():ChatPrint("Disconnected from all Starfall HUDs.")
 end)
-
-
