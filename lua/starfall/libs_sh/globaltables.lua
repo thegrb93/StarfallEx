02aaa8d2b21440104de442deef9b873aa4d74f79
Garrett Brown <grbrown93@sbcglobal.net>
2017-02-25 20:45:32 -0600

    Fixed globaltables

--- lua/starfall/libs_sh/globaltables.lua
+++ lua/starfall/libs_sh/globaltables.lua
@@ -4,29 +4,35 @@
 local gtables_library = SF.Libraries.Register("globaltables")
 
 SF.GlobalTables = {}
-
 SF.GlobalTables.Global = {}
 SF.GlobalTables.Players = {}
+
+SF.Libraries.AddHook("initialize",function(inst)
+	if not SF.GlobalTables.Players[inst.player] then
+		SF.GlobalTables.Players[inst.player] = {}
+	end
+	inst.env.globaltables.global = SF.GlobalTables.Global
+	inst.env.globaltables.player = SF.GlobalTables.Players[inst.player]
+end)
+
+SF.Libraries.AddHook("deinitialize", function(inst)
+	local clean = true
+	for _, inst2 in pairs(SF.allInstances) do
+		if inst~=inst2 then
+			clean = false
+			break
+		end
+	end
+	if clean then
+		SF.GlobalTables.Players[inst.player] = nil
+	end
+end)
 
 --- Global table shared by all instances on the same side.
 -- @name gtables_library.global
 -- @class table
-gtables_library.global = SF.GlobalTables.Global
 
 --- Player-unique global table.
 -- @name gtables_library.player
 -- @class table
 
-hook.Add("PlayerInitialSpawn", "SF_GlobalTables_cn", function(ply)
-	SF.GlobalTables.Players[ply] = {}
-end)
-
-hook.Add("PlayerDisconnected", "SF_GlobalTables_dc", function(ply)
-	SF.GlobalTables.Players[ply] = nil
-end)
-
-setmetatable(gtables_library, {__index = function(k)
-	if k == "player" then
-		return SF.GlobalTables.Players[SF.instance.player]
-	end
-end } )
