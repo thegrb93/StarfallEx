f402824a11b7e0e80a931671e99150b2d5614845
Garrett Brown <grbrown93@sbcglobal.net>
2017-03-24 00:41:05 -0500

    Added file.open and binary file methods.

--- lua/starfall/libs_cl/file.lua
+++ lua/starfall/libs_cl/file.lua
@@ -6,15 +6,34 @@
 -- @client
 local file_library = SF.Libraries.Register( "file" )
 
+--- File type
+-- @client
+local file_methods, file_metamethods = SF.Typedef( "File" )
+local wrap, unwrap = SF.CreateWrapper( file_metamethods, true, false )
+
 -- Register privileges
 do
 	local P = SF.Permissions
 	P.registerPrivilege( "file.read", "Read files", "Allows the user to read files from data/sf_filedata directory", {["Client"] = {default = 1}} )
 	P.registerPrivilege( "file.write", "Write files", "Allows the user to write files to data/sf_filedata directory", {["Client"] = {default = 1}} )
 	P.registerPrivilege( "file.exists", "File existence check", "Allows the user to determine whether a file in data/sf_filedata exists", {["Client"] = {default = 1}} )
+	P.registerPrivilege( "file.open", "Get a file object", "Allows the user to use a file object", {["Client"] = {default = 1}} )
 end
 
 file.CreateDir( "sf_filedata/" )
+
+--- Opens and returns a file
+-- @param path Filepath relative to data/sf_filedata/. Cannot contain '..'
+-- @param mode The file mode to use. See lua manual for explaination
+-- @return File object or nil if it failed
+function file_library.open ( path, mode )
+	SF.Permissions.check( SF.instance.player, path, "file.open" )
+	SF.CheckType( path, "string" )
+	SF.CheckType( mode, "string" )
+	if path:find( "..", 1, true ) then SF.throw( "path contains '..'", 2 ) return end
+	local f = file.Open( "sf_filedata/" .. path, mode, "DATA" )
+	if f then return wrap(f) else SF.throw( "Failed to open file", 2 ) return end
+end
 
 --- Reads a file from path
 -- @param path Filepath relative to data/sf_filedata/. Cannot contain '..'
@@ -100,3 +119,161 @@ function file_library.find ( path, sorting )
 	if path:find( "..", 1, true ) then SF.throw( "path contains '..'", 2 ) return end
 	return file.Find( "sf_filedata/" .. path, "DATA", sorting )
 end
+
+--- Wait until all changes to the file are complete
+function file_methods:flush()
+	SF.CheckType( self, file_metamethods )
+	unwrap(self):Flush()
+end
+
+--- Flushes and closes the file. The file must be opened again to use a new file object.
+function file_methods:close()
+	SF.CheckType( self, file_metamethods )
+	unwrap(self):Close()
+end
+
+--- Sets the file position
+-- @param n The position to set it to
+function file_methods:seek(n)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( n, "number" )
+	unwrap(self):Seek(n)
+end
+
+--- Moves the file position relative to its current position
+-- @param n How much to move the position
+-- @return The resulting position
+function file_methods:skip(n)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( n, "number" )
+	return unwrap(self):Skip(n)
+end
+
+--- Returns the current file position
+-- @return The current file position
+function file_methods:tell()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):Tell()
+end
+
+--- Returns the file's size in bytes
+-- @return The file's size
+function file_methods:size()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):Size()
+end
+
+--- Reads a certain length of the file's bytes
+-- @param n The length to read
+-- @return The data
+function file_methods:read(n)
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):Read(n)
+end
+
+--- Reads a boolean and advances the file position
+-- @return The data
+function file_methods:readBool()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadBool()
+end
+
+--- Reads a byte and advances the file position
+-- @return The data
+function file_methods:readByte()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadByte()
+end
+
+--- Reads a double and advances the file position
+-- @return The data
+function file_methods:readDouble()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadDouble()
+end
+
+--- Reads a float and advances the file position
+-- @return The data
+function file_methods:readFloat()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadFloat()
+end
+
+--- Reads a line and advances the file position
+-- @return The data
+function file_methods:readLine()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadLine()
+end
+
+--- Reads a long and advances the file position
+-- @return The data
+function file_methods:readLong()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadLong()
+end
+
+--- Reads a short and advances the file position
+-- @return The data
+function file_methods:readShort()
+	SF.CheckType( self, file_metamethods )
+	return unwrap(self):ReadShort()
+end
+
+--- Writes a string to the file and advances the file position
+-- @param str The data to write
+function file_methods:write(str)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( str, "string" )
+	unwrap(self):Write(str)
+end
+
+--- Writes a boolean and advances the file position
+-- @param x The boolean to write
+function file_methods:writeBool(x)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( x, "boolean" )
+	unwrap(self):WriteBool(x)
+end
+
+--- Writes a byte and advances the file position
+-- @param x The byte to write
+function file_methods:writeByte(x)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( x, "number" )
+	unwrap(self):WriteByte(x)
+end
+
+--- Writes a double and advances the file position
+-- @param x The double to write
+function file_methods:writeDouble(x)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( x, "number" )
+	unwrap(self):WriteDouble(x)
+end
+
+--- Writes a float and advances the file position
+-- @param x The float to write
+function file_methods:writeFloat(x)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( x, "number" )
+	unwrap(self):WriteFloat(x)
+end
+
+--- Writes a long and advances the file position
+-- @param x The long to write
+function file_methods:writeLong(x)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( x, "number" )
+	unwrap(self):WriteLong(x)
+end
+
+--- Writes a short and advances the file position
+-- @param x The short to write
+function file_methods:writeShort(x)
+	SF.CheckType( self, file_metamethods )
+	SF.CheckType( x, "number" )
+	unwrap(self):WriteShort(x)
+end
+
+
