929b651fa0735e04ec237cf76825c8f2c342ee42
Damianu <xdamianu@gmail.com>
2017-08-22 22:32:01 +0200

    prop.createComponent (#289)
    * Added prop.createComponent
    
    Closes #245
    
    * Whitespaces fix for prop.lua
    
    * Added model check to prop.createComponent
    
    * Update prop.lua

--- lua/starfall/libs_sv/prop.lua
+++ lua/starfall/libs_sv/prop.lua
@@ -12,7 +12,7 @@ SF.Props.personalquota = CreateConVar("sf_props_personalquota", "-1", { FCVAR_AR
 
 SF.Props.burstmax = CreateConVar("sf_props_burstmax", "4", { FCVAR_ARCHIVE, FCVAR_REPLICATED },
 	"The number of props allowed to spawn in a short interval of time via Starfall scripts for a single instance ( burst )")
-	
+
 SF.Props.burstrate = CreateConVar("sf_props_burstrate", "4", { FCVAR_ARCHIVE, FCVAR_REPLICATED },
 	"The rate at which the burst regenerates per second.")
 
@@ -40,7 +40,7 @@ SF.Libraries.AddHook("deinitialize", function(inst)
 			end
 		end
 	end
-	
+
 	inst.data.props.props = nil
 end)
 
@@ -66,7 +66,7 @@ end
 -- @server
 -- @return The prop object
 function props_library.create (pos, ang, model, frozen)
-	
+
 	SF.Permissions.check(SF.instance.player,  nil, "prop.create")
 
 	SF.CheckType(pos, SF.Types["Vector"])
@@ -85,31 +85,31 @@ function props_library.create (pos, ang, model, frozen)
 
 	local propdata = instance.data.props
 	local propent = ents.Create("prop_physics")
-	
+
 	propent:CallOnRemove("starfall_prop_delete", propOnDestroy, propdata, instance.player)
 	SF.setPos(propent, pos)
 	SF.setAng(propent, ang)
 	propent:SetModel(model)
 	propent:Spawn()
-	
+
 	if not propent:GetModel() then propent:Remove() return end
-	
+
 	for I = 0,  propent:GetPhysicsObjectCount() - 1 do
 		local obj = propent:GetPhysicsObjectNum(I)
 		if obj:IsValid() then
 			obj:EnableMotion(not frozen)
 		end
 	end
-	
+
 	if propdata.undo then
 		undo.Create("Prop")
 			undo.SetPlayer(instance.player)
 			undo.AddEntity(propent)
 		undo.Finish("Prop (" .. tostring(model) .. ")")
 	end
-	
+
 	instance.player:AddCleanup("props", propent)
-	
+
 	gamemode.Call("PlayerSpawnedProp", instance.player, model, propent)
 	FixInvalidPhysicsObject(propent)
 
@@ -117,15 +117,91 @@ function props_library.create (pos, ang, model, frozen)
 
 	propdata.props[prop] = prop
 	plyCount[instance.player] = plyCount[instance.player] + 1
-	
+
 	return prop
 end
 
+local allowed_components = {
+	["starfall_screen"] = true,
+	["starfall_hud"] = true,
+}
+--- Creates starfall component.\n Allowed components:\n starfall_hud\n starfall_screen
+-- @param pos Position of created component
+-- @param ang Angle of created component
+-- @param class Class of created component
+-- @param model Model of created component
+-- @param frozen True to spawn frozen
+-- @server
+-- @return Component entity
+function props_library.createComponent (pos, ang, class, model, frozen)
+	SF.Permissions.check(SF.instance.player,  nil, "prop.create")
+	SF.CheckType(pos, SF.Types["Vector"])
+	SF.CheckType(ang, SF.Types["Angle"])
+	SF.CheckType(class, "string")
+
+	if not allowed_components[class] then return SF.Throw("Invalid class!", 1) end
+
+	local pos = vunwrap(pos)
+	local ang = SF.Angles.Unwrap(ang)
+
+	local instance = SF.instance
+	local propdata = instance.data.props
+
+	if not instance.player:CheckLimit("starfall_components") then SF.Throw("Limit of components reached!", 2) end
+	if not instance.data.props.burst:use(1) then return SF.Throw("Can't spawn props that often", 2) end
+	if personal_max_reached(instance) then return SF.Throw("Can't spawn props, maximum personal limit of " .. SF.Props.personalquota:GetInt() .. " has been reached", 2) end
+	if not gamemode.Call("PlayerSpawnProp", instance.player, model) then return end
+
+	local comp = ents.Create(class)
+
+	comp:CallOnRemove("starfall_prop_delete", propOnDestroy, propdata, instance.player)
+
+	comp:SetAngles(ang)
+	comp:SetPos(pos)
+	comp:SetModel(model)
+	comp:Spawn()
+
+	local mdl = comp:GetModel()
+	if not mdl or mdl == "models/error.mdl" then
+		comp:Remove()
+		return SF.Throw("Wrong model!", 1)
+	end
+
+	for I = 0,  comp:GetPhysicsObjectCount() - 1 do
+		local obj = comp:GetPhysicsObjectNum(I)
+		if obj:IsValid() then
+			obj:EnableMotion(not frozen)
+		end
+	end
+
+	if propdata.undo then
+		undo.Create(class)
+			undo.SetPlayer(instance.player)
+			undo.AddEntity(comp)
+		undo.Finish("Prop (" .. tostring(model) .. ")")
+	end
+
+	instance.player:AddCount("starfall_components", comp)
+	instance.player:AddCleanup("starfall_components", comp)
+
+	local prop = SF.Entities.Wrap(comp)
+
+	propdata.props[prop] = prop
+	plyCount[instance.player] = plyCount[instance.player] + 1
+
+	return prop
+
+end
+
 --- Creates a sent.
+-- @param pos Position of created sent
+-- @param ang Angle of created sent
+-- @param class Class of created sent
+-- @param frozen True to spawn frozen
 -- @server
 -- @return The sent object
 function props_library.createSent (pos, ang, class, frozen)
-	
+
 	SF.Permissions.check(SF.instance.player,  nil, "prop.create")
 
 	SF.CheckType(pos, SF.Types["Vector"])
@@ -152,12 +228,12 @@ function props_library.createSent (pos, ang, class, frozen)
 	if swep then
 
 		if ((not swep.Spawnable and not instance.player:IsAdmin()) or
-			  (swep.AdminOnly and not instance.player:IsAdmin())) then return end
+				(swep.AdminOnly and not instance.player:IsAdmin())) then return end
 		if (not gamemode.Call("PlayerSpawnSWEP", instance.player, class, swep)) then return end
 
 
 		entity = ents.Create(swep.ClassName)
-		
+
 		hookcall = "PlayerSpawnedSWEP"
 
 	elseif sent then
@@ -205,10 +281,10 @@ function props_library.createSent (pos, ang, class, frozen)
 		if (not gamemode.Call("PlayerSpawnVehicle", instance.player, vehicle.Model, vehicle.Class, vehicle)) then return end
 
 		entity = ents.Create(vehicle.Class)
-		
+
 		if IsValid(entity) then
 			entity:SetModel(vehicle.Model)
-			if (vehicle.Model == "models/buggy.mdl") then 
+			if (vehicle.Model == "models/buggy.mdl") then
 				entity:SetKeyValue("vehiclescript", "scripts/vehicles/jeep_test.txt")
 			end
 			if (vehicle.Model == "models/vehicle.mdl") then
@@ -235,7 +311,7 @@ function props_library.createSent (pos, ang, class, frozen)
 				table.Merge(entity, vehicle.Members)
 				duplicator.StoreEntityModifier(entity, "VehicleMemDupe", vehicle.Members)
 			end
-			
+
 			if (entity.SetVehicleClass) then entity:SetVehicleClass(class) end
 			entity.VehicleName = class
 			entity.VehicleTable = vehicle
@@ -246,38 +322,38 @@ function props_library.createSent (pos, ang, class, frozen)
 		hookcall = "PlayerSpawnedVehicle"
 
 	end
-	
+
 	if (IsValid(entity)) then
-		
+
 		entity:CallOnRemove("starfall_prop_delete", propOnDestroy, propdata, instance.player)
-		
+
 		SF.setPos(entity, pos)
 		SF.setAng(entity, ang)
-		
+
 		entity:Spawn()
 		entity:Activate()
-		
+
 		local phys = entity:GetPhysicsObject()
 		if phys:IsValid() then
 			phys:EnableMotion(not frozen)
 		end
-		
+
 		if propdata.undo then
 			undo.Create("SF")
 				undo.SetPlayer(instance.player)
 				undo.AddEntity(entity)
 			undo.Finish("SF (" .. class .. ")")
 		end
-		
+
 		instance.player:AddCleanup("props", entity)
 		gamemode.Call(hookcall, instance.player, entity)
-	
+
 		local wrapped = SF.WrapObject(entity)
 
 		propdata.props[wrapped] = wrapped
 
 		plyCount[instance.player] = plyCount[instance.player] + 1
-	
+
 		return wrapped
 	end
 end
@@ -288,10 +364,10 @@ end
 function props_library.canSpawn ()
 
 	if not SF.Permissions.hasAccess(SF.instance.player,  nil, "prop.create") then return false end
-	
+
 	local instance = SF.instance
 	return not personal_max_reached(instance) and instance.data.props.burst:check()>1
-	
+
 end
 
 --- Checks how many props can be spawned
@@ -300,12 +376,12 @@ end
 function props_library.propsLeft ()
 
 	if not SF.Permissions.hasAccess(SF.instance.player,  nil, "prop.create") then return 0 end
-	
+
 	local instance = SF.instance
-	
+
 	if SF.Props.personalquota:GetInt() < 0 then return -1 end
 	return math.min(SF.Props.personalquota:GetInt() - plyCount[instance.player], instance.data.props.burst)
-	
+
 end
 
 --- Returns how many props per second the user can spawn
@@ -314,7 +390,7 @@ end
 function props_library.spawnRate ()
 
 	return SF.Props.burstrate:GetFloat() or 4
-	
+
 end
 
 --- Sets whether the chip should remove created props when the chip is removed
