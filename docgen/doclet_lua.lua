d86978d34a12a93d63eae8ded7388f2e999e61dd
Damianu <xdamianu@gmail.com>
2017-12-14 04:03:58 +0100

    More cleanups

--- docgen/doclet_lua.lua
+++ docgen/doclet_lua.lua
@@ -1,24 +1,22 @@
 
 local lfs = require"lfs"
-local json = require"json"
 package.path = package.path .. ";../table_show.lua"
 
-local showtable = require("table_show") 
+local showtable = require("table_show")
 doclet_dump = {}
 function cleanup(doc)
 	doc["code"] = nil
 	doc["comment"] = nil
+	doc["files"] = nil
+	doc["examples"] = nil
  	for k, v in pairs(doc) do
 		if type(v) == "table" then
 			cleanup(v)
 		end
 	end
-end 
+end
 function doclet_dump.start(doc)
 	local docout = assert(lfs.open(doclet_dump.options.output_dir.."/docs.lua", "w"))
-	print(doclet_dump.options.output_dir.."/docs.lua")
-	doc["files"] = nil
-	doc["examples"] = nil
 	cleanup(doc)
 	docout:write(showtable(doc, "SF.Docs"))
 	docout:close()
