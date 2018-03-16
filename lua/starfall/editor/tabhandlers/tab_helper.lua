3f076bd5ea2a4686a1b864b5d09ab6ab68123268
Damianu <xdamianu@gmail.com>
2018-03-16 13:31:31 +0100

    Fixed casing and removed unused code (#370)

--- lua/starfall/editor/tabhandlers/tab_helper.lua
+++ lua/starfall/editor/tabhandlers/tab_helper.lua
@@ -8,10 +8,10 @@ local PANEL = {} -- It's our VGUI
 -- Handler part (Tab Handler)
 -------------------------------
 
-function TabHandler:init() -- It's caled when editor is initalized, you can create library map there etc
+function TabHandler:Init() -- It's caled when editor is initalized, you can create library map there etc
 end
 
-function TabHandler:registerSettings() -- Setting panels should be registered there
+function TabHandler:RegisterSettings() -- Setting panels should be registered there
 
 end
 
@@ -38,7 +38,7 @@ local function htmlSetup(old, new)
 	end
 end
 
-function TabHandler:registerTabMenu(menu, content)
+function TabHandler:RegisterTabMenu(menu, content)
 	menu:AddOption("Undock",function()
 
 
@@ -46,7 +46,7 @@ function TabHandler:registerTabMenu(menu, content)
 	end)
 end
 
-function TabHandler:cleanup() -- Called when editor is reloaded/removed
+function TabHandler:Cleanup() -- Called when editor is reloaded/removed
 end
 
 
@@ -99,11 +99,11 @@ function PANEL:Undock()
 	self:CloseTab()
 end
 
-function PANEL:getCode() -- Return name of hanlder or code if it's editor
+function PANEL:GetCode() -- Return name of hanlder or code if it's editor
 	return "@name "..(self.title or "StarfallEx Reference")
 end
 
-function PANEL:setCode()
+function PANEL:SetCode()
 
 end
 
@@ -111,7 +111,7 @@ function PANEL:OnFocusChanged(gained) -- When this tab is opened
 
 end
 
-function PANEL:validate(movecarret) -- Validate request, has to return success,message
+function PANEL:Validate(movecarret) -- Validate request, has to return success,message
 
 end
 --------------
