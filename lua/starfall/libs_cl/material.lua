5c7cf7bf79e265fa3c4ae41a3e88d9f3fd105ee7
Garrett Brown <grbrown93@sbcglobal.net>
2018-05-11 19:51:56 -0500

    Made html panels use a resource manager since their textures aren't freed

--- lua/starfall/libs_cl/material.lua
+++ lua/starfall/libs_cl/material.lua
@@ -60,15 +60,37 @@ local material_bank = SF.ResourceHandler(cv_max_materials:GetInt(),
 	end,
 	FindMetaTable("IMaterial").GetShader
 )
+local html_panel_bank = SF.ResourceHandler(cv_max_materials:GetInt(),
+	function()
+		local Panel = vgui.Create("DHTML")
+		Panel:SetSize(1024, 1024)
+		Panel:SetAlpha(0)
+		Panel:SetMouseInputEnabled(false)
+		Panel:SetHTML(
+[[<html style="overflow:hidden"><body><script>
+var img = new Image();
+img.onload = function (){
+	sf.imageLoaded(img.width, img.height);
+}
+document.body.appendChild(img);
+</script></body></html>]]
+		)
+		Panel:Hide()
+		return Panel
+	end,
+	function() return "HTMLPanel" end
+)
 
 cvars.AddChangeCallback( "sf_render_maxusermaterials", function()
 	material_bank.max = cv_max_materials:GetInt()
+	html_panel_bank.max = cv_max_materials:GetInt()
 end )
 
 -- Register functions to be called when the chip is initialised and deinitialised
 SF.AddHook("initialize", function (inst)
 	inst.data.material = {
-		usermaterials = {}
+		usermaterials = {},
+		htmlpanels = {}
 	}
 end)
 
@@ -76,6 +98,11 @@ SF.AddHook("deinitialize", function (inst)
 	for k, v in pairs(inst.data.material.usermaterials) do
 		material_bank:free(inst.player, v)
 		inst.data.material.usermaterials[k] = nil
+	end
+	for k, v in pairs(inst.data.material.htmlpanels) do
+		html_panel_bank:free(inst.player, k)
+		k:Hide()
+		inst.data.material.htmlpanels[k] = nil
 	end
 end)
 
@@ -103,10 +130,12 @@ function material_library.create(name, shader, keyvalues)
 	checkluatype(shader, TYPE_STRING)
 	local instance = SF.instance
 	checkpermission(instance, nil, "material.create")
+	if instance.data.material.usermaterials[name] then SF.Throw("The material name \""..name.."\" is already taken.", 2) end
 	if not allowed_shaders[shader] then SF.Throw("Tried to use unsupported shader: "..shader, 2) end
 	local m = material_bank:use(instance.player, shader)
 	if not m then SF.Throw("Exceeded the maximum user materials", 2) end
-	return wrap(m)
+	instance.data.material.usermaterials[name] = m
+	return lwrap(m)
 end
 
 --- Gets a previously created material
@@ -116,7 +145,7 @@ function material_library.get(name)
 	return wrap(SF.instance.data.material.usermaterials[name])
 end
 
-local image_params = {["vertexlitgeneric"] = true,["nocull"] = true,["alphatest"] = true,["mips"] = true,["noclamp"] = true,["smooth"] = true}
+local image_params = {["nocull"] = true,["alphatest"] = true,["mips"] = true,["noclamp"] = true,["smooth"] = true}
 --- Creates a .jpg or .png material from file
 -- @param path The path to the image file
 -- @param params The shader parameters to apply to the material. See http://wiki.garrysmod.com/page/Material_Parameters
@@ -137,93 +166,46 @@ function material_library.createFromImage(path, params)
 end
 
 local LoadingTextureQueue = {}
-local Panel
 local function NextInTextureQueue()
 	local requestTbl = LoadingTextureQueue[1]
 	if requestTbl then
 		if requestTbl.Instance.error then
+			-- Chip already deinitialized so don't need to free anything
 			table.remove(LoadingTextureQueue, 1)
 			NextInTextureQueue()
 			return
 		end
-		if Panel then
-			Panel:RunJavascript("img.src = \""..requestTbl.Url.."\";")
-		else
-			Panel = vgui.Create("DHTML")
-			Panel:SetSize(1024, 1024)
-			Panel:SetAlpha(0)
-			Panel:SetMouseInputEnabled(false)
-			Panel:AddFunction("sf", "imageLoaded", function(w,h)
-				timer.Simple(0.2, function()
-					if not requestTbl.Instance.error then
-						local tex = Panel:GetHTMLMaterial():GetTexture("$basetexture")
-						requestTbl.Material:SetTexture("$basetexture", tex)
-						if requestTbl.cb then
-							requestTbl.Instance:runFunction(requestTbl.cb, requestTbl.Tbl, requestTbl.Url)
-						end
-					end
-					table.remove(LoadingTextureQueue, 1)
-					NextInTextureQueue()
-				end)
-			end)
-			Panel:SetHTML([[<html style="overflow:hidden"><head><script>
-var img = new Image();
-img.onload = function (){
-	sf.imageLoaded(img.width, img.height);
-}
-img.src = "]]..requestTbl.Url..[[";
-</script></head><body></body></html>]])
+
+		local Panel = html_panel_bank:use(requestTbl.Instance.player, "HTMLPanel")
+
+		local function applyTexture()
+			if not requestTbl.Instance.error then
+				local mat = Panel:GetHTMLMaterial()
+				if not mat then timer.Simple(0.01, applyTexture) return end
+				requestTbl.Material:SetTexture(requestTbl.Target, mat:GetTexture("$basetexture"))
+				if requestTbl.cb then
+					requestTbl.Instance:runFunction(requestTbl.cb, requestTbl.Tbl, requestTbl.Url)
+				end
+			end
+			table.remove(LoadingTextureQueue, 1)
+			NextInTextureQueue()
 		end
+
+		Panel:AddFunction("sf", "imageLoaded", applyTexture)
+		Panel:RunJavascript([[img.src = "]] .. requestTbl.Url .. [[";]])
+		Panel:Show()
+		requestTbl.Instance.data.material.htmlpanels[Panel] = true
+
 		timer.Create("SF_URLTextureTimeout", 10, 1, function()
+			html_panel_bank:free(requestTbl.Instance.player, Panel)
+			requestTbl.Instance.data.material.htmlpanels[Panel] = nil
+			Panel:Hide()
 			table.remove(LoadingTextureQueue, 1)
 			NextInTextureQueue()
 		end)
-	elseif Panel then
-		Panel:Remove()
-		Panel = nil
-	end
-end
-
---- Creates a material from a url
--- @param name The name of the material
--- @param callback The function called when the material finishes loading.
-local function OnlineTexture(mat, umat, url, prefix, callback)
-	local instance = SF.instance
-
-	if #url > cv_max_data_material_size:GetInt() then
-		SF.Throw("Texture URL/Data too long!", 3)
-	end
-
-	if prefix=="http" or prefix=="https" then
-		checkpermission (instance, url, "material.urlcreate")
-		if #url>2000 then SF.Throw("URL is too long!", 3) end
-		url = string.gsub(url, "[^%w _~%.%-/:]", function(str)
-			return string.format("%%%02X", string.byte(str))
-		end)
-		SF.HTTPNotify(instance.player, url)
 	else
-		checkpermission (instance, nil, "material.datacreate")
-		url = string.match(url, "data:image/[%w%+]+;base64,[%w/%+%=]+") -- No $ at end etc so there can be cariage return etc, we'll skip that part anyway
-		if not url then
-			SF.Throw("Texture data isnt proper base64 encoded image.", 3)
-		end
+		timer.Remove("SF_URLTextureTimeout")
 	end
-
-	-- if alignment then
-		-- checkluatype (alignment, TYPE_STRING)
-		-- local args = string.Split(alignment, " ")
-		-- local validargs = { ["left"] = true, ["center"] = true, ["right"] = true, ["top"] = true, ["bottom"] = true }
-		-- if #args ~= 1 and #args ~= 2 then SF.Throw("Invalid urltexture alignment given.") end
-		-- for i = 1, #args do
-			-- if not validargs[args[i]] then SF.Throw("Invalid urltexture alignment given.") end
-		-- end
-	-- else
-		-- alignment = "center"
-	-- end
-
-	local inqueue = #LoadingTextureQueue
-	LoadingTextureQueue[inqueue + 1] = { Instance = instance, Tbl = mat, Material = umat, Url = url, Alignment = alignment, cb = cb }
-	if inqueue == 0 then timer.Simple(0, NextInTextureQueue) end
 end
 
 --- Returns the material's engine name
@@ -361,13 +343,13 @@ function material_methods:getTexture(key)
 	checktype(self, material_metamethods)
 	checkluatype(key, TYPE_STRING)
 	local tex = unwrap(self):GetTexture(key)
-	if tex then return tex:GetName() end 
+	if tex then return tex:GetName() end
 end
 function lmaterial_methods:getTexture(key)
 	checktype(self, lmaterial_metamethods)
 	checkluatype(key, TYPE_STRING)
 	local tex = lunwrap(self):GetTexture(key)
-	if tex then return tex:GetName() end 
+	if tex then return tex:GetName() end
 end
 
 --- Returns a vector keyvalue
@@ -446,20 +428,45 @@ end
 
 --- Sets a texture keyvalue
 -- @param key The key name to set. $basetexture is the key name for most purposes.
--- @param v The texture name to set it to
+-- @param v The texture name to set it to.
 function material_methods:setTexture(key, v)
 	checktype(self, material_metamethods)
 	checkluatype(key, TYPE_STRING)
 	checkluatype(v, TYPE_STRING)
-	
-	local m = unwrap(self)
-	
-	local _1, _2, prefix = string.find(v, "^(%w-):")
-	if prefix=="http" or prefix=="https" or prefix == "data" then
-		OnlineTexture(self, m, v, prefix, cb, alignment, skip_hack)
-	else
-		m:SetTexture(key, v)
+
+	unwrap(self):SetTexture(key, v)
+end
+
+function material_methods:setTextureURL(key, url, cb, x, y, w, h)
+	checktype(self, material_metamethods)
+	checkluatype(key, TYPE_STRING)
+	checkluatype(url, TYPE_STRING)
+
+	if #url > cv_max_data_material_size:GetInt() then
+		SF.Throw("Texture URL/Data too long!", 2)
 	end
+
+	local instance = SF.instance
+	local _1, _2, prefix = string.find(url, "^(%w-):")
+	if prefix=="http" or prefix=="https" then
+		checkpermission (instance, url, "material.urlcreate")
+		if #url>2000 then SF.Throw("URL is too long!", 2) end
+		url = string.gsub(url, "[^%w _~%.%-/:]", function(str)
+			return string.format("%%%02X", string.byte(str))
+		end)
+		SF.HTTPNotify(instance.player, url)
+	else
+		checkpermission (instance, nil, "material.datacreate")
+		url = string.match(url, "data:image/[%w%+]+;base64,[%w/%+%=]+") -- No $ at end etc so there can be cariage return etc, we'll skip that part anyway
+		if not url then
+			SF.Throw("Texture data isnt proper base64 encoded image.", 2)
+		end
+	end
+
+	local inqueue = #LoadingTextureQueue
+	LoadingTextureQueue[inqueue + 1] = { Instance = instance, Tbl = self, Material = unwrap(self), Target = key, Url = url, cb = cb }
+	if inqueue == 0 then timer.Simple(0, NextInTextureQueue) end
+	
 end
 
 --- Sets a keyvalue to be undefined
