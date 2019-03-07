3d70b9113c1be373882d36041ec31560cdd1eaf1
CornerPin <cornerpinart@gmail.com>
2019-03-07 18:27:59 +0300

    Fixed an error when trying to create a file/folder using the file browser without Wiremod installed

--- lua/starfall/editor/sfframe.lua
+++ lua/starfall/editor/sfframe.lua
@@ -4,28 +4,30 @@ https://github.com/wiremod/wire
 File in use: https://github.com/wiremod/wire/blob/master/lua/wire/client/text_editor/sf_editor.lua
 ]]
 
---Part of WireLib
-local function Derma_StringRequestNoBlur(...)
-	local f = math.max
+if not Derma_StringRequestNoBlur then
+	--Part of WireLib
+	function Derma_StringRequestNoBlur(...)
+		local f = math.max
 
-	function math.max(...)
-		local ret = f(...)
+		function math.max(...)
+			local ret = f(...)
 
-		for i = 1,20 do
-			local name, value = debug.getlocal(2, i)
-			if name == "Window" then
-				value:SetBackgroundBlur( false )
-				break
+			for i = 1,20 do
+				local name, value = debug.getlocal(2, i)
+				if name == "Window" then
+					value:SetBackgroundBlur( false )
+					break
+				end
 			end
-		end
 
+			return ret
+		end
+		local ok, ret = xpcall(Derma_StringRequest, debug.traceback, ...)
+		math.max = f
+
+		if not ok then error(ret, 0) end
 		return ret
 	end
-	local ok, ret = xpcall(Derma_StringRequest, debug.traceback, ...)
-	math.max = f
-
-	if not ok then error(ret, 0) end
-	return ret
 end
 
 local function Derma_QueryNoBlur(...)
