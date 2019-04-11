783b4641de08eb57c9f7cf44976297b7ba8693ae
thegrb93 <garrettrbrown@mavs.uta.edu>
2019-04-10 21:07:15 -0400

    Added convar name

--- lua/starfall/libs_sh/builtins.lua
+++ lua/starfall/libs_sh/builtins.lua
@@ -646,7 +646,7 @@ if SERVER then
 		SF.instance.player:ConCommand(cmd)
 	end
 
-	--- Sets the chip's userdata that the duplicator tool saves. max 1MiB; can be changed with convar
+	--- Sets the chip's userdata that the duplicator tool saves. max 1MiB; can be changed with convar sf_userdata_max
 	-- @server
 	-- @param str String data
 	function SF.DefaultEnvironment.setUserdata(str)
