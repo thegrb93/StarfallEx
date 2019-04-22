7731c0537395f8268794a1f42dd0840a6234d5b1
Garrett Brown <grbrown93@sbcglobal.net>
2019-04-21 20:51:23 -0400

    Add vehicle.stripDriver vehicle.lock vehicle.unlock

--- lua/starfall/libs_sh/vehicles.lua
+++ lua/starfall/libs_sh/vehicles.lua
@@ -26,8 +26,10 @@ end)
 -- Register privileges
 do
 	local P = SF.Permissions
-	P.registerPrivilege("vehicle.eject", "Vehicle eject", "Removes a driver from vehicle")
+	P.registerPrivilege("vehicle.eject", "Vehicle eject", "Removes a driver from vehicle", { entities = {} })
 	P.registerPrivilege("vehicle.kill", "Vehicle kill", "Kills a driver in vehicle", { entities = {} })
+	P.registerPrivilege("vehicle.strip", "Vehicle strip", "Strips weapons from a driver in vehicle", { entities = {} })
+	P.registerPrivilege("vehicle.lock", "Vehicle lock", "Allow vehicle locking/unlocking", { entities = {} })
 end
 
 --- To string
@@ -84,6 +86,45 @@ if SERVER then
 		if driver:IsValid() then
 			driver:Kill()
 		end
+	end
+
+	--- Strips weapons of the driver
+	-- @param class Optional weapon class to strip. Otherwise all are stripped.
+	-- @server
+	function vehicle_methods:stripDriver (class)
+		checktype(self, vehicle_metamethods)
+		if class ~= nil then checkluatype(class, TYPE_STRING) end
+		local ent = unwrap(self)
+		if not IsValid(ent) then SF.Throw("Invalid entity", 2) end
+		checkpermission(SF.instance, ent, "vehicle.strip")
+		local driver = ent:GetDriver()
+		if driver:IsValid() then
+			if class then
+				driver:StripWeapon(class)
+			else
+				driver:StripWeapons()
+			end
+		end
+	end
+
+	--- Will lock the vehicle preventing players from entering or exiting the vehicle.
+	-- @server
+	function vehicle_methods:lock()
+		checktype(self, vehicle_metamethods)
+		local ent = unwrap(self)
+		if not IsValid(ent) then SF.Throw("Invalid entity", 2) end
+		checkpermission(SF.instance, ent, "vehicle.lock")
+		ent:Fire("Lock")
+	end
+
+	--- Will unlock the vehicle.
+	-- @server
+	function vehicle_methods:unlock()
+		checktype(self, vehicle_metamethods)
+		local ent = unwrap(self)
+		if not IsValid(ent) then SF.Throw("Invalid entity", 2) end
+		checkpermission(SF.instance, ent, "vehicle.lock")
+		ent:Fire("Unlock")
 	end
 
 
