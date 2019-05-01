3b5333edf9fb00263d9ebb2a8f56bd45b9242357
thegrb93 <garrettrbrown@mavs.uta.edu>
2019-05-01 19:51:42 -0400

    Add double packing code. Will test/finish soon

--- lua/starfall/libs_sh/bit.lua
+++ lua/starfall/libs_sh/bit.lua
@@ -110,6 +110,63 @@ local function UnpackIEEE754Float(b1, b2, b3, b4)
     end
     return math.ldexp(mantissa, exponent - 0x7F)
 end
+local function PackIEEE754Double(number)
+    if number == 0 then
+        return 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    elseif number ~= number then
+        return 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+    else
+        local sign = 0x00
+        if number < 0 then
+            sign = 0x80
+            number = -number
+        end
+        local mantissa, exponent = math.frexp(number)
+        exponent = exponent + 0xFFFF
+        if exponent <= 0 then
+            mantissa = math.ldexp(mantissa, exponent - 1)
+            exponent = 0
+        elseif exponent > 0 then
+            if exponent >= 0x7FF then
+                return string.char(sign + 0x7F, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+            elseif exponent == 1 then
+                exponent = 0
+            else
+                mantissa = mantissa * 2 - 1
+                exponent = exponent - 1
+            end
+        end
+        mantissa = math.floor(math.ldexp(mantissa, 52) + 0.5)
+        return sign + math.floor(exponent / 16),
+                (exponent % 16) * 0x10 + math.floor(mantissa / 0x1000000000000),
+                math.floor(mantissa / 0x10000000000) % 0x10000000000,
+                math.floor(mantissa / 0x100000000) % 0x100000000,
+                math.floor(mantissa / 0x1000000) % 0x1000000,
+                math.floor(mantissa / 0x10000) % 0x10000,
+                math.floor(mantissa / 0x100) % 0x100,
+                mantissa % 0x100
+    end
+end
+local function UnpackIEEE754Double(b1, b2, b3, b4, b5, b6, b7, b8)
+    local exponent = (b1 % 0x80) * 0x10 + math.floor(b2 / 0x10)
+    local mantissa = math.ldexp(((((((b2 % 0xF0) * 0x100 + b3) * 0x100 + b4) * 0x100 + b5) * 0x100 + b6) * 0x100 + b7) * 0x100 + b8, -52)
+    if exponent == 0x7FF then
+        if mantissa > 0 then
+            return 0 / 0
+        else
+            mantissa = math.huge
+            exponent = 0xFFFF
+        end
+    elseif exponent > 0 then
+        mantissa = mantissa + 1
+    else
+        exponent = exponent + 1
+    end
+    if b1 >= 0x80 then
+        mantissa = -mantissa
+    end
+    return math.ldexp(mantissa, exponent - 0xFFFF)
+end
 
 --- Returns little endian bytes (A B) (all 32 bits)
 --@param n The number to pack
