ff7a49ac7110e91b00380464fac569122e941cec
adamnejm <adamnejm@gmail.com>
2019-07-04 23:15:26 +0200

    Added Player:isSpeaking()

--- lua/starfall/libs_sh/players.lua
+++ lua/starfall/libs_sh/players.lua
@@ -530,4 +530,13 @@ if CLIENT then
 		local ent = unwrap(self)
 		return ent and ent:IsMuted()
 	end
+	
+	--- Returns whenever the player is heard by the local player.
+	-- @client
+	-- @return bool True/False
+	function player_methods:isSpeaking()
+		checktype(self, player_metamethods)
+		local ent = unwrap(self)
+		return IsValid(ent) and ent:IsSpeaking()
+	end
 end
