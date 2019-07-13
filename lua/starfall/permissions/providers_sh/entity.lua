d5176eadb4e30e6c9bd915ec4df39ca8d7556cbd
Garrett Brown <grbrown93@sbcglobal.net>
2019-07-13 19:17:44 -0400

    Use CPPI if it's available. Also fix not remembering disconnected players. Fixes: #614 Fixes: #520

--- lua/starfall/permissions/providers_sh/entity.lua
+++ lua/starfall/permissions/providers_sh/entity.lua
@@ -26,117 +26,207 @@ local dumbtrace = {
 }
 
 if SERVER then
-	P.checks = {
-		function(instance, target)
-			if isentity(target) and target:IsValid() then
-				if instance.player:IsSuperAdmin() then return true end
-				return P.props[target]==instance.player, "You're not the owner of this prop"
-			else
-				return false, "Entity is invalid"
-			end
-		end,
-		function(instance, target)
-			if isentity(target) and target:IsValid() then
-				local pos = target:GetPos()
-				dumbtrace.Entity = target		
-				return hook.Run("CanTool", instance.player, dumbtrace, "starfall_ent_lib") ~= false, "Target doesn't have toolgun access"
-			else
-				return false, "Entity is invalid"
-			end
-		end,
-		function(instance, target)
-			if isentity(target) and target:IsValid() then
-				if hook.Run("PhysgunPickup", instance.player, target) ~= false then
-					-- Some mods expect a release when there's a pickup involved.
-					hook.Run("PhysgunDrop", instance.player, target)
-					return true
+	if CPPI then
+		P.checks = {
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if instance.player:IsSuperAdmin() then return true end
+					if target:CPPIGetOwner()==instance.player then
+						return true
+					else
+						return false, "You're not the owner of this prop"
+					end
 				else
-					return false, "Target doesn't have physgun access"
+					return false, "Entity is invalid"
 				end
-			else
-				return false, "Entity is invalid"
-			end
-		end,
-		function() return true end
-	}
+			end,
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if target:CPPICanTool(instance.player, "starfall_ent_lib") then
+						return true
+					else
+						return false, "You can't toolgun this entity"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if target:CPPICanPhysgun(instance.player) then
+						return true
+					else
+						return false, "You can't physgun this entity"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function() return true end
+		}
+	else
+		P.checks = {
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if instance.player:IsSuperAdmin() then return true end
+					if P.props[target]==instance.player then
+						return true
+					else
+						return false, "You're not the owner of this prop"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					local pos = target:GetPos()
+					dumbtrace.Entity = target
+					if hook.Run("CanTool", instance.player, dumbtrace, "starfall_ent_lib") ~= false then
+						return true
+					else
+						return false, "Target doesn't have toolgun access"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if hook.Run("PhysgunPickup", instance.player, target) ~= false then
+						-- Some mods expect a release when there's a pickup involved.
+						hook.Run("PhysgunDrop", instance.player, target)
+						return true
+					else
+						return false, "Target doesn't have physgun access"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function() return true end
+		}
+	end
 else
-	P.checks = {
-		function(instance, target)
-			if isentity(target) and target:IsValid() then
-				if instance.player == target or LocalPlayer()==instance.player or instance.player:IsSuperAdmin() then return true end
-				local owner = target:GetNWEntity("SFPP")
-				if owner ~= NULL then
-					return owner==instance.player, "You're not the owner of this prop"
+	if CPPI then
+		P.checks = {
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if instance.player:IsSuperAdmin() then return true end
+					if target:CPPIGetOwner()==instance.player then
+						return true
+					else
+						return false, "You're not the owner of this prop"
+					end
 				else
-					return false, "The entity's owner hasn't been transmitted yet or doesn't exist"
+					return false, "Entity is invalid"
 				end
-			else
-				return false, "Entity is invalid"
-			end
-		end,
-		function() return false, "Target doesn't have physgun access" end,
-		function() return false, "Target doesn't have toolgun access" end,
-		function() return true end
-	}
+			end,
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if target:CPPICanTool(instance.player, "starfall_ent_lib") then
+						return true
+					else
+						return false, "You can't toolgun this entity"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if target:CPPICanPhysgun(instance.player) then
+						return true
+					else
+						return false, "You can't physgun this entity"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function() return true end
+		}
+	else
+		P.checks = {
+			function(instance, target)
+				if isentity(target) and target:IsValid() then
+					if instance.player == target or LocalPlayer()==instance.player or instance.player:IsSuperAdmin() then return true end
+					local owner = target:GetNWEntity("SFPP")
+					if owner ~= NULL then
+						if owner==instance.player then
+							return true
+						else
+							return false, "You're not the owner of this prop"
+						end
+					else
+						return false, "The entity's owner hasn't been transmitted yet or doesn't exist"
+					end
+				else
+					return false, "Entity is invalid"
+				end
+			end,
+			function() return false, "Target doesn't have physgun access" end,
+			function() return false, "Target doesn't have toolgun access" end,
+			function() return true end
+		}
+	end
 end
 
-if SERVER then
-	P.props = setmetatable({},{__mode="k"})
-
-	function SF.Permissions.getOwner(ent)
-		return P.props[ent] or NULL
-	end
-
-	local function PropOwn(ply,ent)
-		P.props[ent] = ply
-		ent:SetNWEntity("SFPP", ply)
-	end
-
-	if(cleanup) then
-		local backupcleanupAdd = cleanup.Add
-		function cleanup.Add(ply, enttype, ent)
-			if IsValid(ent) and ply:IsPlayer() then
-				PropOwn(ply, ent)
+hook.Add("Initialize","SF_PPInitialize",function()
+	if not CPPI then
+		if SERVER then
+			P.props = setmetatable({},{__mode="k"})
+			function SF.Permissions.getOwner(ent)
+				return P.props[ent] or NULL
 			end
-			backupcleanupAdd(ply, enttype, ent)
+
+			local function PropOwn(ply,ent)
+				P.props[ent] = ply
+				ent:SetNWEntity("SFPP", ply)
+			end
+
+			if(cleanup) then
+				local backupcleanupAdd = cleanup.Add
+				function cleanup.Add(ply, enttype, ent)
+					if IsValid(ent) and ply:IsPlayer() then
+						PropOwn(ply, ent)
+					end
+					backupcleanupAdd(ply, enttype, ent)
+				end
+			end
+			local metaply = FindMetaTable("Player")
+			if(metaply.AddCount) then
+				local backupAddCount = metaply.AddCount
+				function metaply:AddCount(enttype, ent)
+					PropOwn(self, ent)
+					backupAddCount(self, enttype, ent)
+				end
+			end
+			hook.Add("PlayerSpawnedSENT", "SFPP.PlayerSpawnedSENT", PropOwn)
+			hook.Add("PlayerSpawnedVehicle", "SFPP.PlayerSpawnedVehicle", PropOwn)
+			hook.Add("PlayerSpawnedSWEP", "SFPP.PlayerSpawnedSWEP", PropOwn)
+			hook.Add("PlayerInitialSpawn","SFPP.PlayerInitialSpawn", function(ply)
+				local steamid = ply:SteamID64()
+				for k, v in pairs(P.props) do
+					if v==steamid then
+						PropOwn(ply,k)
+					end
+				end
+			end)
+			hook.Add("PlayerDisconnected","SFPP.PlayerDisconnected", function(ply)
+				local steamid = ply:SteamID64()
+				for k, v in pairs(P.props) do
+					if v==ply then
+						P.props[k] = steamid
+					end
+				end
+			end)
+		else
+			function SF.Permissions.getOwner(ent)
+				return ent:GetNWEntity("SFPP")
+			end
 		end
 	end
-	local metaply = FindMetaTable("Player")
-	if(metaply.AddCount) then
-		local backupAddCount = metaply.AddCount
-		function metaply:AddCount(enttype, ent)
-			PropOwn(self, ent)
-			backupAddCount(self, enttype, ent)
-		end
-	end
-	hook.Add("PlayerSpawnedSENT", "SFPP.PlayerSpawnedSENT", PropOwn)
-	hook.Add("PlayerSpawnedVehicle", "SFPP.PlayerSpawnedVehicle", PropOwn)
-	hook.Add("PlayerSpawnedSWEP", "SFPP.PlayerSpawnedSWEP", PropOwn)
-	
-	--[[net.Receive("SFPPTransmit", function(len, pl)
-		local e = net.ReadEntity()
-		if P.props[e] then
-			net.Start("SFPPTransmit")
-			net.WriteEntity(P.props[e])
-			net.WriteEntity(e)
-			net.Send(pl)
-		end
-	end)]]
-else
-	--[[hook.Add("NetworkEntityCreated", "SFPP.RequestOwner", function(ent)
-		net.Start("SFPPTransmit")
-		net.WriteEntity(ent)
-		net.SendToServer()
-	end)
-	
-	net.Receive("SFPPTransmit", function()
-		local ply, ent = net.ReadEntity(), net.ReadEntity()
-		P.props[ent] = ply
-	end)]]
-	
-	function SF.Permissions.getOwner(ent)
-		return ent:GetNWEntity("SFPP")
-	end
-end
+end)
 
 SF.Permissions.registerProvider(P)
