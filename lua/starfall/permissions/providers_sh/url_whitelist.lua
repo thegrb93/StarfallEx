99c30f720cdd8147e89260310b19888961f9740c
thegrb93 <garrettrbrown@mavs.uta.edu>
2019-07-26 19:12:48 -0400

    Fix pattern

--- lua/starfall/permissions/providers_sh/url_whitelist.lua
+++ lua/starfall/permissions/providers_sh/url_whitelist.lua
@@ -178,4 +178,4 @@ simple [[ipfs.io]]
 --- Examples:
 --- https://fauux.neocities.org/LainDressSlow.gif
 
-pattern [[[A-Za-z0-9](?:[A-Za-z0-9%-]{0,61}[A-Za-z0-9])?%.neocities%.org/(.+)]
+pattern pattern [[([%w-_]+)%.neocities%.org/(.+)]]
