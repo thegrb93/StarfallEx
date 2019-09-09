4c4bb8abe50b35dd665ca36f52b648b58e4f85b4
Wizard Lizard <seanomahoney116@gmail.com>
2019-09-08 23:07:04 -0400

    Proper ownership, and removing notifications when chip is deleted

--- lua/starfall/libs_cl/notification.lua
+++ lua/starfall/libs_cl/notification.lua
@@ -42,18 +42,39 @@ function notification_library.addLegacy(text, type, length)
 	notification.AddLegacy( text, type, length )
 end
 
---- Displays a notification with an animated progress bar, will persist unless killed.
--- @param id Index of the notification
+local ids = {}
+
+--- Displays a notification with an animated progress bar, will persist unless killed or chip is removed.
+-- @param id String index of the notification
 -- @param text The text to display
 function notification_library.addProgress(id, text)
 	checkpermission(SF.instance, nil, "notification")
+	checkluatype(id, TYPE_STRING)
 	checkluatype(text, TYPE_STRING)
+	
+	--Keep the ID unique to each player
+	id = "SF:"..SF.instance.player:SteamID64()..id
 	notification.AddProgress( id, text )
+	ids[id] = true
 end
 
 --- Removes the notification with the given index after 0.8 seconds
--- @param id Index of the notification to kill
+-- @param id String index of the notification to kill
 function notification_library.kill(id)
 	checkpermission(SF.instance, nil, "notification")
-	notification.Kill( id )
+	checkluatype(id, TYPE_STRING)
+	
+	id = "SF:"..SF.instance.player:SteamID64()..id
+	
+	if ids[id] then
+		notification.Kill( id )
+		ids[id] = nil
+	end
 end
+
+SF.AddHook("deinitialize", function( inst )
+	for id,_ in pairs(ids) do
+		notification.Kill( id )
+		ids[id] = nil
+	end
+end)
