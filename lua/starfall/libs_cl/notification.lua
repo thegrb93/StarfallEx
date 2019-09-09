f140a1040eacd1b3b9ab88ab9a37dfeb99919679
Wizard Lizard <seanomahoney116@gmail.com>
2019-09-08 21:21:31 -0400

    camelCase fix

--- lua/starfall/libs_cl/notification.lua
+++ lua/starfall/libs_cl/notification.lua
@@ -5,7 +5,6 @@
 --- Notification functions. Allows the user to display hints on the bottom right of their screen
 -- @client
 local notification_library = SF.RegisterLibrary("notification")
-print("notify library loaded")
 SF.AddHook("postload", function()
 	-- @name SF.DefaultEnvironment.NOTIFY
 	-- @class table
@@ -16,7 +15,6 @@ SF.AddHook("postload", function()
 		["HINT"] = NOTIFY_HINT,
         ["CLEANUP"] = NOTIFY_CLEANUP,
     }
-    print("Notify Postloaded")
 end)
 
 --- Displays a standard notification.
@@ -28,19 +26,19 @@ end)
 ---NOTIFY.HINT
 ---NOTIFY.CLEANUP
 -- @param length Time in seconds to display the notification
-function notification_library.AddLegacy(text, type, length)
+function notification_library.addLegacy(text, type, length)
 	notification.AddLegacy( text, type, length )
 end
 
 --- Displays a notification with an animated progress bar, will persist unless killed.
 -- @param id Index of the notification
 -- @param text The text to display
-function notification_library.AddProgress(id, text)
+function notification_library.addProgress(id, text)
 	notification.AddProgress( id, text )
 end
 
 --- Removes the notification with the given index after 0.8 seconds
 -- @param id Index of the notification to kill
-function notification_library.Kill(id)
+function notification_library.kill(id)
 	notification.Kill( id )
 end
