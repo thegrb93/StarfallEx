a85671f8c3f07147aabcd21d375603fa73c1c047
Garrett Brown <grbrown93@sbcglobal.net>
2019-12-24 10:01:40 -0500

    Add game.getSunInfo. Fixes: #739

--- lua/starfall/libs_sh/game.lua
+++ lua/starfall/libs_sh/game.lua
@@ -49,4 +49,13 @@ if CLIENT then
 	-- @class function
 	-- @return boolean True if the game is focused
 	game_lib.hasFocus = system.HasFocus
+	
+	--- Returns the direction and how obstructed the map's sun is
+	-- @client
+	-- @return vector The direction of the sun
+	-- @return number How obstructed the sun is 0 to 1.
+	function game_lib.getSunInfo()
+		local info = util.GetSunInfo()
+		return SF.Vectors.Wrap(info.direction), info.obstruction
+	end
 end
