a1a9bb74fab6a43380062a1aecbb705f01b74de9
H3xCat <2304691+h3xcat@users.noreply.github.com>
2020-01-03 07:31:57 -0800

    Swizzle setter for vectors

--- lua/starfall/libs_sh/vectors.lua
+++ lua/starfall/libs_sh/vectors.lua
@@ -47,6 +47,18 @@ local xyz = { x = 1, y = 2, z = 3 }
 function vec_metamethods.__newindex (t, k, v)
 	if xyz[k] then
 		rawset(t, xyz[k], v)
+
+	elseif (#k == 2 and xyz[k[1]] and xyz[k[2]])  then
+		checktype(v, vec_metamethods)
+
+		rawset(t, xyz[k[1]], rawget(v, 1))
+		rawset(t, xyz[k[2]], rawget(v, 2))
+	elseif (#k == 3 and xyz[k[1]] and xyz[k[2]] and xyz[k[3]]) then
+		checktype(v, vec_metamethods)
+
+		rawset(t, xyz[k[1]], rawget(v, 1))
+		rawset(t, xyz[k[2]], rawget(v, 2))
+		rawset(t, xyz[k[3]], rawget(v, 3))
 	else
 		rawset(t, k, v)
 	end
