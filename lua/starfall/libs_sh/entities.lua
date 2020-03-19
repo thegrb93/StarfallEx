350891895a162b5548fa9364c889d958199327c6
adamnejm <adamnejm@gmail.com>
2020-03-19 03:27:04 +0100

    added entOwner

--- lua/starfall/libs_sh/entities.lua
+++ lua/starfall/libs_sh/entities.lua
@@ -19,6 +19,7 @@ SF.RegisterType("Entity", false, true, debug.getregistry().Entity)
 
 return function(instance)
 
+local owrap, ounwrap = instance.WrapObject, instance.UnwrapObject
 local ents_methods, ent_meta, ewrap, eunwrap = instance.Types.Entity.Methods, instance.Types.Entity, instance.Types.Entity.Wrap, instance.Types.Entity.Unwrap
 local ang_meta, awrap, aunwrap = instance.Types.Angle, instance.Types.Angle.Wrap, instance.Types.Angle.Unwrap
 local vec_meta, vwrap, vunwrap = instance.Types.Vector, instance.Types.Vector.Wrap, instance.Types.Vector.Unwrap
@@ -1165,6 +1166,14 @@ function ents_methods:isEffectActive(effect)
 	
 	local ent = getent(self)
 	return ent:IsEffectActive(effect)
+end
+
+--- Returns the assigned owner. Use Entity.getOwner to check prop ownership
+-- @shared
+-- @return Owner
+function ents_methods:entOwner()
+	local ent = getent(self)
+	return owrap(ent:GetOwner())
 end
 
 end
