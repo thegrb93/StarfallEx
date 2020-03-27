1601ebc992015762cc4d225e330048ecbf0bea6c
Damianu <xdamianu@gmail.com>
2020-03-27 15:23:58 +0100

    ehh

---
+++ src/Components/Pages/Library.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function LibraryPage(props)
+{
+    console.log("Rendering library "+JSON.stringify(props))
+    const methods = [];
+
+    for(const key in props.methods)
+    {
+      const value = props.methods[key];
+      methods.push(<tr key = {key}><td>{key}</td><td>{value}</td></tr>);
+    }
+    
+    return (
+        <div className="page page-library">
+            <h1>{props.name}</h1>
+            <span>{props.description}</span>
+
+            <h3>Methods:</h3>
+            <table>
+                {methods}
+            </table>
+        </div>
+
+    )
+}
