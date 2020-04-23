6fbf8d4fc7312472cc97a3ef57ecbb0039013597
Garrett Brown <grbrown93@sbcglobal.net>
2020-04-23 16:42:13 -0400

    Cleanup, allow async send+receive together.

--- lua/starfall/examples/tcpsocket.lua
+++ lua/starfall/examples/tcpsocket.lua
@@ -5,114 +5,223 @@
 -- Requires setup of the socket library
 -- This example performs async GET request from an http server and prints the response.
 
+local operationTimeout = 5
+
 local tcpclient = class("tcpclient")
+local tcptask = class("tcptask")
+local tcpconnecttask = class("tcpconnecttask", tcptask)
+local tcpsendtask = class("tcpsendtask", tcptask)
+local tcprecvtask = class("tcprecvtask", tcptask)
 
-function tcpclient:initialize()
-	self.queue = {}
-	self.tries = 0
-	self.maxtries = 60
+do -- tcptask
+	function tcptask:initialize(client, success, fail)
+		self.client = client
+		self.socket = client.socket
+		self.success = success
+		self.fail = fail
+	end
 
-	self.socket = socket.tcp()
-	self.socket:settimeout(0)
+	function tcptask:success()
+	end
 
-	hook.add("think",tostring(self),function() self:process() end)
+	function tcptask:fail()
+	end
+
+	function tcptask:isTimedout()
+		if self.timeout then
+			return timer.curtime()<self.timeout
+		else
+			self.timeout = timer.curtime()+operationTimeout
+			return false
+		end
+	end
 end
 
-function tcpclient:connect(addr, port, success, fail)
-	self.queue[#self.queue + 1] = function(timeout)
+do -- tcpconnecttask
+	function tcpconnecttask:initialize(client, addr, port, success, fail)
+		tcptask.initialize(self, client, success, fail)
 		self.socket:connect(addr, port)
-		return true
 	end
-	self.queue[#self.queue + 1] = function(timeout)
-		if timeout then if fail then fail("Connect operation timed out!") end return end
-		local r, w, e = socket.select(nil, {self.socket}, 0)
-		if e==nil then if success then success() end return true end
-		if e=="timeout" then return end
-		if fail then fail(e) end
-		return true
-	end
-end
 
-function tcpclient:receive(success, fail)
-	self.queue[#self.queue + 1] = function(timeout)
-		if timeout then if fail then fail("Receive operation timed out!") end return end
-		local r, w, e = socket.select({self.socket}, nil, 0)
-		if e=="timeout" then return end
-		if r[1] then
-			local recv, err, recv2 = self.socket:receive(2048)
-			if recv then
-				if success then success(recv) end
-			elseif err=="closed" then
-				if success then success(recv2) end
-			elseif err=="timeout" then
-				return
+	function tcpconnecttask:run()
+		local r, w, e = socket.select(nil, {self.socket}, 0)
+		if e==nil then
+			self:success()
+		elseif e=="timeout" then
+			if self:isTimedout() then
+				self:fail("Connect operation timed out!")
 			else
-				if fail then fail(err) end
-			end
-		else
-			if fail then fail(e) end
-		end
-		return true
-	end
-end
-
-function tcpclient:send(data, success, fail)
-	local bytesSent = 0
-	self.queue[#self.queue + 1] = function(timeout)
-		if timeout then if fail then fail("Send operation timed out!") end return end
-		local r, w, e = socket.select(nil, {self.socket}, 0)
-		if e=="timeout" then return end
-		if w[1] then
-			local sent, err, err2 = self.socket:send(data, bytesSent+1, math.min(#data, bytesSent+2048))
-			print(sent, err, err2)
-			if sent then
-				bytesSent = bytesSent + sent
-				if bytesSent == #data then
-					if success then success() end
-					return true
-				end
 				return false
-			else
-				if fail then fail(err) end
 			end
 		else
-			if fail then fail(e) end
+			self:fail(e)
 		end
 		return true
 	end
 end
 
-function tcpclient:process()
-	local func = self.queue[1]
-	if func then
-		local result = func()
-		if result then
-			self.tries = 0
-			table.remove(self.queue, 1)
-		elseif result == false then
-			self.tries = 0
-		else
-			if self.tries == self.maxtries then
-				func(true)
-				self.tries = 0
-				table.remove(self.queue, 1)
+do -- tcpsendtask
+	function tcpsendtask:initialize(client, data, success, fail)
+		tcptask.initialize(self, client, success, fail)
+		self.data = data
+		self.bytesSent = 0
+	end
+
+	function tcpsendtask:run()
+		local r, w, e = socket.select(nil, {self.socket}, 0)
+		if e==nil then
+			local sent, err, sent2 = self.socket:send(self.data, self.bytesSent+1, math.min(#self.data, self.bytesSent+2048))
+			print(sent, err, sent2)
+			if err==nil then
+				self.bytesSent = self.bytesSent + sent
+				if self.bytesSent == #self.data then
+					self:success()
+				else
+					return false
+				end
+			elseif err=="closed" then
+				self:fail(err, sent2)
+				self.client:close()
+			elseif err=="timeout" then
+				if self:isTimedout() then
+					self:fail("Send operation timed out!")
+				else
+					return false
+				end
 			else
-				self.tries = self.tries + 1
+				self:fail(err)
 			end
+		elseif e=="timeout" then
+			if self:isTimedout() then
+				self:fail("Send operation timed out!")
+			else
+				return false
+			end
+		else
+			self:fail(e)
 		end
+		return true
 	end
 end
 
-function tcpclient:close()
-	hook.remove("think",tostring(self))
-	self.socket:close()
-	self.socket = nil
+do -- tcprecvtask
+	function tcprecvtask:initialize(client, addr, port, success, fail)
+		tcptask.initialize(self, client, success, fail)
+	end
+
+	function tcprecvtask:run()
+		local r, w, e = socket.select({self.socket}, nil, 0)
+		if e==nil then
+			local recv, err, recv2 = self.socket:receive(2048)
+			if err==nil then
+				self:success(recv)
+			elseif err=="closed" then
+				self:success(recv2, true)
+				self.client:close()
+			elseif err=="timeout" then
+				if self:isTimedout() then
+					self:fail("Receive operation timed out!")
+				else
+					return false
+				end
+			else
+				self:fail(err)
+			end
+		elseif e=="timeout" then
+			if self:isTimedout() then
+				self:fail("Receive operation timed out!")
+			else
+				return false
+			end
+		else
+			self:fail(e)
+		end
+		return true
+	end
+end
+
+do -- tcpclient
+	function tcpclient:initialize()
+		self.connected = false
+		self.sendqueue = {}
+		self.recvqueue = {}
+
+		self.socket = socket.tcp()
+		self.socket:settimeout(0)
+
+		hook.add("think",tostring(self),function()
+			self:process()
+		end)
+	end
+
+	function tcpclient:connect(addr, port, success, fail)
+		if self.connected then
+			error("This socket is already connected!")
+		elseif self.connecting then
+			error("This socket is already connecting!")
+		else
+			self.connecting = tcpconnecttask:new(self, addr, port, success, fail)
+		end
+	end
+
+	function tcpclient:receive(success, fail)
+		if self.connected then
+			self.recvqueue[#self.recvqueue + 1] = tcprecvtask:new(self, success, fail)
+		else
+			error("The socket is not connected!")
+		end
+	end
+
+	function tcpclient:send(data, success, fail)
+		if self.connected then
+			self.sendqueue[#self.sendqueue + 1] = tcpsendtask:new(self, data, success, fail)
+		else
+			error("The socket is not connected!")
+		end
+	end
+
+	function tcpclient:process()
+		if self.connected then
+			if #self.recvqueue>0 and self.recvqueue[1]:process() then
+				table.remove(self.recvqueue, 1)
+			end
+			if #self.sendqueue>0 and self.sendqueue[1]:process() then
+				table.remove(self.sendqueue, 1)
+			end
+		elseif self.connecting then
+			local result = self.connecting:process()
+			if result == true then
+				self.connected = true
+				self.connecting = nil
+			elseif result == false then
+				self.connecting = nil
+			end
+		end
+	end
+
+	function tcpclient:close()
+		hook.remove("think",tostring(self))
+		self.socket:close()
+		self.socket = nil
+		self.connected = false
+		self.connecting = nil
+	end
 end
 
 local sock = tcpclient:new()
 sock:connect("sparkysandbox.org", 80, nil, error)
 sock:send("GET / HTTP/1.0\r\nHost: sparkysandbox.org\r\n\r\n", nil, error)
-sock:receive(function(data)
-	print(data)
-	sock:close()
-end, error)
+-- Keep reading until it closes or times out.
+local chunks = {}
+local function receiveData()
+	sock:receive(function(data, closed)
+		chunks[#chunks+1] = data
+		if closed then
+			print(table.concat(chunks))
+		else
+			receiveData()
+		end
+	end, error)
+end
+receiveData()
+
