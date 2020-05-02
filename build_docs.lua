2cc70b3173567d1321c483af21c56a8c818050f2
Garrett Brown <grbrown93@sbcglobal.net>
2020-05-02 19:25:23 -0400

    Done

--- build_docs.lua
+++ build_docs.lua
@@ -21,19 +21,21 @@ ErrorNoHalt = print
 local lfs = require"lfs"
 local json = require"json"
 
-local outputdir = arg[1] or "."
-local sourcecode = arg[2] or "../lua/starfall"
+local outputdir = "."
+local sourcecode = "../lua/starfall"
 
 SF = {Modules = {}}
 local function readModules(path)
     for file in lfs.dir(path) do
-        local moduleTbl = SF.Modules[file]
-        if not moduleTbl then moduleTbl = {} SF.Modules[file] = moduleTbl end
-        
-        local filen = path.."/"..file
-        local f = assert(io.open(filen, "r"))
-        moduleTbl[filen] = {source = f:read("*all")}
-        f:close()
+        if string.match(file,".+%.lua") then
+            local moduleTbl = SF.Modules[file]
+            if not moduleTbl then moduleTbl = {} SF.Modules[file] = moduleTbl end
+            
+            local filen = path.."/"..file
+            local f = assert(io.open(filen, "r"))
+            moduleTbl[filen] = {source = f:read("*all")}
+            f:close()
+        end
     end
 end
 readModules(sourcecode.."/libs_cl")
