780a732687f5af3dbd96c8c6c0f37653530af458
Garrett Brown <grbrown93@sbcglobal.net>
2020-05-02 18:56:05 -0400

    Paths

--- ulua/lua.cmd
+++ ulua/lua.cmd
@@ -19,6 +19,6 @@ SET LUA_ROOT=%LUA_ROOT:~0,-1%
 SET LUA_ROOT=%LUA_ROOT:\=/%
 SET LJ_SYS=Windows
 SET LJ_CORE=%LUA_ROOT%/%LJ_VER_EXT%/%LJ_SYS%/%LJ_ARCH%
-SET LUA_PATH=%LUA_ROOT%/../?.lua;%LUA_ROOT%/../../starfall/editor/?.lua;%LUA_ROOT%/?/init.lua;%LUA_ROOT%/?.lua;%LJ_CORE%/?/init.lua;%LJ_CORE%/?.lua;
+SET LUA_PATH=%LUA_ROOT%/../?.lua;%LUA_ROOT%/../../lua/starfall/editor/?.lua;%LUA_ROOT%/?/init.lua;%LUA_ROOT%/?.lua;%LJ_CORE%/?/init.lua;%LJ_CORE%/?.lua;
 SET LUA_CPATH=%LUA_ROOT%/?.dll;%LUA_ROOT%/loadall.dll;
 "%LJ_CORE%/luajit" -l__init %*
