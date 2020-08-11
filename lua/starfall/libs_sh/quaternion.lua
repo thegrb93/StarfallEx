983bb8011d34fed8bee035475c51c32e40849220
adamnejm <adamnejm@gmail.com>
2020-08-12 00:10:53 +0200

    Ported all the remaining functions

--- lua/starfall/libs_sh/quaternion.lua
+++ lua/starfall/libs_sh/quaternion.lua
@@ -24,6 +24,7 @@ local quat_methods, quat_meta, qwrap, unwrap = instance.Types.Quaternion.Methods
 local ent_methods, ent_meta, ewrap, eunwrap = instance.Types.Entity.Methods, instance.Types.Entity, instance.Types.Entity.Wrap, instance.Types.Entity.Unwrap
 local ang_methods, ang_meta, awrap, aunwrap = instance.Types.Angle.Methods, instance.Types.Angle, instance.Types.Angle.Wrap, instance.Types.Angle.Unwrap
 local vec_methods, vec_meta, vwrap, vunwrap = instance.Types.Vector.Methods, instance.Types.Vector, instance.Types.Vector.Wrap, instance.Types.Vector.Unwrap
+local math_library = instance.Libraries.math
 
 local function wrap(tbl)
 	return setmetatable(tbl, quat_meta)
@@ -42,9 +43,13 @@ local math_log = math.log
 local math_sin = math.sin
 local math_cos = math.cos
 local math_acos = math.acos
+local math_clamp = math.Clamp
+local math_max = math.max
 
 local deg2rad = math.pi / 180
 local rad2deg = 180 / math.pi
+
+-- Following helper functions are strictly operating on tables, so be sure to wrap the return value
 
 local function quatMul(lhs, rhs)
 	local lhs1, lhs2, lhs3, lhs4 = lhs[1], lhs[2], lhs[3], lhs[4]
@@ -83,6 +88,30 @@ local function quatLog(q)
 			return { math_log(len), 0, 0, 0 }
 		end
 	end
+end
+
+local function quatFromAngle(ang)
+	local p = ang[1] * deg2rad * 0.5
+	local y = ang[2] * deg2rad * 0.5
+	local r = ang[3] * deg2rad * 0.5
+	
+	local qr = { math_cos(r), math_sin(r), 0, 0 }
+	local qp = { math_cos(p), 0, math_sin(p), 0 }
+	local qy = { math_cos(y), 0, 0, math_sin(y) }
+	
+	return quatMul(qy, quatMul(qp, qr))
+end
+
+local function quatFromAngleComponents(p, y, r)
+	p = p * deg2rad * 0.5
+	y = y * deg2rad * 0.5
+	r = r * deg2rad * 0.5
+	
+	local qr = { math_cos(r), math_sin(r), 0, 0 }
+	local qp = { math_cos(p), 0, math_sin(p), 0 }
+	local qy = { math_cos(y), 0, 0, math_sin(y) }
+	
+	return quatMul(qy, quatMul(qp, qr))
 end
 
 -------------------------------------
@@ -443,9 +472,114 @@ end
 
 -------------------------------------
 
+function quat_methods:getSlerp(quat, t)
+	checkluatype(t, TYPE_NUMBER)
+	quat = unwrap(quat)
+	
+	local len = self[1] * self[1] + self[2] * self[2] + self[3] * self[3] + self[4] * self[4]
+	if len == 0 then
+		return wrap({ 0, 0, 0, 0})
+	else
+		local dot = self[1] * quat[1] + self[2] * quat[2] + self[3] * quat[3] + self[4] * quat[4]
+		local new
+		if dot < 0 then
+			new = { -quat[1], -quat[2], -quat[3], -quat[4] }
+		else
+			new = quat
+		end
+		
+		local inv = { self[1] / len, -self[2] / len, -self[3] / len, -self[4] / len }
+		local log = quatLog(quatMul(inv, new))
+		local q = quatExp({ log[1] * t, log[2] * t, log[3] * t, log[4] * t })
+		
+		return wrap(quatMul(self, q))
+	end
+end
+
 function quat_methods:getVector()
 	return vwrap(Vector(self[2], self[3], self[4]))
 end
+
+-- quat_lib.rotationEulerAngle(q)
+function quat_methods:getEulerAngle()
+	local len = math_sqrt(self[1] * self[1] + self[2] * self[2] + self[3] * self[3] + self[4] * self[4])
+	if len == 0 then
+		return awrap(Angle(0, 0, 0))
+	else
+		local q1, q2, q3, q4 = self[1] / len, self[2] / len, self[3] / len, self[4] / len
+		local x = Vector(q1 * q1 + q2 * q2 - q3 * q3 - q4 * q4,
+			2 * q3 * q2 + 2 * q4 * q1,
+			2 * q4 * q2 - 2 * q3 * q1)
+		local y = Vector(2 * q2 * q3 - 2 * q4 * q1,
+			q1 * q1 - q2 * q2 + q3 * q3 - q4 * q4,
+			2 * q2 * q1 + 2 * q3 * q4)
+		
+		local ang = x:Angle()
+		if ang[1] > 180 then ang[1] = ang[1] - 360 end
+		if ang[2] > 180 then ang[2] = ang[2] - 360 end
+		
+		local yaw = Vector(0, 1, 0)
+		yaw:Rotate(Angle(0, ang[1], 0))
+		
+		ang[3] = math_acos(math_clamp(y:Dot(yaw), -1, 1)) * rad2deg
+		local dot = q1 * q2 + q3 * q4
+		if dot < 0 then
+			ang[3] = -ang[3]
+		end
+		
+		return awrap(ang)
+	end
+end
+
+-- full: true = [0..360]; false = [-180..180]
+function quat_methods:getRotationAngle(full)
+	local len = self[1] * self[1] + self[2] * self[2] + self[3] * self[3] + self[4] * self[4]
+	if len == 0 then
+		return 0
+	else
+		local ang = 2 * math_acos(math_clamp(self[1] / math_sqrt(len), -1, 1)) * rad2deg
+		
+		if full then
+			checkluatype(full, TYPE_BOOL)
+			return ang
+		else
+			if ang > 180 then
+				ang = ang - 360
+			end
+			return ang
+		end
+	end
+end
+
+-- coder0xff
+function quat_methods:getRotationAxis()
+	local m = self[2] * self[2] + self[3] * self[3] + self[4] * self[4]
+	if m == 0 then
+		return vwrap(Vector(0, 0, 1))
+	else
+		local m_sqrt = math_sqrt(m)
+		return vwrap(Vector(self[2] / m_sqrt, self[3] / m_sqrt, self[4] / m_sqrt))
+	end
+end
+
+function quat_methods:getRotationVector()
+	local len = self[1] * self[1] + self[2] * self[2] + self[3] * self[3] + self[4] * self[4]
+	local m = math_max(self[2] * self[2] + self[3] * self[3] + self[4] * self[4], 0)
+	
+	if len == 0 or m == 0 then
+		return vwrap(Vector(0, 0, 0))
+	else
+		local ang = 2 * math_acos(math_clamp(self[1] / math_sqrt(len), -1, 1)) * rad2deg
+		if ang > 180 then
+			ang = ang - 360
+		end
+		ang = ang / math_sqrt(m)
+		
+		return vwrap(Vector(self[2] * ang, self[3] * ang, self[4] * ang))
+	end
+end
+
+-------------------------------------
 
 --- Converts vector to quaternion
 -- @param up Upward direction. If specified, the original vector will act like a forward pointing one
@@ -461,57 +595,76 @@ function vec_methods:getQuaternion(up)
 		if ang[1] > 180 then ang[1] = ang[1] - 360 end
 		if ang[2] > 180 then ang[2] = ang[2] - 360 end
 		
-		local yyaw = Vector(0, 1, 0)
-		yyaw:Rotate(Angle(0, ang[2], 0))
+		local yaw = Vector(0, 1, 0)
+		yaw:Rotate(Angle(0, ang[2], 0))
 		
-		local roll = math_acos(math.Clamp(y:Dot(yyaw), -1, 1)) * rad2deg
-		if y.z < 0 then
+		local roll = math_acos(math_clamp(y:Dot(yaw), -1, 1)) * rad2deg
+		if y[3] < 0 then
 			roll = -roll
 		end
 		
-		local p = ang[1] * deg2rad * 0.5
-		local y = ang[2] * deg2rad * 0.5
-		local r = roll * deg2rad * 0.5
-		
-		local qr = { math_cos(r), math_sin(r), 0, 0 }
-		local qp = { math_cos(p), 0, math_sin(p), 0 }
-		local qy = { math_cos(y), 0, 0, math_sin(y) }
-		
-		return wrap(quatMul(qy, quatMul(qp, qr)))
+		return wrap(quatFromAngleComponents(ang[1], ang[2], roll))
 	else
 		return wrap({ 0, self[1], self[2], self[3] })
 	end
 end
 
+-- quat_lib.qRotation(axis, ang)
+function vec_methods:getQuaternionFromAxis(ang)
+	local axis = vunwrap(self):Normalize()
+	local rang = anuwrap(ang) * deg2rad * 0.5
+	
+	return wrap({ math_cos(rang), axis[1] * math_sin(rang), axis[2] * math_sin(rang), axis[3] * math_sin(rang) })
+end
+
+-- quat_lib.qRotation(rv1) // coder0xff (github)
+function vec_methods:getQuaternionFromRotation()
+	local sqr = self[1] * self[1] + self[2] * self[2] + self[3] * self[3]
+	if sqr == 0 then
+		return wrap({ 0, 0, 0, 0})
+	else
+		local len = math_sqrt(sqr)
+		local norm = (len + 180) % 360 - 180
+		local ang = norm * deg2rad * 0.5
+		local anglen = math_sin(ang2) / len
+		
+		return wrap({ math_cos(ang2), self[1] * ang2len, self[2] * ang2len, self[3] * ang2len })
+	end
+end
+
 function ang_methods:getQuaternion()
-	local p = self[1] * deg2rad * 0.5
-	local y = self[2] * deg2rad * 0.5
-	local r = self[3] * deg2rad * 0.5
-	
-	local qr = { math_cos(r), math_sin(r), 0, 0 }
-	local qp = { math_cos(p), 0, math_sin(p), 0 }
-	local qy = { math_cos(y), 0, 0, math_sin(y) }
-	
-	return wrap(quatMul(qy, quatMul(qp, qr)))
+	return wrap(quatFromAngle(aunwrap(self)))
 end
 
 function ent_methods:getQuaternion()
 	local ang = getent(self):GetAngles()
-	
-	local p = ang[1] * deg2rad * 0.5
-	local y = ang[2] * deg2rad * 0.5
-	local r = ang[3] * deg2rad * 0.5
-	
-	local qr = { math_cos(r), math_sin(r), 0, 0 }
-	local qp = { math_cos(p), 0, math_sin(p), 0 }
-	local qy = { math_cos(y), 0, 0, math_sin(y) }
-	
-	return wrap(quatMul(qy, quatMul(qp, qr)))
+	return wrap(quatFromAngle(ang))
 end
 
-
-
-
+function math_library.slerpQuaternion(from, to, t)
+	checkluatype(t, TYPE_NUMBER)
+	quat1 = unwrap(from)
+	quat2 = unwrap(to)
+	
+	local len = quat1[1] * quat1[1] + quat1[2] * quat1[2] + quat1[3] * quat1[3] + quat1[4] * quat1[4]
+	if len == 0 then
+		return wrap({ 0, 0, 0, 0})
+	else
+		local dot = quat1[1] * quat2[1] + quat1[2] * quat2[2] + quat1[3] * quat2[3] + quat1[4] * quat2[4]
+		local new
+		if dot < 0 then
+			new = { -quat2[1], -quat2[2], -quat2[3], -quat2[4] }
+		else
+			new = quat2
+		end
+		
+		local inv = { quat1[1] / len, -quat1[2] / len, -quat1[3] / len, -quat1[4] / len }
+		local log = quatLog(quatMul(inv, new))
+		local q = quatExp({ log[1] * t, log[2] * t, log[3] * t, log[4] * t })
+		
+		return wrap(quatMul(quat1, q))
+	end
+end
 
 end
 
@@ -526,10 +679,13 @@ V Setting
 V Lookup
 V Meta events
 V Better name for qMod
-~ Documentation
+V math.slerpQuaternion
+Documentation
+Replace rad2deg -> math.deg; deg2rad -> math.rad
 Credits
 Compare all the calculations to E2 to ensure that there were no mistakes during original Starfall rewrite
 Check if quatLong and other functions that use E2's `delta` thingy work correctly without it
+Translate Fizyk's applyTorque example to SF
 Blackmain Divran to test this
 Remove quaternions.txt
 
