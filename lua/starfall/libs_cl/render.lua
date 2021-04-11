5198bbc5af70e68aafd010e2ef496543b794eab3
Garrett Brown <grbrown93@sbcglobal.net>
2021-04-10 23:42:57 -0400

    Fix error

--- lua/starfall/libs_cl/render.lua
+++ lua/starfall/libs_cl/render.lua
@@ -287,7 +287,7 @@ function instance:prepareRender()
 end
 
 function instance:prepareRenderOffscreen()
-	prepareRender()
+	self:prepareRender()
 	renderdata.needRT = true
 	render.PushRenderTarget(dummyrt, 0, 0, 1024, 1024)
 	cam.Start2D()
