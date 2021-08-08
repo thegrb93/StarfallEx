207ac9569015e51dfa8cb722776e18b8f8e57ecf
Vurv <56230599+Vurv78@users.noreply.github.com>
2021-08-08 08:26:44 -0700

    Add lua part of [src] override

--- lua/starfall/editor/docs.lua
+++ lua/starfall/editor/docs.lua
@@ -221,6 +221,20 @@ local parseAttributes = {
 		else
 			ErrorNoHalt("Invalid field doc (" .. value .. ") in file: " .. curfile .. "\n")
 		end
+	end,
+
+	--- Overrides the [src] link which would normally go to the SF github.
+	-- This would be for addons extending SF and wanting to link to their repos.
+	-- The links would be something like https://github.com/User/Repo/.../file.lua
+	-- It will be a direct link to a page and the line number that SF finds will be appended to it.
+	-- Only github links are allowed.
+	["src"] = function(parsing, value)
+		local link = string_match(value, "https://github.com/(.+)")
+		if link then
+			parsing.path = link .. string_match(parsing.path, "(#L[%d%-]+)")
+		else
+			ErrorNoHalt("Invalid src override (" .. value .. ") in file: " .. curfile .. ", make sure it's a github link!\n")
+		end
 	end
 }
 
