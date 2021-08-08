532fd1e52573aa2f247c3b5788d53a8b72cbbb99
Garrett Brown <grbrown93@sbcglobal.net>
2021-08-07 23:58:25 -0400

    Updated source links. Fixes: #1181

--- src/Modules/Links.js
+++ src/Modules/Links.js
@@ -1,8 +1,13 @@
+const IS_SHORTSRC = /^libs_\w+/;
+
 // Returns a link to the github's starfall source code so that the
 // local url given can be used.
 // You get 'properties' from the cards initial args/
-// 'relative' may be something like: libs_sh/math.lua#L124
+// 'path' may be something like libs_sh/math.lua#L124 or <ghpath>/file.lua#L124
 export default function getGitSourceLink(properties) {
-    let relative = properties.ghpath;
-    return "https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/" + relative;
+    let path = properties.ghpath;
+    if ( IS_SHORTSRC.test(path) ) {
+        return "https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/" + path;
+    }
+    return "https://github.com/" + path;
 }
