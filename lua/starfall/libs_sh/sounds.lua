5e0a8208aa73de4c22b271fd7ffe03db86280cc3
Cheatoid <Cheatoid@users.noreply.github.com>
2021-09-08 17:40:02 +0200

    Valve

--- lua/starfall/libs_sh/sounds.lua
+++ lua/starfall/libs_sh/sounds.lua
@@ -187,7 +187,7 @@ function sound_methods:isPlaying()
 end
 
 --- Sets the sound level in dB. Won't work unless the sound is playing.
--- @param number level dB level, see <a href='https://developer.valvesoftware.com/wiki/Soundscripts#SoundLevel'> Vale Dev Wiki</a>, for information on the value to use.
+-- @param number level dB level, see <a href='https://developer.valvesoftware.com/wiki/Soundscripts#SoundLevel'>Valve Dev Wiki</a>, for information on the value to use.
 function sound_methods:setSoundLevel(level)
 	checkluatype(level, TYPE_NUMBER)
 	unwrap(self):SetSoundLevel(math.Clamp(level, 0, 511))
