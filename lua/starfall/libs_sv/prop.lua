7dd4ea1c16d9e75327ee648640ed74a2e59dbe75
Cheatoid <Cheatoid@users.noreply.github.com>
2021-09-30 23:30:03 +0200

    Fixes #1215 (#1222)
    * Fix #1215 (for real)
    
    * Throw after restore, if `pcall` section failed
    
    * Remove the entity too
    
    * This is required in case entity was spawned, but some 'Post' function caused an error

--- lua/starfall/libs_sv/prop.lua
+++ lua/starfall/libs_sv/prop.lua
@@ -612,21 +612,38 @@ function props_library.createSent(pos, ang, class, frozen, data)
 		enttbl.Pos = pos
 		enttbl.Angle = ang
 
-		if sent2._preFactory then
-			sent2._preFactory(ply, enttbl)
-		end
+		-- Temporarily disable SF runningOps, because SENT like E2 rely on string metatable methods!
+		-- E2 calls string:find with colon sugar syntax, this means E2 code ended up calling into string __index metamethod (see sflib.lua where SF does metatable patching).
+		-- In case of E2, it usually results in obscure errors being thrown (such as "error in error handling", random things being nil, etc).
+		local runningOpsBackup = SF.runningOps
+		SF.runningOps = nil
 
-		entity = duplicator.CreateEntityFromTable(ply, enttbl)
+		-- Better be safe, pcall this to ensure we continue running our code, in case these external functions cause an error...
+		local isOk, errorMsg = pcall(function()
+			if sent2._preFactory then
+				sent2._preFactory(ply, enttbl)
+			end
 
-		if sent2._postFactory then
-			sent2._postFactory(ply, entity, enttbl)
-		end
+			entity = duplicator.CreateEntityFromTable(ply, enttbl)
 
-		if entity.PreEntityCopy then
-			entity:PreEntityCopy() -- To build dupe modifiers
-		end
-		if entity.PostEntityPaste then
-			entity:PostEntityPaste(ply, entity, {[entity:EntIndex()] = entity})
+			if sent2._postFactory then
+				sent2._postFactory(ply, entity, enttbl)
+			end
+
+			if entity.PreEntityCopy then
+				entity:PreEntityCopy() -- To build dupe modifiers
+			end
+			if entity.PostEntityPaste then
+				entity:PostEntityPaste(ply, entity, {[entity:EntIndex()] = entity})
+			end
+		end)
+
+		SF.runningOps = runningOpsBackup -- Restore back runningOps to SF control, and everything is fine :)
+		if not isOk then
+			if IsValid(entity) then
+				entity:Remove()
+			end
+			SF.Throw("Failed to create entity: " .. errorMsg, 2)
 		end
 
 		hookcall = "PlayerSpawnedSENT"
