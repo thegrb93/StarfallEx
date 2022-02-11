0d6a3575fcfa007b2b30ae04218f0d41538ebffe
x4fx77x4f <70858634+x4fx77x4f@users.noreply.github.com>
2022-02-11 18:35:05 -0500

    revise money request example may be subject to further radical change

--- lua/starfall/examples/moneyRequest.lua
+++ lua/starfall/examples/moneyRequest.lua
@@ -1,9 +1,11 @@
 --@name Semi-auto gunshop example
 --@shared
+
 -- Connect this chip to a screen and customers will be able to choose an item from the following list.
 -- Once a customer makes a choice, they'll be asked if they want to send you money via a pop-up.
 -- If they click "accept", the money for the product will be transferred to you, and you'll see
 -- the product name and quantity appear in chat.
+
 local products = {
     {name="Desert eagle", price=215},
     {name="Fiveseven", price=205},
@@ -16,85 +18,135 @@ local products = {
     {name="Pump shotgun", price=1750, count=10},
     {name="Mac 10", price=2150, count=10},
 }
+
 if SERVER then
-    local customers = {}
+    -- Because there's a limit on how often we can make money requests,
+    -- we need a FIFO (first in, first out) queue to deal with multiple players buying items in a short time span.
     local queue = {}
-    net.receive('buy', function(_, ply)
-        if customers[ply] then return end
-        local index = net.readUInt(32)
-        local productInfo = products[index]
-        if not productInfo then return end
-        table.insert(queue, {
-            customer = ply,
-            productInfo = productInfo
-        })
-        customers[ply] = true
-    end)
-    hook.add('think', 'queue', function()
-        if not darkrp.canMakeMoneyRequest() then return end
-        local queued = queue[1]
-        if not queued then return end
-        if not isValid(queued.customer) then
+    hook.add("think", "queue", function()
+        while darkrp.canMakeMoneyRequest() do
+            local firstInQueue = queue[1]
+            if not firstInQueue then return end
+            firstInQueue.shop:processTransaction(firstInQueue.ply, firstInQueue.item)
             table.remove(queue, 1)
-            customers[queued.customer] = nil
+        end
+    end)
+
+    local gunShop = class("GunShop")
+    function gunShop:initialize(products)
+        self.customers = {}
+        self.products = products
+    end
+    function gunShop:processTransaction(ply, item)
+        if not ply:isValid() then
+            self.customers[ply] = nil
             return
         end
-        queued.customer:requestMoney(queued.productInfo.name, queued.productInfo.price, function()
-            if isValid(queued.customer) then
-                print(string.format("%q just bought %q (quantity: %d)!", queued.customer:getName(), queued.productInfo.name, queued.productInfo.count or 1))
+        ply:requestMoney(item.name, item.price, function()
+            if ply:isValid() then
+                print(string.format("%q just bought %q (quantity: %d)!", ply:getName(), item.name, item.count or 1))
             end
-            customers[queued.customer] = nil
+            self.customers[ply] = nil
         end, function(reason)
-            if isValid(queued.customer) then
-                print(string.format("Money request for %q failed! Reason: %q", queued.customer:getName(), reason or "nil"))
+            if ply:isValid() then
+                print(string.format("Money request for %q failed! Reason: %q", ply:getName(), reason or "nil"))
             end
-            customers[queued.customer] = nil
+            self.customers[ply] = nil
         end)
-        table.remove(queue, 1)
+    end
+    function gunShop:buy(ply, item)
+        if self.customers[ply] then return end
+        item = self.products[item]
+        if not item then return end
+        table.insert(queue, {
+            ply = ply,
+            item = item,
+            shop = self
+        })
+    end
+
+    local myGunShop = gunShop:new(products)
+    net.receive("buy", function(_, ply)
+        myGunShop:buy(ply, net.readUInt(32))
     end)
-    return
-end
-local y
-local wasPressed = {}
-local function button(str)
-    local retval, isPressed = false, false
-    local cx, cy = render.cursorPos()
-    if not cx or not cy or cx < 20 or cx >= 320 or cy < y or cy >= y+20 then
-        wasPressed[str] = nil
-    else
-        isPressed = player():keyDown(IN_KEY.USE)
-        if isPressed then
-            wasPressed[str] = true
-        elseif wasPressed[str] then
-            -- The key was pressed last frame, but not this frame
-            wasPressed[str] = nil
-            retval = true
+elseif CLIENT then
+    local button = class("Button")
+    function button:initialize(x, y, text, w, h)
+        self.x = x
+        self.y = y
+        self.w = w or 300
+        self.h = h or 20
+        self.text = text
+        self.pressed = false
+    end
+    function button:isHovered(cx, cy)
+        return cx and cx > self.x and cx < self.x+self.w and cy > self.y and cy < self.y+self.h
+    end
+    function button:draw()
+        render.setRGBA(127, 127, 127, 255)
+        render.drawRect(20, self.y+20, 300, 2)
+        if not self.pressed then
+            render.setRGBA(255, 255, 255, 255)
+        end
+        render.drawRect(self.x, self.y, self.w, self.h)
+        render.setRGBA(0, 0, 0, 255)
+        render.setFont("DermaDefault")
+        local offset = self.pressed and 4 or 3
+        render.drawText(self.x+offset, self.y+offset, self.text)
+    end
+    
+    local menu = class("Menu")
+    function menu:initialize(products)
+        self.buttons = {}
+        self.pressed = false
+        for i, item in ipairs(products) do
+            self.buttons[i] = button:new(20, (i-1)*30+20, string.format(
+                "Buy %s for %s (quantity: %d)",
+                item.name,
+                darkrp.formatMoney(item.price),
+                item.count or 1
+            ))
         end
     end
-    render.setRGBA(127, 127, 127, 255)
-    render.drawRect(20, y+20, 300, 2)
-    if not isPressed then
-        render.setRGBA(255, 255, 255, 255)
-    end
-    render.drawRect(20, y, 300, 20)
-    render.setRGBA(0, 0, 0, 255)
-    render.setFont("DermaDefault")
-    render.drawText(30, y+(isPressed and 4 or 3), str)
-    y = y+30
-    return retval
-end
-hook.add('render', 'list', function()
-    y = 20
-    for index, productInfo in ipairs(products) do
-        if button(string.format(
-            "Buy %s for %s (quantity: %d)",
-            productInfo.name,
-            darkrp.formatMoney(productInfo.price),
-            productInfo.count or 1
-        )) then
-            net.start("buy")
-                net.writeUInt(index, 32)
-            net.send()
+    function menu:drawButtons()
+        for i, button in ipairs(self.buttons) do
+            button:draw()
         end
     end
-end)
+    function menu:handleInput()
+        -- Only trigger when the use key is pushed down
+        if player():keyDown(IN_KEY.USE) then
+            if not self.pressed then -- If use was pressed this frame but not last frame
+                self.pressed = true
+
+                local cx, cy = render.cursorPos()
+                if not cx or not cy then return end
+
+                for i, button in ipairs(self.buttons) do
+                    if button:isHovered(cx, cy) then
+                        self:buy(i)
+                        button.pressed = true
+                        break
+                    end
+                end
+            end
+        elseif self.pressed then -- Otherwise, if use was not pressed this frame, but was pressed last frame
+            self.pressed = false
+
+            for i, button in ipairs(self.buttons) do
+                button.pressed = false
+            end
+        end
+    end
+    function menu:buy(index)
+        net.start("buy")
+            net.writeUInt(index, 32)
+        net.send()
+    end
+
+    local myMenu = menu:new(products)
+    hook.add("render", "list", function()
+        myMenu:handleInput()
+        myMenu:drawButtons()
+    end)
+end
