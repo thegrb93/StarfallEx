91267386d787ba28ce0ae4e072ccb1feed64c477
x4fx77x4f <70858634+x4fx77x4f@users.noreply.github.com>
2022-02-11 23:59:15 -0500

    Revert "fix "attempt to index a nil value" error serverside when deinitializing an instance by a blocked user"
    This reverts commit 5003064ae056a3262ddf0a69853f63fe8aa541ea.

--- lua/starfall/instance.lua
+++ lua/starfall/instance.lua
@@ -655,12 +655,9 @@ end
 function SF.Instance:deinitialize()
 	self:RunHook("deinitialize")
 	SF.allInstances[self] = nil
-	local playerInstances = SF.playerInstances[self.player]
-	if playerInstances then
-		playerInstances[self] = nil
-		if not next(playerInstances) then
-			SF.playerInstances[self.player] = nil
-		end
+	SF.playerInstances[self.player][self] = nil
+	if not next(SF.playerInstances[self.player]) then
+		SF.playerInstances[self.player] = nil
 	end
 	self.error = true
 end
