2c0258d93139277537daf60e1ea0f14e43252f9a
thegrb93 <grbrown93@sbcglobal.net>
2022-03-08 23:36:16 -0500

    Forgot angle update

--- lua/starfall/libs_sv/wire.lua
+++ lua/starfall/libs_sv/wire.lua
@@ -166,7 +166,7 @@ local typeToE2Type = {
 	[TYPE_STRING] = function(x) return x, "s" end,
 	[vec_meta] = function(x) return {x[1], x[2], x[3]}, "v" end,
 	[vec2_meta] = function(v) return {v[1], v[2]}, "xv2" end,
-	[ang_meta] = function(x) return {x.p, x.y, x.r}, "a" end,
+	[ang_meta] = function(x) return {x[1], x[2], x[3]}, "a" end,
 	[TYPE_TABLE] = function(x)
 		local meta = debug_getmetatable(x)
 		if typeToE2Type[meta] then return typeToE2Type[meta](x) end
