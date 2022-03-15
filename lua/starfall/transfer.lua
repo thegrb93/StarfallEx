df7c7c3fa1e23404148762797c64299f8287ccb4
thegrb93 <grbrown93@sbcglobal.net>
2022-03-15 00:26:03 -0400

    Cleaner transfer code

--- lua/starfall/transfer.lua
+++ lua/starfall/transfer.lua
@@ -43,41 +43,30 @@ function net.WriteStarfall(sfdata, callback)
 end
 
 function SF.CompressFiles(files)
-	local header = SF.StringStream()
-	header:writeInt32(table.Count(files))
-
-	local filecodes = {}
+	local data = SF.StringStream()
+	data:writeInt16(table.Count(files))
 	for filename, code in pairs(files) do
 		if #filename > 255 then error("File name too large: " .. #filename .. " (max is 255)") end
-		header:writeInt32(#filename)
-		header:write(filename)
-		header:writeInt32(#code)
-		filecodes[#filecodes + 1] = code
+		data:writeInt8(#filename)
+		data:write(filename)
+		data:writeInt32(#code)
+		data:write(code)
 	end
-	local headerdata = header:getString()
-	local headersize = SF.StringStream()
-	headersize:writeInt32(#headerdata)
-	table.insert(filecodes, 1, headersize:getString())
-	table.insert(filecodes, 2, headerdata)
-	filecodes = table.concat(filecodes)
-	if #filecodes > 64000000 then error("Too much file data!") end
-	return util.Compress(filecodes)
+	data = data:getString()
+	if #data > 64000000 then error("Too much file data!") end
+	return util.Compress(data)
 end
 
 function SF.DecompressFiles(data)
-	local files = {}
 	data = util.Decompress(data)
-	local headersize = SF.StringStream(string.sub(data, 1, 4))
-	headersize = headersize:readUInt32()
-	local header = SF.StringStream(string.sub(data, 5, 4+headersize))
-	local headers = {}
-	for i=1, header:readUInt32() do
-		headers[#headers + 1] = {name = header:read(header:readUInt32()), size = header:readUInt32()}
-	end
-	local pos = headersize+5
-	for k, v in pairs(headers) do
-		files[v.name] = string.sub(data, pos, pos+v.size-1)
-		pos = pos + v.size
+	if not data then return end
+	if #data < 8 then return end
+	data = SF.StringStream(data)
+	local files = {}
+	for i=1, data:readUInt16() do
+		local name = data:read(data:readUInt8())
+		local code = data:read(data:readUInt32())
+		files[name] = code
 	end
 	return files
 end
