2e42538506d36740490cadb61f1b1947a6f3680d
Klen_list <49125075+klen-list@users.noreply.github.com>
2022-06-08 23:42:28 +0300

    Added Entity:getSaveTable( showAll ) (#1325)

--- lua/starfall/libs_sh/entities.lua
+++ lua/starfall/libs_sh/entities.lua
@@ -1713,4 +1713,13 @@ function ents_methods:getNearestPoint(pos)
 	return vwrap(getent(self):NearestPoint(vunwrap(pos)))
 end
 
+-- Returns a table of save values for an entity.
+-- These tables are not the same between the client and the server, and different entities may have different fields.
+-- @shared
+-- @param boolean showAll If set, shows all variables, not just the ones for save.
+-- @return table A table containing all save values in key/value format. The value may be a sequential table (starting to 1) if the field in question is an array in engine.
+function ents_methods:getSaveTable(showAll)
+	return instance.Sanitize(getent(self):GetSaveTable(showAll and true or false))
+end
+
 end
