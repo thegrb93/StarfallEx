f1abd326fe45b7177cff617e53b4740eaa0c75a8
thegrb93 <grbrown93@sbcglobal.net>
2023-02-23 19:54:51 -0500

    Update mesh.lua
    Remove old framerate conversion

--- lua/starfall/libs_sh/mesh.lua
+++ lua/starfall/libs_sh/mesh.lua
@@ -632,7 +632,7 @@ SF.Permissions.registerPrivilege("mesh", "Create custom mesh", "Allows users to 
 
 -- 1M triangles is about 195.4M VRAM
 local plyTriangleCount = SF.LimitObject("mesh_triangles", "total mesh triangles", 1000000, "How many triangles total can be loaded for meshes.")
-local plyTriangleRenderBurst = SF.BurstObject("mesh_triangles", "rendered triangles", 50000, 50000, "Number of triangles that can be rendered per frame", "Number of triangles that can be drawn in a short period of time", 60)
+local plyTriangleRenderBurst = SF.BurstObject("mesh_triangles", "rendered triangles", 50000, 50000, "Number of triangles that can be rendered per frame", "Number of triangles that can be drawn in a short period of time")
 local plyMeshCount = SF.LimitObject("mesh", "total meshes", 1000, "How many meshes total can be loaded.")
 
 function plyTriangleRenderBurst:calc(obj)
