56992fcb3d53d26d7de61d6b92c361c29b3f1f4a
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2023-12-07 22:30:33 -0300

    Reverts 053474d
    Workaround is no longer needed.

--- lua/starfall/libs_sh/hook.lua
+++ lua/starfall/libs_sh/hook.lua
@@ -456,17 +456,9 @@ add("PropBreak")
 -- @shared
 -- @param Entity ent The entity that fired the bullet
 -- @param table data The bullet data. See http://wiki.facepunch.com/gmod/Structures/Bullet
--- @return function? Optional callback to called as if it were the Bullet structure's Callback. Called before the bullet deals damage with attacker, traceResult.
 add("EntityFireBullets", nil, function(instance, ent, data)
 	return true, { instance.WrapObject(ent), SF.StructWrapper(instance, data, "Bullet") }
-end, function(instance, ret, ent, data)
-	if ret[1] and isfunction(ret[2]) then
-		data.Callback = function(attacker, tr, dmginfo)
-			instance:runFunction(ret[2], instance.WrapObject(attacker), SF.StructWrapper(instance, tr, "TraceResult"))
-		end
-		return true
-	end
-end, true)
+end)
 
 --- Called whenever a sound has been played. This will not be called clientside if the server played the sound without the client also calling Entity:EmitSound.
 -- @name EntityEmitSound
