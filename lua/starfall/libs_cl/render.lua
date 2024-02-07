08ebe9f76aaba38458ef06501c4183349604682f
Will Stewart <fevenkitsune@gmail.com>
2024-02-07 12:50:41 -0500

    oops

--- lua/starfall/libs_cl/render.lua
+++ lua/starfall/libs_cl/render.lua
@@ -1894,7 +1894,7 @@ end
 -- @param Vector endPos Ending position
 -- @param boolean? writeZ Optional should the line be drawn with depth considered
 function render_library.draw3DLine(startPos, endPos, writeZ)
-	if writeZ ~= nil then checkluatype(writeZ, TYPE_BOOL)
+	if writeZ ~= nil then checkluatype(writeZ, TYPE_BOOL) end
 	if not renderdata.isRendering then SF.Throw("Not in rendering hook.", 2) end
 	startPos = vunwrap(startPos)
 	endPos = vunwrap(endPos)
