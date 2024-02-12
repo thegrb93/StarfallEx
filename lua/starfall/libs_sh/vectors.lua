42ff20d9f218f377d598de4ce4fc2075efc56753
Livelandr <Livelandr@gmail.com>
2024-02-12 15:35:24 +0300

    Get rid of built-in rotate function

--- lua/starfall/libs_sh/vectors.lua
+++ lua/starfall/libs_sh/vectors.lua
@@ -388,26 +388,39 @@ function vec_methods:normalize()
 	self[3] = self[3] / len
 end
 
+local math_sin, math_cos, math_rad = math.sin, math.cos, math.rad
+local deg2rad = math.pi/180
+
 --- Rotate the vector by Angle b.
 -- Self-Modifies. Does not return anything
 -- @param Angle b Angle to rotate by.
 function vec_methods:rotate(b)
-	local vec = unwrap(self)
-	vec:Rotate(aunwrap(b))
+	local vec, ang = unwrap(self), aunwrap(b) 
+	local yaw, pitch, roll = ang.yaw * deg2rad, ang.pitch * deg2rad, ang.roll * deg2rad
+	local ycos, ysin = math_cos(yaw), math_sin(yaw)	
+	local pcos, psin = math_cos(pitch), math_sin(pitch)
+	local rcos, rsin = math_cos(roll), math_sin(roll)
 
-	self[1] = vec.x
-	self[2] = vec.y
-	self[3] = vec.z
+	rawset(self, 1, (vec.x*(ycos*pcos) + vec.y*(ycos*pcos*rcos-ysin*rcos) + vec.z*(ycos*pcos*rcos+ysin*rcos))	)
+	rawset(self, 2, (vec.x*(ysin*pcos) + vec.y*(ysin*pcos*rcos+ycos*rcos) + vec.z*(ysin*pcos*rcos-ycos*rcos)))
+	rawset(self, 3, (vec.x*(-pcos) + vec.y*(pcos*rcos) + vec.z*(pcos*rcos)))
 end
 
 --- Returns Rotated vector by Angle b
 -- @param Angle b Angle to rotate by.
 -- @return Vector Rotated Vector
 function vec_methods:getRotated(b)
-	local vec = unwrap(self)
-	vec:Rotate(aunwrap(b))
+	local vec, ang = unwrap(self), aunwrap(b) 
+	local yaw, pitch, roll = ang.yaw * deg2rad, ang.pitch * deg2rad, ang.roll * deg2rad
+	local ycos, ysin = math_cos(yaw), math_sin(yaw)	
+	local pcos, psin = math_cos(pitch), math_sin(pitch)
+	local rcos, rsin = math_cos(roll), math_sin(roll)
 
-	return wrap({ vec.x, vec.y, vec.z })
+	return wrap({ 
+		(vec.x*(ycos*pcos) + vec.y*(ycos*pcos*rcos-ysin*rcos) + vec.z*(ycos*pcos*rcos+ysin*rcos)), 
+		(vec.x*(ysin*pcos) + vec.y*(ysin*pcos*rcos+ycos*rcos) + vec.z*(ysin*pcos*rcos-ycos*rcos)), 
+		(vec.x*(-pcos) + vec.y*(pcos*rcos) + vec.z*(pcos*rcos))
+	})
 end
 
 --- Returns an arbitrary orthogonal basis from the direction of the vector. Input must be a normalized vector
