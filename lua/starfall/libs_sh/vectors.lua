743521dd7dffb3bce21d69cb6220f4a695c4dbba
Livelandr <Livelandr@gmail.com>
2024-02-13 00:02:55 +0300

    got misinformated, revert

--- lua/starfall/libs_sh/vectors.lua
+++ lua/starfall/libs_sh/vectors.lua
@@ -396,7 +396,8 @@ local deg2rad = math.pi/180
 -- @param Angle b Angle to rotate by.
 function vec_methods:rotate(b)
 	checktype(b, ang_meta)
-	local y, p, r = b[3] * deg2rad, b[1] * deg2rad, b[2] * deg2rad
+	local y, p, r = b[1] * deg2rad, b[2] * deg2rad, b[3] * deg2rad
+
 	local ysin, ycos, psin, pcos, rsin, rcos = math_sin(y), math_cos(y), math_sin(p), math_cos(p), math_sin(r), math_cos(r)
 	local psin_rsin, psin_rcos = psin*rsin, psin*rcos
 	local x, y, z = self[1], self[2], self[3]
@@ -411,7 +412,7 @@ end
 -- @return Vector Rotated Vector
 function vec_methods:getRotated(b)
 	checktype(b, ang_meta)
-	local y, p, r = b[3] * deg2rad, b[1] * deg2rad, b[2] * deg2rad
+	local y, p, r = b[1] * deg2rad, b[2] * deg2rad, b[3] * deg2rad
 	local ysin, ycos, psin, pcos, rsin, rcos = math_sin(y), math_cos(y), math_sin(p), math_cos(p), math_sin(r), math_cos(r)
 	local psin_rsin, psin_rcos = psin*rsin, psin*rcos
 	local x, y, z = self[1], self[2], self[3]
