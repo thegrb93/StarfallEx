251203b1b4809438cdc1a37e0ad1b1e4f39ae09d
thegrb93 <grbrown93@sbcglobal.net>
2024-02-27 20:32:24 -0500

    Fix

--- lua/starfall/libs_sh/hook.lua
+++ lua/starfall/libs_sh/hook.lua
@@ -477,7 +477,7 @@ end, true)
 add("PostEntityFireBullets", nil, function(instance, ent, data)
 	local ret = SF.StructWrapper(instance, data, "Bullet")
 	ret.Trace = SF.StructWrapper(instance, data.Trace, "TraceResult")
-	return true, ret
+	return true, {instance.WrapObject(ent), ret}
 end)
 
 --- Called whenever a sound has been played. This will not be called clientside if the server played the sound without the client also calling Entity:EmitSound.
