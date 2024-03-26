9f2439bc5d140bb0e304ec895db45042fb50b92d
ax255 <66443256+ax255@users.noreply.github.com>
2024-03-26 21:26:33 +0100

    Update sfframe.lua

--- lua/starfall/editor/sfframe.lua
+++ lua/starfall/editor/sfframe.lua
@@ -2083,8 +2083,7 @@ function PANEL:UpdatePlayers(players)
 			local svtotal = 0
 			local cltotal = 0
 			for instance, _ in pairs(SF.playerInstances[ply] or {}) do
-				if not isValid(instance.entity) then continue end
-				svtotal = svtotal + instance.entity:GetNWInt("CPUus")
+				svtotal = svtotal + (instance.entity:IsValid() and instance.entity:GetNWInt("CPUus") or 0)
 				cltotal = cltotal + instance.cpu_average
 			end
 			cpuServerText:SetText(string.format("SV CPU: %3.1f us", svtotal))
