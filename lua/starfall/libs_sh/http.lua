26468995964e2805e1fac0c6b5002ad630301ecb
thegrb93 <grbrown93@sbcglobal.net>
2024-04-27 17:08:16 -0400

    Fixes

--- lua/starfall/libs_sh/http.lua
+++ lua/starfall/libs_sh/http.lua
@@ -143,9 +143,11 @@ end
 -- @param string data The data to convert
 -- @return string The converted data
 function http_library.base64Encode(data)
-	if #data > 64e6 then SF.Throw("String exceeds length limit!", 2) end
 	checkluatype(data, TYPE_STRING)
-	return string.gsub(util.Base64Encode(data),"\n","")
+	if #data > 64e6 then SF.Throw("String exceeds length limit!", 2) end
+	local ret = string.gsub(util.Base64Encode(data),"\n","")
+	instance:checkCpu()
+	return ret
 end
 
 --- Converts data from base64 format
@@ -159,8 +161,8 @@ http_library.base64Decode = util.Base64Decode
 -- @param string data The data to convert
 -- @return string The converted data
 function http_library.urlEncode(data)
-	if #data > 64e3 then SF.Throw("String exceeds length limit!", 2) end
 	checkluatype(data, TYPE_STRING)
+	if #data > 64e3 then SF.Throw("String exceeds length limit!", 2) end
 	data = string.gsub(data, "[^%w_~%.%-%(%)!%*]", function(char)
 		return string.format("%%%02X", string.byte(char))
 	end)
@@ -171,8 +173,8 @@ end
 -- @param string data The data to convert
 -- @return string The converted data
 function http_library.urlDecode(data)
-	if #data > 64e3 then SF.Throw("String exceeds length limit!", 2) end
 	checkluatype(data, TYPE_STRING)
+	if #data > 64e3 then SF.Throw("String exceeds length limit!", 2) end
 	data = string.gsub(data, "%%(..)", function(char)
 		char = tonumber(char, 16)
 		if char==nil or char < 0 or char > 255 then error("Invalid '%' value found: "..char) end
@@ -185,6 +187,7 @@ end
 -- @param string url The url to convert
 -- @return string The converted url
 function http_library.urlGoogleDriveToRaw(url)
+	checkluatype(url, TYPE_STRING)
 	if #url > 64e3 then SF.Throw("String exceeds length limit!", 2) end
 	local id = string.match(url, "https://drive%.google%.com/file/d/([^/]+)/view") or SF.Throw("Failed to parse google drive link!", 2)
 	return "https://drive.google.com/uc?export=download&id="..id
@@ -194,6 +197,7 @@ end
 -- @param string url The url to convert
 -- @return string The converted url
 function http_library.urlDropboxToRaw(url)
+	checkluatype(url, TYPE_STRING)
 	if #url > 64e3 then SF.Throw("String exceeds length limit!", 2) end
 	return string.gsub(url, "www%.dropbox%.com", "dl%.dropboxusercontent%.com")
 end
