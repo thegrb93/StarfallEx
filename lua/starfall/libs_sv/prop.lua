98a6c016698767465772dda0e8a63b0615f2f592
thegrb93 <grbrown93@sbcglobal.net>
2024-05-01 03:55:56 -0400

    data.props

--- lua/starfall/libs_sv/prop.lua
+++ lua/starfall/libs_sv/prop.lua
@@ -31,7 +31,7 @@ return function(instance)
 local checkpermission = instance.player ~= SF.Superuser and SF.Permissions.check or function() end
 
 local propConfig = {clean = true, undo = false, propList = entList}
-instance.props = propConfig
+instance.data.props = propConfig
 
 instance:AddHook("deinitialize", function()
 	entList:deinitialize(instance, propConfig.clean)
