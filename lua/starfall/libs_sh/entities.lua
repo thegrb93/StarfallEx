ce6506eb380d7f9f855ffc1a03a40b649d632a93
ъ знака <111152233+friztailo@users.noreply.github.com>
2024-08-23 05:43:25 +0500

    Add Entity:getRenderBounds() (#1834)
    * Add Entity:getRenderBounds()
    
    * Edit Entity:getRenderBounds()

--- lua/starfall/libs_sh/entities.lua
+++ lua/starfall/libs_sh/entities.lua
@@ -237,12 +237,22 @@ if CLIENT then
 		local ent = getent(self)
 		if not ent.IsSFHologram and not ent.IsSFProp then SF.Throw("The entity isn't a hologram or custom-prop", 2) end
 
-
 		checkpermission(instance, ent, "entities.setRenderProperty")
 
 		mins, maxs = vunwrap(mins), vunwrap(maxs)
 		ent:SetRenderBounds(mins, maxs)
 		ent.sf_userrenderbounds = {mins, maxs}
+	end
+
+	--- Returns render bounds of the entity as local vectors
+	-- If the render bounds are not inside players view, the entity will not be drawn!
+	-- @client
+	-- @return Vector The minimum vector of the bounds
+	-- @return Vector The maximum vector of the bounds
+	function ents_methods:getRenderBounds()
+		local ent = getent(self)
+		local mins, maxs = ent:GetRenderBounds()
+		return vwrap(mins), vwrap(maxs)
 	end
 
 	--- Sets the Level Of Detail model to use with this entity. This may not work for all models if the model doesn't include any LOD sub models.
