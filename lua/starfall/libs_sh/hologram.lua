d0f2285cdd6fa0165e2bd707a188454106561a35
thegrb93 <grbrown93@sbcglobal.net>
2024-10-28 15:03:47 -0400

    Add rendergroup check to network func (#1884)

--- lua/starfall/libs_sh/hologram.lua
+++ lua/starfall/libs_sh/hologram.lua
@@ -500,14 +500,7 @@ function hologram_methods:setCullMode(mode)
 	holo:SetCullMode(mode==1)
 end
 
-local allowedRenderGroups = {
-[RENDERGROUP_OPAQUE]=true,
-[RENDERGROUP_TRANSLUCENT]=true,
-[RENDERGROUP_BOTH]=true,
-[RENDERGROUP_VIEWMODEL]=true,
-[RENDERGROUP_VIEWMODEL_TRANSLUCENT]=true,
-[RENDERGROUP_OPAQUE_BRUSH]=true,
-}
+
 --- Set the render group for a hologram.
 -- @shared
 -- @param number|nil group Render group. If unset, the engine will decide the render group based on the entity's materials. Can be RENDERGROUP.OPAQUE RENDERGROUP.TRANSLUCENT RENDERGROUP.BOTH RENDERGROUP.VIEWMODEL RENDERGROUP.VIEWMODEL.TRANSLUCENT RENDERGROUP.OPAQUE.BRUSH
@@ -517,7 +510,7 @@ function hologram_methods:setRenderGroup(group)
 	
 	if group then
 		checkluatype(group, TYPE_NUMBER)
-		if not allowedRenderGroups[group] then SF.Throw("Invalid rendergroup!") end
+		if not SF.allowedRenderGroups[group] then SF.Throw("Invalid rendergroup!") end
 		holo:SetRenderGroupInternal(group)
 	else
 		holo:SetRenderGroupInternal(-1)
