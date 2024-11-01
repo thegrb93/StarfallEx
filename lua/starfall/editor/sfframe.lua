7100a5fc53d05b9a093e22c72795fd8c81b243a5
thegrb93 <grbrown93@sbcglobal.net>
2024-11-01 00:49:26 -0400

    Fix

--- lua/starfall/editor/sfframe.lua
+++ lua/starfall/editor/sfframe.lua
@@ -2094,7 +2094,7 @@ end
 function PANEL:CheckPlayersChanged()
 	local players = {}
 	for k, v in pairs(player.GetAll()) do
-		if not rawget(SF.playerInstances, v) or SF.BlockedUsers:isBlocked(v:SteamID()) then
+		if rawget(SF.playerInstances, v) or SF.BlockedUsers:isBlocked(v:SteamID()) then
 			players[v] = true
 		end
 	end
