ce59a261cc9ebe924a58634f1a258947edb0f7d8
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-03-24 13:03:47 -0600

    Fix render.clearDepth() (#2026)
    * Use clearStencil param, don't require RT
    
    * Only allow if in RT or using a HUD

--- lua/starfall/libs_cl/render.lua
+++ lua/starfall/libs_cl/render.lua
@@ -1994,11 +1994,13 @@ function render_library.setBlend(alpha)
 	render.SetBlend(alpha)
 end
 
---- Resets the depth buffer
-function render_library.clearDepth()
+--- Resets the depth buffer. (Only works in a render target or with a connected HUD)
+-- @param boolean? clearStencil Also clears the stencil buffer. Default: true
+function render_library.clearDepth( clearStencil )
 	if not renderdata.isRendering then SF.Throw("Not in a rendering hook.", 2) end
-	if renderdata.usingRT then
-		render.ClearDepth()
+
+	if renderdata.usingRT or SF.IsHUDActive(instance.entity) then
+		render.ClearDepth( clearStencil )
 	end
 end
 
