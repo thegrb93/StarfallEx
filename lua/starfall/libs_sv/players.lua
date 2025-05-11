9043ba30c9ffb3a61fb74b71631d43d45a2abf1a
thegrb93 <grbrown93@sbcglobal.net>
2025-05-11 18:41:07 -0400

    Fix indents

--- lua/starfall/libs_sv/players.lua
+++ lua/starfall/libs_sv/players.lua
@@ -34,49 +34,49 @@ local vunwrap1, vunwrap2
 local aunwrap1
 instance:AddHook("initialize", function()
 	getent = ent_meta.GetEntity
-    getply = player_meta.GetPlayer
+	getply = player_meta.GetPlayer
 	player_meta.__tostring = ent_meta.__tostring
 	vunwrap1, vunwrap2 = vec_meta.QuickUnwrap1, vec_meta.QuickUnwrap2
 	aunwrap1 = ang_meta.QuickUnwrap1
 end)
 
 instance:AddHook("deinitialize", function()
-    for k, ply in pairs(player.GetAll()) do
-        if instance.data.viewEntityChanged then
-            Ply_SetViewEntity(ply)
-        end
-    end
+	for k, ply in pairs(player.GetAll()) do
+		if instance.data.viewEntityChanged then
+			Ply_SetViewEntity(ply)
+		end
+	end
 end)
 
 --- Lets you change the size of yourself if the server has sf_permissions_entity_owneraccess 1
 -- @param number scale The scale to apply, will be truncated to the first two decimal places (min 0.01, max 100)
 function player_methods:setModelScale(scale)
-    checkvalidnumber(scale)
-    local ply = getply(self)
-    checkpermission(instance, ply, "entities.setRenderProperty")
-    Ent_SetModelScale(ply, math.Clamp(math.Truncate(scale, 2), 0.01, playerMaxScale:GetFloat()))
+	checkvalidnumber(scale)
+	local ply = getply(self)
+	checkpermission(instance, ply, "entities.setRenderProperty")
+	Ent_SetModelScale(ply, math.Clamp(math.Truncate(scale, 2), 0.01, playerMaxScale:GetFloat()))
 end
 
 --- Checks if the player is connected to a HUD component that's linked to this chip
 -- @return boolean True if a HUD component is connected and active for the player, nil otherwise
 function player_methods:isHUDActive()
-    return SF.IsHUDActive(instance.entity, getply(self))
+	return SF.IsHUDActive(instance.entity, getply(self))
 end
 
 --- Sets the view entity of the player. Only works if they are linked to a hud.
 -- @param Entity ent Entity to set the player's view entity to, or nothing to reset it
 function player_methods:setViewEntity(ent)
-    local ply = getply(self)
-    if ent~=nil then ent = getent(ent) end
-    if not SF.IsHUDActive(instance.entity, ply) then SF.Throw("Player isn't connected to HUD!", 2) end
-    instance.data.viewEntityChanged = ent ~= nil and ent ~= ply
-    Ply_SetViewEntity(ply, ent)
+	local ply = getply(self)
+	if ent~=nil then ent = getent(ent) end
+	if not SF.IsHUDActive(instance.entity, ply) then SF.Throw("Player isn't connected to HUD!", 2) end
+	instance.data.viewEntityChanged = ent ~= nil and ent ~= ply
+	Ply_SetViewEntity(ply, ent)
 end
 
 --- Returns whether or not the player has godmode
 -- @return boolean True if the player has godmode
 function player_methods:hasGodMode()
-    return Ply_HasGodMode(getply(self))
+	return Ply_HasGodMode(getply(self))
 end
 
 --- Drops the player's weapon
@@ -84,100 +84,100 @@ end
 -- @param Vector? target If set, launches the weapon at the given position
 -- @param Vector? velocity If set and target is unset, launches the weapon with the given velocity
 function player_methods:dropWeapon(weapon, target, velocity)
-    local ply = getply(self)
-    checkpermission(instance, ply, "player.dropweapon")
+	local ply = getply(self)
+	checkpermission(instance, ply, "player.dropweapon")
 
-    if target~=nil then target = vunwrap1(target) end
-    if velocity~=nil then velocity = vunwrap2(velocity) end
+	if target~=nil then target = vunwrap1(target) end
+	if velocity~=nil then velocity = vunwrap2(velocity) end
 
-    if isstring(weapon) then
-        Ply_DropNamedWeapon(ply, weapon, target, velocity)
-    else
-        weapon = wunwrap(weapon)
-        Ply_DropWeapon(ply, weapon, target, velocity)
-    end
+	if isstring(weapon) then
+		Ply_DropNamedWeapon(ply, weapon, target, velocity)
+	else
+		weapon = wunwrap(weapon)
+		Ply_DropWeapon(ply, weapon, target, velocity)
+	end
 end
 
 --- Strips the player's weapon
 -- @param string weapon The weapon class name of the weapon to strip
 function player_methods:stripWeapon(weapon)
-    local ply = getply(self)
-    checkpermission(instance, ply, "player.dropweapon")
-    checkluatype(weapon, TYPE_STRING)
-    Ply_StripWeapon(ply, weapon)
+	local ply = getply(self)
+	checkpermission(instance, ply, "player.dropweapon")
+	checkluatype(weapon, TYPE_STRING)
+	Ply_StripWeapon(ply, weapon)
 end
 
 --- Strips all the player's weapons
 function player_methods:stripWeapons()
-    local ply = getply(self)
-    checkpermission(instance, ply, "player.dropweapon")
-    Ply_StripWeapons(ply)
+	local ply = getply(self)
+	checkpermission(instance, ply, "player.dropweapon")
+	Ply_StripWeapons(ply)
 end
 
 --- Sets the player's ammo
 -- @param number amount The ammo value
 -- @param number|string ammoType Ammo type id or name
 function player_methods:setAmmo(amount, ammoType)
-    local ply = getply(self)
-    checkpermission(instance, ply, "player.setammo")
+	local ply = getply(self)
+	checkpermission(instance, ply, "player.setammo")
 
-    checkvalidnumber(amount)
-    if not (isstring(ammoType) or isnumber(ammoType)) then
-        SF.ThrowTypeError("number or string", SF.GetType(ammoType), 2)
-    end
+	checkvalidnumber(amount)
+	if not (isstring(ammoType) or isnumber(ammoType)) then
+		SF.ThrowTypeError("number or string", SF.GetType(ammoType), 2)
+	end
 
-    Ply_SetAmmo(ply, amount, ammoType)
+	Ply_SetAmmo(ply, amount, ammoType)
 end
 
 --- Removes all a player's ammo
 function player_methods:stripAmmo()
-    local ply = getply(self)
-    checkpermission(instance, ply, "player.setammo")
-    Ply_StripAmmo(ply)
+	local ply = getply(self)
+	checkpermission(instance, ply, "player.setammo")
+	Ply_StripAmmo(ply)
 end
 
 --- Returns the hitgroup where the player was last hit.
 -- @return number Hitgroup, see https://wiki.facepunch.com/gmod/Enums/HITGROUP
 function player_methods:lastHitGroup()
-    return Ply_LastHitGroup(getply(self))
+	return Ply_LastHitGroup(getply(self))
 end
 
 --- Sets a player's eye angles
 -- @param Angle ang New angles
 function player_methods:setEyeAngles(ang)
-    local ent = getent(self)
-    checkpermission(instance, ent, "entities.setEyeAngles")
-    Ply_SetEyeAngles(ent, aunwrap1(ang))
+	local ent = getent(self)
+	checkpermission(instance, ent, "entities.setEyeAngles")
+	Ply_SetEyeAngles(ent, aunwrap1(ang))
 end
 
 --- Returns the packet loss of the client
 -- @return number Packets lost
 function player_methods:getPacketLoss()
-    return Ply_PacketLoss(getply(self))
+	return Ply_PacketLoss(getply(self))
 end
 
 --- Returns the time in seconds since the player connected
 -- @return number Time connected
 function player_methods:getTimeConnected()
-    return Ply_TimeConnected(getply(self))
+	return Ply_TimeConnected(getply(self))
 end
 
 --- Returns the number of seconds that the player has been timing out for
 -- @return number Timeout seconds
 function player_methods:getTimeoutSeconds()
-    return Ply_GetTimeoutSeconds(getply(self))
+	return Ply_GetTimeoutSeconds(getply(self))
 end
 
 --- Returns true if the player is timing out
 -- @return boolean isTimingOut
 function player_methods:isTimingOut()
-    return Ply_IsTimingOut(getply(self))
+	return Ply_IsTimingOut(getply(self))
 end
 
 --- Returns whether the player is connected
 -- @return boolean True if player is connected
 function player_methods:isConnected()
-    return Ply_IsConnected(getply(self))
+	return Ply_IsConnected(getply(self))
 end
 
 --- Forces the player to say the first argument
@@ -185,149 +185,149 @@ end
 -- @param string text The text to force the player to say
 -- @param boolean? teamOnly Team chat only?, Defaults to false.
 function player_methods:say(text, teamOnly)
-    checkluatype(text, TYPE_STRING)
-    if teamOnly~=nil then checkluatype(teamOnly, TYPE_BOOL) end
-    local ply = getply(self)
-    if instance.player ~= ply then SF.Throw("Player say can only be used on yourself!", 2) end
-    if CurTime() < (ply.sf_say_cd or 0) then SF.Throw("Player say must wait 0.5s between calls!", 2) end
-    ply.sf_say_cd = CurTime() + 0.5
-    Ply_Say(ply, text, teamOnly)
+	checkluatype(text, TYPE_STRING)
+	if teamOnly~=nil then checkluatype(teamOnly, TYPE_BOOL) end
+	local ply = getply(self)
+	if instance.player ~= ply then SF.Throw("Player say can only be used on yourself!", 2) end
+	if CurTime() < (ply.sf_say_cd or 0) then SF.Throw("Player say must wait 0.5s between calls!", 2) end
+	ply.sf_say_cd = CurTime() + 0.5
+	Ply_Say(ply, text, teamOnly)
 end
 
 --- Sets the armor of the player.
 -- @param number newarmor New armor value.
 function player_methods:setArmor(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.setArmor")
-    checkvalidnumber(val)
-    Ply_SetArmor(ent, val)
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.setArmor")
+	checkvalidnumber(val)
+	Ply_SetArmor(ent, val)
 end
 
 --- Sets the maximum armor for player. You can still set a player's armor above this amount with Player:setArmor.
 -- @param number newmaxarmor New max armor value.
 function player_methods:setMaxArmor(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.setMaxArmor")
-    checkvalidnumber(val)
-    Ply_SetMaxArmor(ent, val)
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.setMaxArmor")
+	checkvalidnumber(val)
+	Ply_SetMaxArmor(ent, val)
 end
 
 --- Sets Crouched Walk Speed
 -- @param number newcwalkspeed New Crouch Walk speed, This is a multiplier from 0 to 1.
 function player_methods:setCrouchedWalkSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetCrouchedWalkSpeed(ent, math.Clamp(val,0,1))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetCrouchedWalkSpeed(ent, math.Clamp(val,0,1))
 end
 
 --- Sets Duck Speed
 -- @param number newduckspeed New Duck speed, This is a multiplier from 0 to 1.
 function player_methods:setDuckSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetDuckSpeed(ent, math.Clamp(val,0.005,0.995))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetDuckSpeed(ent, math.Clamp(val,0.005,0.995))
 end
 
 --- Sets UnDuck Speed
 -- @param number newunduckspeed New UnDuck speed, This is a multiplier from 0 to 1.
 function player_methods:setUnDuckSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetUnDuckSpeed(ent, math.Clamp(val,0.005,0.995))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetUnDuckSpeed(ent, math.Clamp(val,0.005,0.995))
 end
 
 --- Sets Ladder Climb Speed, probably unstable
 -- @param number newladderclimbspeed New Ladder Climb speed.
 function player_methods:setLadderClimbSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetLadderClimbSpeed(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetLadderClimbSpeed(ent, math.max(val,0))
 end
 
 --- Sets Max Speed
 -- @param number newmaxspeed New Max speed.
 function player_methods:setMaxSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetMaxSpeed(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetMaxSpeed(ent, math.max(val,0))
 end
 
 --- Sets Run Speed ( +speed )
 -- @param number newrunspeed New Run speed.
 function player_methods:setRunSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetRunSpeed(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetRunSpeed(ent, math.max(val,0))
 end
 
 --- Sets Slow Walk Speed ( +walk )
 -- @param number newslowwalkspeed New Slow Walk speed.
 function player_methods:setSlowWalkSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetSlowWalkSpeed(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetSlowWalkSpeed(ent, math.max(val,0))
 end
 
 --- Sets Walk Speed
 -- @param number newwalkspeed New Walk speed.
 function player_methods:setWalkSpeed(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetWalkSpeed(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetWalkSpeed(ent, math.max(val,0))
 end
 
 --- Sets Jump Power
 -- @param number newjumppower New Jump Power.
 function player_methods:setJumpPower(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetJumpPower(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetJumpPower(ent, math.max(val,0))
 end
 
 --- Sets Step Size
 -- @param number newstepsize New Step Size.
 function player_methods:setStepSize(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ply_SetStepSize(ent, math.max(val,0))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ply_SetStepSize(ent, math.max(val,0))
 end
 
 --- Sets Friction
 -- @param number newfriction New Friction.
 function player_methods:setFriction(val)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.modifyMovementProperties")
-    checkvalidnumber(val)
-    Ent_SetFriction(ent, math.Clamp(val/cvars.Number("sv_friction"),0,10))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.modifyMovementProperties")
+	checkvalidnumber(val)
+	Ent_SetFriction(ent, math.Clamp(val/cvars.Number("sv_friction"),0,10))
 end
 
 --- Kills the target.
 --- Requires 'entities.setHealth' permission.
 function player_methods:kill()
-    local ent = getply(self)
-    checkpermission(instance, ent, "entities.setHealth")
-    if Ply_Alive(ent) then
-        Ply_Kill(ent)
-    end
+	local ent = getply(self)
+	checkpermission(instance, ent, "entities.setHealth")
+	if Ply_Alive(ent) then
+		Ply_Kill(ent)
+	end
 end
 
 --- Attempts to force the target into a vehicle.
 --- Requires 'player.enterVehicle' permission on the player.
 -- @param Vehicle vehicle
 function player_methods:enterVehicle(vehicle)
-    local ent = getply(self)
-    checkpermission(instance, ent, "player.enterVehicle")
-    Ply_EnterVehicle(ent, vhunwrap(vehicle))
+	local ent = getply(self)
+	checkpermission(instance, ent, "player.enterVehicle")
+	Ply_EnterVehicle(ent, vhunwrap(vehicle))
 end
 
 end
