d3b97e06a1502db9c8568cad0b792ad14621f617
thegrb93 <grbrown93@sbcglobal.net>
2025-05-19 01:45:20 -0400

    Add driverUse function

--- lua/starfall/libs_sh/vehicles.lua
+++ lua/starfall/libs_sh/vehicles.lua
@@ -1,16 +1,18 @@
 -- Global to all starfalls
 local checkluatype = SF.CheckLuaType
 local registerprivilege = SF.Permissions.registerPrivilege
-local ENT_META = FindMetaTable("Entity")
-local PLY_META = FindMetaTable("Player")
-local VEH_META = FindMetaTable("Vehicle")
+local dgetmeta = debug.getmetatable
+local ENT_META,PLY_META,VEH_META = FindMetaTable("Entity"),FindMetaTable("Player"),FindMetaTable("Vehicle")
 
+local sf_max_driveruse_dist
 if SERVER then
 	-- Register privileges
 	registerprivilege("vehicle.eject", "Vehicle eject", "Removes a driver from vehicle", { entities = {} })
 	registerprivilege("vehicle.kill", "Vehicle kill", "Kills a driver in vehicle", { entities = {} })
 	registerprivilege("vehicle.strip", "Vehicle strip", "Strips weapons from a driver in vehicle", { entities = {} })
 	registerprivilege("vehicle.lock", "Vehicle lock", "Allow vehicle locking/unlocking", { entities = {} })
+
+	sf_max_driveruse_dist = CreateConVar("sf_vehicle_use_distance", 100, FCVAR_ARCHIVE, "The max reach distance allowed for Vehicle:driverUse function.")
 end
 
 
@@ -23,15 +25,19 @@ SF.RegisterType("Vehicle", false, true, VEH_META, "Entity")
 
 return function(instance)
 local checkpermission = instance.player ~= SF.Superuser and SF.Permissions.check or function() end
-local Ent_Fire,Ent_IsValid = ENT_META.Fire,ENT_META.IsValid
-local Ply_ExitVehicle,Ply_Kill,Ply_StripWeapon,Ply_StripWeapons = PLY_META.ExitVehicle,PLY_META.Kill,PLY_META.StripWeapon,PLY_META.StripWeapons
+local Ent_Fire,Ent_GetPos,Ent_IsValid,Ent_Use = ENT_META.Fire,ENT_META.GetPos,ENT_META.IsValid,ENT_META.Use
+local Ply_ExitVehicle,Ply_GetShootPos,Ply_Kill,Ply_StripWeapon,Ply_StripWeapons = PLY_META.ExitVehicle,PLY_META.GetShootPos,PLY_META.Kill,PLY_META.StripWeapon,PLY_META.StripWeapons
 local Veh_GetDriver,Veh_GetPassenger = VEH_META.GetDriver,VEH_META.GetPassenger
+
+local function Ent_IsVehicle(ent) return dgetmeta(ent)==VEH_META end
 
 local vehicle_methods, vehicle_meta, wrap, unwrap = instance.Types.Vehicle.Methods, instance.Types.Vehicle, instance.Types.Vehicle.Wrap, instance.Types.Vehicle.Unwrap
 local ent_meta, ewrap, eunwrap = instance.Types.Entity, instance.Types.Entity.Wrap, instance.Types.Entity.Unwrap
 local pwrap = instance.Types.Player.Wrap
 
+local getent
 instance:AddHook("initialize", function()
+	getent = ent_meta.GetEntity
 	vehicle_meta.__tostring = ent_meta.__tostring
 end)
 
@@ -115,6 +121,26 @@ if SERVER then
 		hook.Remove("CanExitVehicle", "SF_CanExitVehicle"..ent:EntIndex())
 		Ent_Fire(ent, "Unlock")
 	end
+
+	--- Simulate a Use action on the entity by the driver
+	-- @param Entity ent The entity to be used.
+	-- @param number? usetype The USE_ enum use type. (Default: USE_ON)
+	-- @param number? value The use value (Default: 0)
+	function vehicle_methods:driverUse(ent, usetype, value)
+		ent = getent(ent)
+		if Ent_IsVehicle(ent) then return end -- Prevent source engine bug when using vehicle while in a vehicle
+		local driver = Veh_GetDriver(getveh(self))
+		if not Ent_IsValid(driver) then return end
+		if usetype~=nil then checkluatype(usetype, TYPE_NUMBER) end
+		if value~=nil then checkluatype(value, TYPE_NUMBER) end
+
+		if Ply_GetShootPos(driver):DistToSqr(Ent_GetPos(ent)) > sf_max_driveruse_dist:GetFloat()^2 then SF.Throw("Entity is greater than "..sf_max_driveruse_dist:GetFloat().." units from the player", 2) end
+
+		checkpermission(instance, ent, "entities.use")
+
+		Ent_Use(ent, driver, instance.entity, usetype, value)
+	end
+
 end
 
 end
