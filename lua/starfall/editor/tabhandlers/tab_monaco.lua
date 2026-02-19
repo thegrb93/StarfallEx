cbcb5ec0d3be158449d234498bd3a468207073c2
thegrb93 <grbrown93@sbcglobal.net>
2026-02-19 13:09:06 -0500

    Start monaco editor integrating

---
+++ lua/starfall/editor/tabhandlers/tab_monaco.lua
@@ -0,0 +1,149 @@
+
+local TabHandler = {
+	ControlName = "sf_monaco_editor", -- Its name of vgui panel used by handler, there has to be one
+	IsEditor = true,
+	Description = "Monaco editor"
+ }
+local PANEL = {} -- It's our VGUI
+
+-------------------------------
+-- Handler part (Tab Handler)
+-------------------------------
+
+function TabHandler:Init() -- It's caled when editor is initalized, you can create library map there etc
+end
+
+function TabHandler:RegisterSettings() -- Setting panels should be registered there
+
+end
+
+function TabHandler:Cleanup() -- Called when editor is reloaded/removed
+end
+
+function TabHandler:OnThemeChange()
+end
+
+-----------------------
+-- VGUI part (content)
+-----------------------
+function PANEL:Init() --That's init of VGUI like other PANEL:Methods(), separate for each tab
+
+	local theme = SF.Editor.Themes.CurrentTheme
+
+	--Background
+	self:SetPaintBackground( true )
+	self:SetBackgroundColor( theme.background )
+
+	--Left Panel
+	self.htmlPanel = vgui.Create("DHTML",self)
+	self.htmlPanel:Dock(FILL)
+    self.htmlPanel:SetHTML(
+[[
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<style>
+html, body { height: 100%; margin: 0; overflow: hidden; }
+body { display: flex; flex-direction: column; }
+#editor { flex-grow: 1; border: solid 1px gray; overflow: hidden; }
+</style>
+</head>
+<body>
+<div id="editor"></div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/vs/loader.min.js"></script>
+<script>
+require.config({ paths: { "vs": "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/vs/" }});
+
+window.MonacoEnvironment = {
+    getWorkerUrl: function(workerId, label) {
+        return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+            self.MonacoEnvironment = { baseUrl: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/" };
+            importScripts("https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/vs/base/worker/workerMain.min.js");`
+        )}`;
+    }
+};
+
+var sfPreInitCode = "";
+function sfSetCode(str) {sfPreInitCode = str;}
+function sfGetCode() {return "";}
+
+require(["vs/editor/editor.main"], function () {
+    var editor = monaco.editor.create(document.getElementById("editor"), {
+        value: sfPreInitCode,
+        language: "lua",
+        theme: "vs-dark"
+    });
+
+    const editorElement = document.getElementById("editor");
+    window.addEventListener("resize", () => editor.layout({
+        width: editorElement.offsetWidth,
+        height: editorElement.offsetHeight
+    }));
+
+    function sfSetCode(str) {editor.setValue(str);}
+    function sfGetCode() {sf.getCode(editor.getValue());}
+    sf.doneLoading();
+    sfPreInitCode = null;
+});
+</script>
+</body>
+</html>
+
+]]
+    )
+    self.htmlPanel:AddFunction( "sf", "getCode", function( str )
+        self.innerCode = str
+    end)
+
+    self.isLoaded = false
+    self.htmlPanel:AddFunction( "sf", "doneLoading", function( val )
+        self.isLoaded = true
+    end)
+    self.innerCode = ""
+
+    local function reloadInit()
+        if not self:IsValid() or not self.htmlPanel:IsValid() then return end
+        if self.isLoaded then
+            self:SetCode(self.innerCode)
+            return true
+        else
+            timer.Simple(0.5, reloadInit)
+            return false
+        end
+    end
+    if not reloadInit() then timer.Simple(0.5, reloadInit) end
+end
+
+function PANEL:OnThemeChange(theme)
+end
+
+function PANEL:OnFocusChanged(gained) -- When this tab is opened
+
+end
+
+function PANEL:Validate(movecarret) -- Validate request, has to return success,message
+
+end
+
+function PANEL:GetCode()
+    if self.isLoaded then
+	    self.htmlPanel:RunJavascript("sfGetCode();")
+    end
+    return self.innerCode
+end
+
+function PANEL:SetCode(text)
+	text = SF.Editor.normalizeCode(text)
+    self.innerCode = text
+    if self.isLoaded then
+        self.htmlPanel:RunJavascript("sfSetCode(\""..string.JavascriptSafe(text).."\");")
+    end
+end
+
+--------------
+-- We're done
+--------------
+vgui.Register(TabHandler.ControlName, PANEL, "DPanel") -- Registering VGUI element of handler
+return TabHandler -- Our file has to return table of handler
