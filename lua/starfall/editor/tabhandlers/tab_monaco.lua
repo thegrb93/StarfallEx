a0f74e62c5572ae3cf9efec823aeb3ce096dc7d0
thegrb93 <grbrown93@sbcglobal.net>
2026-02-20 18:06:07 -0500

    Update tab_monaco.lua

--- lua/starfall/editor/tabhandlers/tab_monaco.lua
+++ lua/starfall/editor/tabhandlers/tab_monaco.lua
@@ -1,43 +1,119 @@
+----------------------------------------------------
+-- Monaco TabHandler
+----------------------------------------------------
+
+CreateClientConVar("sf_editor_monaco_wordwrap", 1, true, false)
+CreateClientConVar("sf_editor_monaco_linenumbers", 1, true, false)
+CreateClientConVar("sf_editor_monaco_invisiblecharacters", 0, true, false)
+CreateClientConVar("sf_editor_monaco_indentguides", 1, true, false)
+CreateClientConVar("sf_editor_monaco_autocompletion", 1, true, false)
+CreateClientConVar("sf_editor_monaco_fixconsolebug", 1, true, false)
+CreateClientConVar("sf_editor_monaco_disablelinefolding", 0, true, false)
+CreateClientConVar("sf_editor_monaco_fontsize", 13, true, false)
+
+----------------
+-- Handler part
+----------------
 
 local TabHandler = {
-	ControlName = "sf_monaco_editor", -- Its name of vgui panel used by handler, there has to be one
+	ControlName = "sf_tab_monaco",
 	IsEditor = true,
-	Description = "Monaco editor"
- }
-local PANEL = {} -- It's our VGUI
+	Description = "Monaco Editor",
+}
 
--------------------------------
--- Handler part (Tab Handler)
--------------------------------
-
-function TabHandler:Init() -- It's caled when editor is initalized, you can create library map there etc
+function TabHandler:UpdateSettings()
+	if not self.loaded then return end
+	self.html:QueueJavascript([[
+		editor.updateOptions({
+			lineNumbers: "]]..(GetConVarBool("sf_editor_monaco_linenumbers") and "on" or "off")..[[",
+		});
+	]])
 end
 
-function TabHandler:RegisterSettings() -- Setting panels should be registered there
+function TabHandler:SetEditorTab(tab)
+	self.html:SetParent(tab)
+	--self.html.OnShortcut = function(_, code) tab:OnShortcut(code) end
 
+	tab:DockPadding(0, 0, 0, 0)
+	self.html:DockMargin(0, 0, 0, 0)
+	self.html:Dock(FILL)
+	self.html:SetVisible(true)
+	self.html:RequestFocus()
 end
 
-function TabHandler:Cleanup() -- Called when editor is reloaded/removed
+function TabHandler:RemoveEditorTab(tab)
+	if self.html:GetParent() == tab then
+		self.html:SetVisible(false)
+		self.html:SetParent(nil)
+	end
 end
 
-function TabHandler:OnThemeChange()
+function TabHandler:SetCode(tab)
+	if not self.loaded then return end
+	self.code = tab.code
+	self.htmlPanel:RunJavascript("window.sfSetCode(\""..string.JavascriptSafe(tab.code).."\");")
 end
 
------------------------
--- VGUI part (content)
------------------------
-function PANEL:Init() --That's init of VGUI like other PANEL:Methods(), separate for each tab
+function TabHandler:GetCode(tab)
+	if not self.loaded then return end
+	self.html:RunJavascript("window.sfGetCode();")
+	tab.code = self.code
+end
 
-	local theme = SF.Editor.Themes.CurrentTheme
+function TabHandler:SaveTab(saveas)
+end
 
-	--Background
-	self:SetPaintBackground( true )
-	self:SetBackgroundColor( theme.background )
+function TabHandler:RegisterSettings()
+	local scrollPanel = vgui.Create("DScrollPanel")
+	scrollPanel:Dock(FILL)
+	scrollPanel:SetPaintBackgroundEnabled(false)
 
-	--Left Panel
-	self.htmlPanel = vgui.Create("DHTML",self)
-	self.htmlPanel:Dock(FILL)
-	self.htmlPanel:SetHTML(
+	local form = vgui.Create("DForm", scrollPanel)
+	form:Dock(FILL)
+	form:DockPadding(0, 10, 0, 10)
+	form.Header:SetVisible(false)
+	form.Paint = function () end
+
+	local function setDoClick(panel)
+		panel:SetDark(false)
+		panel.OnChange = function() self:UpdateSettings() end
+		return panel
+	end
+
+	local function setWang(wang, label)
+		wang.OnValueChanged = function() self:UpdateSettings() end
+		wang:GetParent():DockPadding(10, 1, 10, 1)
+		wang:Dock(RIGHT)
+		label:SetDark(false)
+		return wang, label
+	end
+
+	setWang(form:NumberWang("Font size", "sf_editor_monaco_fontsize", 5, 40))
+	setDoClick(form:CheckBox("Enable word wrap", "sf_editor_monaco_wordwrap"))
+	setDoClick(form:CheckBox("Show line numbers", "sf_editor_monaco_linenumbers"))
+	setDoClick(form:CheckBox("Show invisible characters", "sf_editor_monaco_invisiblecharacters"))
+	setDoClick(form:CheckBox("Show indenting guides", "sf_editor_monaco_indentguides"))
+	setDoClick(form:CheckBox("Auto completion", "sf_editor_monaco_autocompletion"))
+	setDoClick(form:CheckBox("Fix console bug", "sf_editor_monaco_fixconsolebug")):SetTooltip("Fix console opening when pressing ' or @ (UK Keyboad layout)")
+	setDoClick(form:CheckBox("Disable line folding keybinds", "sf_editor_monaco_disablelinefolding"))
+
+	return scrollPanel, "Monaco", "icon16/cog.png", "Monaco options."
+end
+
+function TabHandler:FinishedLoading()
+	self.loaded = true
+	self:UpdateSettings()
+end
+
+function TabHandler:Init()
+	self.loaded = false
+	self.code = ""
+	self.html = vgui.Create("DHTML")
+	self.html:Dock(FILL)
+	self.html:DockMargin(5, 59, 5, 5)
+	self.html:SetKeyboardInputEnabled(true)
+	self.html:SetMouseInputEnabled(true)
+	self.html:SetHTML(
 [[
 <!DOCTYPE html>
 <html lang="en">
@@ -65,9 +141,6 @@ window.MonacoEnvironment = {
 	}
 };
 
-window.sfGetCode = function() {};
-window.sfSetCode = function() {};
-
 require(["vs/editor/editor.main"], function () {
 	const editorElement = document.getElementById("editor");
 
@@ -82,56 +155,96 @@ require(["vs/editor/editor.main"], function () {
 		height: editorElement.offsetHeight
 	}));
 
+	editor.addAction({
+		id: "sf-save",
+		label: "Save",
+		keybindings: [ monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS ],
+		contextMenuGroupId: "File",
+		run: function () { sf.save(); },
+	});
+
+	editor.addAction({
+		id: "sf-save-as",
+		label: "Save As",
+		keybindings: [ monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyS ],
+		contextMenuGroupId: "File",
+		run: function () { sf.saveAs(); },
+	});
+
+	editor.addAction({
+		id: "sf-validate",
+		label: "Validate",
+		keybindings: [ monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.Space ],
+		contextMenuGroupId: "Tasks",
+		run: function () { sf.validate(); },
+	});
+
 	window.sfSetCode = function(str) {editor.setValue(str);};
 	window.sfGetCode = function() {sf.getCode(editor.getValue());};
-	sf.doneLoadingCode();
+	sf.doneLoading();
 });
 </script>
 </body>
 </html>
 
-]]
-	)
-	self.htmlPanel:AddFunction( "sf", "getCode", function( str )
-		self.innerCode = str
-	end)
+]])
 
-	self.isLoaded = false
-	self.htmlPanel:AddFunction( "sf", "doneLoadingCode", function( val )
-		self.isLoaded = true
-		self:SetCode(self.innerCode)
-	end)
-	self.innerCode = ""
+	self.html:AddFunction("sf", "save", function() self:SaveTab() end)
+	self.html:AddFunction("sf", "saveAs", function() self:SaveTab(true) end)
+	self.html:AddFunction("sf", "validate", SF.Editor.doValidation)
+	self.html:AddFunction("sf", "getCode", function(code) self.code = code end)
+	self.html:AddFunction("sf", "doneLoading", function() self:FinishedLoading() end)
+
+	self.html.OnKeyCodePressed = function(_, key, notfirst)
+		--[[if input.IsKeyDown(KEY_LCONTROL) then
+			self:OnShortcut(key)
+		end]]
+		if key == 57 and tobool(GetConVarNumber("sf_editor_monaco_fixconsolebug")) then
+			gui.ActivateGameUI()
+		end
+	end
+
+	self.html:SetVisible(false)
+end
+
+function TabHandler:Cleanup()
+	self.html:Remove()
+	self.html = nil
+	self.loaded = false
+end
+
+-------------
+-- VGUI part
+-------------
+
+local PANEL = {}
+
+function PANEL:Init()
+	self:SetBackgroundColor(Color(39, 40, 34))
+	self:OnThemeChange(SF.Editor.Themes.CurrentTheme)
+	self.code = ""
+end
+
+function PANEL:GetCode()
+	TabHandler:GetCode(self)
+	return self.code
+end
+
+function PANEL:SetCode(code)
+	self.code = code
+	TabHandler:SetCode(self)
 end
 
 function PANEL:OnThemeChange(theme)
 end
 
-function PANEL:OnFocusChanged(gained) -- When this tab is opened
-
+function PANEL:OnFocusChanged(gained)
+	if gained then TabHandler:SetEditorTab(self) end
 end
 
-function PANEL:Validate(movecarret) -- Validate request, has to return success,message
-
+function PANEL:OnRemove()
+	TabHandler:RemoveEditorTab(self)
 end
 
-function PANEL:GetCode()
-	if self.isLoaded then
-		self.htmlPanel:RunJavascript("window.sfGetCode();")
-	end
-	return self.innerCode
-end
-
-function PANEL:SetCode(text)
-	text = SF.Editor.normalizeCode(text)
-	self.innerCode = text
-	if self.isLoaded then
-		self.htmlPanel:RunJavascript("window.sfSetCode(\""..string.JavascriptSafe(text).."\");")
-	end
-end
-
---------------
--- We're done
---------------
-vgui.Register(TabHandler.ControlName, PANEL, "DPanel") -- Registering VGUI element of handler
-return TabHandler -- Our file has to return table of handler
+vgui.Register(TabHandler.ControlName, PANEL, "DPanel")
+return TabHandler
