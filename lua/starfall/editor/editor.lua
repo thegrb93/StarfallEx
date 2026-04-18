1cbaf621b774472b429b5388439e3df99afc168d
thegrb93 <grbrown93@sbcglobal.net>
2026-04-18 00:20:10 -0400

    cleanup

--- lua/starfall/editor/editor.lua
+++ lua/starfall/editor/editor.lua
@@ -108,11 +108,8 @@ if CLIENT then
 		return path
 	end
 
-	--- Converts given text to LF and tabs to spaces.
-	-- Every code which goes into the editor is normalized first.
-	-- Note: remember to normalize every input file when comparing it to already open files to avoid inconsistencies.
 	function SF.Editor.normalizeCode(code)
-		return string.gsub(code, "\r", "")
+		return (string.gsub(code, "\r", ""))
 	end
 
 	function SF.Editor.renameFile(oldFile, newFile)
