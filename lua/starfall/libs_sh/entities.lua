d107d76018978bdad0dce3d213c2e77439620cd2
Cheatoid <Cheatoid@users.noreply.github.com>
2026-08-11 20:31:38 +0200

    Inconsistent API, but whatever

--- lua/starfall/libs_sh/entities.lua
+++ lua/starfall/libs_sh/entities.lua
@@ -1340,15 +1340,14 @@ end
 
 --- Returns a table with a list of bone indices of children bones of the given bone of an entity
 -- @shared
--- @param number? bone Bone index (starts at 0)
+-- @param number bone Bone index (starts at 0)
 -- @return table Table of child bone indices
 function ents_methods:getChildBones(bone)
-	if bone == nil then bone = 0 else checkluatype(bone, TYPE_NUMBER) end
+	checkluatype(bone, TYPE_NUMBER)
 	local ent = eunwrap(self)
 	bone = math.floor(bone)
 	local boneCount = Ent_GetBoneCount(ent)
 	if boneCount == 0 or bone < 0 or bone >= boneCount then SF.Throw("Invalid bone " .. bone, 2) end
-
 	local bones = {}
 	for k = 0, boneCount - 1 do
 		if Ent_GetBoneParent(ent, k) == bone then
