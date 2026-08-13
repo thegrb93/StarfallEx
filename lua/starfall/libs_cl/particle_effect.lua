c63577bd557a494a2f60e111622470bb990cfbdc
Cheatoid <Cheatoid@users.noreply.github.com>
2026-08-13 22:09:58 +0200

    Update
    Co-authored-by: thegrb93 <grbrown93@sbcglobal.net>

--- lua/starfall/libs_cl/particle_effect.lua
+++ lua/starfall/libs_cl/particle_effect.lua
@@ -20,10 +20,142 @@ SF.RegisterLibrary("particleEffect")
 -- @libtbl particleef_methods
 SF.RegisterType("ParticleEffect", false, false)
 
--- Must precache particle effects (on server-side!), otherwise this library won't work (Emitter tool does this as well).
-game.AddParticles("particles/gmod_effects.pcf")
--- NOTE: Only 4096 can be precached on server-side; no limit on client-side.
-PrecacheParticleSystem("generic_smoke")
+-- Must precache particle systems, otherwise this library won't work (error: unknown particle system).
+-- This has to be done in order for this library to work, otherwise particles from those files won't be usable.
+-- We precache all builtin and TF2 PCF since there aren't *that* many, because we can precache any amount on client-side.
+-- All works fine even if some of these are missing (e.g. HL Episodes), such as if you don't have the game installed.
+-- TODO: Use file.Find? But not sure how to find TF2 PCFs...
+for _, name in next, {
+	-- Source Engine base (all are on-demand)
+	"antlion_blood",
+	"blood_impact",
+	"burning_fx",
+	"combineball",
+	"error",
+	"fire_01",
+	"rocket_fx",
+	"train_steam",
+	"vortigaunt_fx",
+	"water_impact",
+	-- GMod specific (all are on-demand)
+	"gmod_effects",
+	"precipitation",
+	-- HL Episodes (all are on-demand)
+	"antlion_gib_01",
+	"antlion_gib_02",
+	"antlion_worker",
+	"grub_blood",
+	"hunter_flechette",
+	"hunter_projectile",
+	"striderbuster",
+	"vehicle",
+	"weapon_fx",
+	-- Team Fortress 2 (all are globally precached except level_fx)
+	"bigboom",
+	"bl_killtaunt",
+	"blood_trail",
+	"bombinomicon",
+	"buildingdamage",
+	"bullet_tracers",
+	"burningplayer",
+	"cig_smoke",
+	"cinefx",
+	"class_fx",
+	"coin_spin",
+	"conc_stars",
+	"crit",
+	"dirty_explode",
+	"disguise",
+	"doomsday_fx",
+	"drg_bison",
+	"drg_cowmangler",
+	"drg_engineer",
+	"drg_pyro",
+	"dxhr_fx",
+	"explosion",
+	"eyeboss",
+	"firstperson_weapon_fx",
+	"flag_particles",
+	"flamethrower",
+	"flamethrower_mvm",
+	"halloween",
+	"halloween2015_unusuals",
+	"halloween2016_unusuals",
+	"halloween2018_unusuals",
+	"halloween2019_unusuals",
+	"halloween2020_unusuals",
+	"halloween2021_unusuals",
+	"halloween2022_unusuals",
+	"halloween2023_unusuals",
+	"halloween2024_unusuals",
+	"halloween2025_unusuals",
+	"harbor_fx",
+	"impact_fx",
+	"invasion_ray_gun_fx",
+	"invasion_unusuals",
+	"item_fx",
+	"items_demo",
+	"items_engineer",
+	"killstreak",
+	--"level_fx", -- marked as on-demand in TF2, so leaving this one out
+	"medicgun_attrib",
+	"medicgun_beam",
+	"muzzle_flash",
+	"mvm",
+	"nailtrails",
+	"nemesis",
+	"npc_fx",
+	"passtime",
+	"passtime_beam",
+	"passtime_tv_projection",
+	"player_recent_teleport",
+	"powerups",
+	"rain_custom",
+	"rankup",
+	"rocketbackblast",
+	"rocketjumptrail",
+	"rocketpack",
+	"rockettrail",
+	"rps",
+	"scary_ghost",
+	"shellejection",
+	"smissmas2019_unusuals",
+	"smissmas2020_unusuals",
+	"smissmas2021_unusuals",
+	"smissmas2022_unusuals",
+	"smissmas2023_unusuals",
+	"smissmas2024_unusuals",
+	"smissmas2025_unusuals",
+	"smoke_blackbillow",
+	"smoke_blackbillow_hoodoo",
+	"smoke_island_volcano",
+	"soldierbuff",
+	"sparks",
+	"speechbubbles",
+	"stamp_spin",
+	"stickybomb",
+	"stormfront",
+	"summer2020_unusuals",
+	"summer2021_unusuals",
+	"summer2022_unusuals",
+	"summer2023_unusuals",
+	"summer2024_unusuals",
+	"summer2025_unusuals",
+	"summer2026_unusuals",
+	"taunt_fx",
+	"teleport_status",
+	"teleported_fx",
+	"training",
+	"urban_fx",
+	"vgui_menu_particles",
+	"water",
+	"weapon_unusual_cool",
+	"weapon_unusual_energyorb",
+	"weapon_unusual_hot",
+	"weapon_unusual_isotope",
+	"xms",
+}
+do game.AddParticles("particles/" .. name .. ".pcf") end
 
 -- Blacklist for bad/expensive particle effects (keys are lowercased name).
 local particle_effect_blacklist = {
@@ -69,12 +201,11 @@ local particle_effects = {} -- setmetatable({}, { __mode = "k" })
 
 instance:AddHook("deinitialize", function()
 	-- Remove all
-	for peff in next, particle_effects do
-		if IsValid(peff) then
-			peff:StopEmissionAndDestroyImmediately()
+	for p in pairs(particle_effects) do
+		if p:IsValid() then
+			p:StopEmissionAndDestroyImmediately()
 		end
-		plyCount:free(instance.player, 1) -- FIXME: what about superusers?
-		particle_effects[peff] = nil
+		plyCount:free(instance.player, 1)
 	end
 end)
 
@@ -92,7 +223,6 @@ end)
 function particleef_library.attach(entity, name, pattach, options)
 	checkluatype(name, TYPE_STRING)
 	checkluatype(pattach, TYPE_NUMBER)
-	if options ~= nil then checkluatype(options, TYPE_TABLE) end
 
 	-- Must unwrap the entity before checking the permission
 	entity = eunwrap(entity)
@@ -101,19 +231,18 @@ function particleef_library.attach(entity, name, pattach, options)
 	checkpermission(instance, entity, "particleEffect.attach")
 
 	-- Superusers bypass the limits.
-	if instance.player ~= SF.Superuser then
-		if isBadParticleEffect(name, instance) then
-			SF.Throw("Bad particle effect: " .. name, 2)
-		end
-		plyCount:use(instance.player, 1)
+	if instance.player ~= SF.Superuser and isBadParticleEffect(name, instance) then
+		SF.Throw("Bad particle effect: " .. name, 2)
 	end
+	plyCount:use(instance.player, 1)
 
-	-- NOTE: Only 4096 can be precached on server-side; no limit on client-side.
+	-- NOTE: There is no precache limit on client-side, but still must precache it!
 	PrecacheParticleSystem(name)
 
 	-- Sanitize the options table.
 	local cleanOptions
 	if options then
+		checkluatype(options, TYPE_TABLE)
 		cleanOptions = {}
 		for i = 1, math.min(#options, 64) do
 			local cp = options[i]
@@ -170,7 +299,8 @@ function particleef_methods:destroy()
 			peff:StopEmissionAndDestroyImmediately()
 		end
 		particle_effects[peff] = nil
-		plyCount:free(instance.player, 1) -- FIXME: what about superusers?
+		plyCount:free(instance.player, 1)
+		particle_meta.sf2sensitive[self] = nil -- make sure that future unwrap throws an error
 	end
 end
 
