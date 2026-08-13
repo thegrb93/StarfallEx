7d924ca475e0b80d6cdcabd7cbb1ee50105ae105
Cheatoid <Cheatoid@users.noreply.github.com>
2026-08-13 11:26:55 +0200

    perf: removed checknumber call and added NaN manual check

--- lua/starfall/libs_sh/effect.lua
+++ lua/starfall/libs_sh/effect.lua
@@ -118,8 +118,7 @@ local function getLimitsForName(name)
 end
 
 local function checkRange(value, min, max, field)
-	checknumber(value)
-	if value < min or value > max then
+	if value ~= value or value < min or value > max then
 		SF.Throw("Effect " .. field .. " must be between " .. min .. " and " .. max, 3)
 	end
 	return value
